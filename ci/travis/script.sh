19d617627165811d7c7573dc48e71019a33fc349
exprmntr <exprmntr@gmail.com>
2017-12-15 16:47:59 +0300

    Update script.sh

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -11,31 +11,31 @@ function install_cuda_linux()
 
 if [ "${CB_BUILD_AGENT}" == 'clang-linux-x86_64-release-cuda' ]; then
     install_cuda_linux;
-    ./ya make --stat -T -r -j 1 catboost/app -DCUDA_ROOT=/usr/local/cuda-8.0;
+    ./ya make --no-emit-status --stat -T -r -j 1 catboost/app -DCUDA_ROOT=/usr/local/cuda-8.0;
     cp $(readlink -f catboost/app/catboost) catboost-cuda-linux;
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python2-linux-x86_64-release' ]; then
      install_cuda_linux;
      cd catboost/python-package;
-     python2 ./mk_wheel.py -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0;
+     python2 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 ;
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python35-linux-x86_64-release' ]; then
      ln -s /home/travis/virtualenv/python3.5.4/bin/python-config /home/travis/virtualenv/python3.5.4/bin/python3-config;
      install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.5.4/bin/python3-config;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.5.4/bin/python3-config;
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python36-linux-x86_64-release' ]; then
      ln -s /home/travis/virtualenv/python3.6.3/bin/python-config /home/travis/virtualenv/python3.6.3/bin/python3-config;
      install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.6.3/bin/python3-config;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.6.3/bin/python3-config;
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'clang-darwin-x86_64-release' ]; then
-    ./ya make --stat -T -r -j 1 catboost/app;
+    ./ya make --no-emit-status --stat -T -r -j 1 catboost/app;
     cp $(readlink catboost/app/catboost) catboost-darwin;
 fi
