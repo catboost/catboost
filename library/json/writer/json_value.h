bbfc36db97db0b9a9ee2aec91b796be5b4717c25
Mikhail Borisov <borisov.mikhail@gmail.com>
2017-12-28 14:13:44 +0300

    Mark GetValueByPath() as const
    ref:f1f35a8f5e0f2b5eab60e37ffe759daf3cbc5cfc

--- library/json/writer/json_value.h
+++ library/json/writer/json_value.h
@@ -81,7 +81,7 @@ public:
     TJsonValue& AppendValue(TJsonValue&& value);
     TJsonValue& Back();
 
-    bool GetValueByPath(TStringBuf path, TJsonValue& result, char delimiter = '.');
+    bool GetValueByPath(TStringBuf path, TJsonValue& result, char delimiter = '.') const;
     bool SetValueByPath(TStringBuf path, const TJsonValue& value, char delimiter = '.');
     bool SetValueByPath(TStringBuf path, TJsonValue&& value, char delimiter = '.');
 
