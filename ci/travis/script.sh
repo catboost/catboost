65dece2a129ee35b5825d773e8c3425f549a65b2
exprmntr <exprmntr@MacBook-Pro-33.local>
2018-01-02 20:02:26 +0300

    Travis - webdav upload

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -13,12 +13,14 @@ if [ "${CB_BUILD_AGENT}" == 'clang-linux-x86_64-release-cuda' ]; then
     install_cuda_linux;
     ./ya make --no-emit-status --stat -T -r -j 1 catboost/app -DCUDA_ROOT=/usr/local/cuda-8.0;
     cp $(readlink -f catboost/app/catboost) catboost-cuda-linux;
+    python ci/webdav_upload.py catboost-cuda-linux
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python2-linux-x86_64-release' ]; then
      install_cuda_linux;
      cd catboost/python-package;
      python2 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 ;
+     python ../../ci/webdav_upload.py *.whl
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python35-linux-x86_64-release' ]; then
@@ -26,6 +28,7 @@ if [ "${CB_BUILD_AGENT}" == 'python35-linux-x86_64-release' ]; then
      install_cuda_linux;
      cd catboost/python-package;
      python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.5.4/bin/python3-config;
+     python ../../ci/webdav_upload.py *.whl
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'python36-linux-x86_64-release' ]; then
@@ -33,9 +36,11 @@ if [ "${CB_BUILD_AGENT}" == 'python36-linux-x86_64-release' ]; then
      install_cuda_linux;
      cd catboost/python-package;
      python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.6.3/bin/python3-config;
+     python ../../ci/webdav_upload.py *.whl
 fi
 
 if [ "${CB_BUILD_AGENT}" == 'clang-darwin-x86_64-release' ]; then
     ./ya make --no-emit-status --stat -T -r -j 1 catboost/app;
     cp $(readlink catboost/app/catboost) catboost-darwin;
+    python ci/webdav_upload.py catboost-darwin
 fi
