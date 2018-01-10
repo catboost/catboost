10654aa1ea380891c25daf365fa2b0c2fd50e966
exprmntr <exprmntr@yandex-team.ru>
2018-01-10 13:02:01 +0300

    Fix version

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -29,10 +29,10 @@ fi
 
 if [ "${CB_BUILD_AGENT}" == 'python34-linux-x86_64-release' ]; then
      ls /home/travis/virtualenv
-     ln -s /home/travis/virtualenv/python3.4.7/bin/python-config /home/travis/virtualenv/python3.4.7/bin/python3-config;
+     ln -s /home/travis/virtualenv/python3.4.6/bin/python-config /home/travis/virtualenv/python3.4.6/bin/python3-config;
      install_cuda_linux;
      cd catboost/python-package;
-     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.4.7/bin/python3-config;
+     python3 ./mk_wheel.py --no-emit-status -T -j 1 -DCUDA_ROOT=/usr/local/cuda-8.0 -DPYTHON_CONFIG=/home/travis/virtualenv/python3.4.6/bin/python3-config;
      python ../../ci/webdav_upload.py *.whl
 fi
 
