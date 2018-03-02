89ba85e1c4daae4774786d1000d7e079b1630412
Nikita Dmitriev <dmitnikita@yandex.ru>
2018-03-02 16:00:56 +0300

    revert commit bug fix r3420061 ref:8d6c20e9d3f09e38ecac8251df0f678e7161f50c

--- catboost/libs/data/load_helpers.h
+++ catboost/libs/data/load_helpers.h
@@ -6,8 +6,7 @@
 
 #include <util/system/fs.h>
 #include <util/stream/file.h>
-#include <util/string/iterator.h>
-#include <util/string/strip.h>
+#include <util/string/split.h>
 
 template <class TStr>
 TVector<TPair> ReadPairs(const TStr& fileName, int docCount) {
@@ -49,7 +48,9 @@ inline int ReadColumnsCount(const TStr& poolFile, char fieldDelimiter = '\t') {
     TIFStream reader(poolFile.c_str());
     TString line;
     CB_ENSURE(reader.ReadLine(line), "pool can't be empty");
-    return StringSplitter(Strip(line)).Split(fieldDelimiter).Count();
+    TVector<TStringBuf> words;
+    SplitRangeTo<const char, TVector<TStringBuf>>(~line, ~line + line.size(), fieldDelimiter, &words);
+    return words.ysize();
 }
 
 template <class TStr>
