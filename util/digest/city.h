a55d6add7f2554d59fe198f95379472ac323abe6
Kostya Bazhanov <yazevnul@yandex-team.ru>
2018-05-03 12:32:53 +0300

    [util/digest/city.*] Add useful note on the used version of CityHash implementation.
    Also:
    - use `data` and `size` instead of `~` and `+` to make interfaces compatible with `stl` strings
    - hide `GetBufFromStr` in private namespace because it's an ugly implementation detail
    
    ref:9b9c5e919aec4578047056e057f85817042e6c05

--- util/digest/city.h
+++ util/digest/city.h
@@ -5,13 +5,16 @@
 
 #include <utility>
 
+// NOTE: These functions provide CityHash 1.0 implementation whose results are *different* from
+// the mainline version of CityHash.
+
 using uint128 = std::pair<ui64, ui64>;
 
-constexpr ui64 Uint128Low64(const uint128& x) noexcept {
+constexpr ui64 Uint128Low64(const uint128& x) {
     return x.first;
 }
 
-constexpr ui64 Uint128High64(const uint128& x) noexcept {
+constexpr ui64 Uint128High64(const uint128& x) {
     return x.second;
 }
 
@@ -30,7 +33,7 @@ uint128 CityHash128WithSeed(const char* s, size_t len, uint128 seed);
 
 // Hash 128 input bits down to 64 bits of output.
 // This is intended to be a reasonably good hash function.
-inline ui64 Hash128to64(const uint128& x) noexcept {
+inline ui64 Hash128to64(const uint128& x) {
     // Murmur-inspired hashing.
     const ui64 kMul = 0x9ddfea08eb382d69ULL;
     ui64 a = (Uint128Low64(x) ^ Uint128High64(x)) * kMul;
@@ -41,38 +44,40 @@ inline ui64 Hash128to64(const uint128& x) noexcept {
     return b;
 }
 
-template <class TStringType>
-inline TStringBuf GetBufFromStr(const TStringType& str) {
-    static_assert(std::is_integral<std::remove_reference_t<decltype(*str.begin())>>::value, "invalid type passed to hash function");
-    return TStringBuf(reinterpret_cast<const char*>(~str), (+str) * sizeof(*str.begin()));
+namespace NPrivateCityHash {
+    template <class TStringType>
+    inline TStringBuf GetBufFromStr(const TStringType& str) {
+        static_assert(std::is_integral<std::remove_reference_t<decltype(*str.data())>>::value, "invalid type passed to hash function");
+        return TStringBuf(reinterpret_cast<const char*>(str.data()), (str.size()) * sizeof(*str.data()));
+    }
 }
 
 template <class TStringType>
 inline ui64 CityHash64(const TStringType& str) {
-    TStringBuf buf = GetBufFromStr(str);
-    return CityHash64(~buf, +buf);
+    TStringBuf buf = NPrivateCityHash::GetBufFromStr(str);
+    return CityHash64(buf.data(), buf.size());
 }
 
 template <class TStringType>
 inline ui64 CityHash64WithSeeds(const TStringType& str, ui64 seed0, ui64 seed1) {
-    TStringBuf buf = GetBufFromStr(str);
-    return CityHash64WithSeeds(~buf, +buf, seed0, seed1);
+    TStringBuf buf = NPrivateCityHash::GetBufFromStr(str);
+    return CityHash64WithSeeds(buf.data(), buf.size(), seed0, seed1);
 }
 
 template <class TStringType>
 inline ui64 CityHash64WithSeed(const TStringType& str, ui64 seed) {
-    TStringBuf buf = GetBufFromStr(str);
-    return CityHash64WithSeed(~buf, +buf, seed);
+    TStringBuf buf = NPrivateCityHash::GetBufFromStr(str);
+    return CityHash64WithSeed(buf.data(), buf.size(), seed);
 }
 
 template <class TStringType>
 inline uint128 CityHash128(const TStringType& str) {
-    TStringBuf buf = GetBufFromStr(str);
-    return CityHash128(~buf, +buf);
+    TStringBuf buf = NPrivateCityHash::GetBufFromStr(str);
+    return CityHash128(buf.data(), buf.size());
 }
 
 template <class TStringType>
 inline uint128 CityHash128WithSeed(const TStringType& str, uint128 seed) {
-    TStringBuf buf = GetBufFromStr(str);
-    return CityHash128WithSeed(~buf, +buf, seed);
+    TStringBuf buf = NPrivateCityHash::GetBufFromStr(str);
+    return CityHash128WithSeed(buf.data(), buf.size(), seed);
 }
