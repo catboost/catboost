71dd7fc05a6297c09ec1f11fa825b5307a01982e
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2018-05-25 18:54:40 +0300

    intermediate changes ref:3d00640f152d06b624e28ffd72f4c7b619b92959

--- library/object_factory/object_factory_ut.cpp
+++ library/object_factory/object_factory_ut.cpp
@@ -1,6 +1,7 @@
 #include <library/object_factory/object_factory.h>
 #include <library/unittest/registar.h>
 
+#include <util/generic/noncopyable.h>
 #include <util/generic/string.h>
 #include <util/generic/ptr.h>
 
@@ -72,6 +73,58 @@ using TTestFactory = TParametrizedObjectFactory<ICommonInterface, TString, const
 static TTestFactory::TRegistrator<TDirectOrder> Direct("direct", new TDirectOrderCreator);
 static TTestFactory::TRegistrator<TInverseOrder> Inverse("inverse");
 
+
+
+class IMoveableOnlyInterface {
+public:
+    virtual ~IMoveableOnlyInterface() {
+    }
+
+    virtual TString GetValue() const = 0;
+};
+
+class TMoveableOnly: public IMoveableOnlyInterface, public TMoveOnly {
+public:
+    TMoveableOnly(TString&& value)
+        : Value(value)
+    {}
+
+    TString GetValue() const override {
+        return Value;
+    }
+
+private:
+    const TString Value;
+};
+
+
+using TMoveableOnlyFactory = TParametrizedObjectFactory<IMoveableOnlyInterface, TString, TString&&>;
+
+static TMoveableOnlyFactory::TRegistrator<TMoveableOnly> MoveableOnlyReg("move");
+
+
+
+class TMoveableOnly2: public IMoveableOnlyInterface, public TMoveOnly {
+public:
+    TMoveableOnly2(THolder<TString>&& value)
+        : Value(std::move(value))
+    {}
+
+    TString GetValue() const override {
+        return *Value;
+    }
+
+private:
+    const THolder<TString> Value;
+};
+
+
+using TMoveableOnly2Factory = TParametrizedObjectFactory<IMoveableOnlyInterface, TString, THolder<TString>&&>;
+
+static TMoveableOnly2Factory::TRegistrator<TMoveableOnly2> MoveableOnly2Reg("move2");
+
+
+
 Y_UNIT_TEST_SUITE(TestObjectFactory) {
     Y_UNIT_TEST(TestParametrized) {
         TArgument directArg{"Name", nullptr};
@@ -86,5 +139,25 @@ Y_UNIT_TEST_SUITE(TestObjectFactory) {
         UNIT_ASSERT(inverse->GetValue() == "Fake1prov2");
 
         UNIT_ASSERT_EQUAL(TDirectOrderCreator::CallsCounter, 1);
+    }
+
+    Y_UNIT_TEST(TestMoveableOnly) {
+        TString v = "value1";
+
+        THolder<IMoveableOnlyInterface> moveableOnly(TMoveableOnlyFactory::Construct("move", std::move(v)));
+
+        UNIT_ASSERT(!!moveableOnly);
+
+        UNIT_ASSERT(moveableOnly->GetValue() == "value1");
+    }
+
+    Y_UNIT_TEST(TestMoveableOnly2) {
+        THolder<TString> v = MakeHolder<TString>("value2");
+
+        THolder<IMoveableOnlyInterface> moveableOnly2(TMoveableOnly2Factory::Construct("move2", std::move(v)));
+
+        UNIT_ASSERT(!!moveableOnly2);
+
+        UNIT_ASSERT(moveableOnly2->GetValue() == "value2");
     }
 }
