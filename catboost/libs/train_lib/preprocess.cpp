3cbc1e4b87fa07db3cadd56b4bfe8bee41e115f3
Nikita Dmitriev <dmitnikita@yandex.ru>
2018-06-01 09:38:49 +0300

    Fix weight exception for pairwise errors
    ref:b80d1e20133d22a9986b1e0c2f7501d938d6db2b

--- catboost/libs/train_lib/preprocess.cpp
+++ catboost/libs/train_lib/preprocess.cpp
@@ -106,9 +106,9 @@ void CheckConsistency(const NCatboostOptions::TLossDescription& lossDescription,
     CB_ENSURE(weightBounds.Min >= 0, "Has negative weight: " + ToString(weightBounds.Min));
     CB_ENSURE(weightBounds.Max > 0, "All weights are 0");
 
-    if (lossDescription.GetLossFunction() == ELossFunction::PairLogit) {
+    if (IsPairwiseError(lossDescription.GetLossFunction())) {
         if (weightBounds.Min != weightBounds.Max) {
-            MATRIXNET_WARNING_LOG << "Pairwise loss doesn't support document weights. They will be ignored in optimization. If a custom metric is specified then they will be used for custom metric calculation." << Endl;
+            MATRIXNET_WARNING_LOG << "Pairwise losses don't support document weights. They will be ignored in optimization. If a custom metric is specified then they will be used for custom metric calculation." << Endl;
         }
     }
 
