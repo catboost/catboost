3e50eaab049ec1596aca5bc3307efaeb22d86d66
Sergei Shirshanov <heretic@yandex-team.ru>
2018-06-05 11:39:00 +0300

    better jdk 8/10 support
    ref:10245c476f73094682552521e33712d0f43974b7

--- build/scripts/build_java_with_error_prone.py
+++ build/scripts/build_java_with_error_prone.py
@@ -6,10 +6,25 @@ ERROR_PRONE_FLAGS = [
     '-Xep:ReturnValueIgnored:WARN',
 ]
 
+JAVA10_EXPORTS = [
+    '--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED',
+    '--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED',
+    '--add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED',
+    '--add-exports=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED',
+    '--add-exports=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED',
+    '--add-exports=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED',
+    '--add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED',
+    '--add-exports=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED',
+    '--add-opens=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED'
+]
+
 
 def just_do_it(argv):
     java, error_prone_tool, javac_cmd = argv[0], argv[1], argv[2:]
-    os.execv(java, [java, '-Xbootclasspath/p:' + error_prone_tool, 'com.google.errorprone.ErrorProneCompiler'] + ERROR_PRONE_FLAGS + javac_cmd)
+    if java.endswith('javac') or java.endswith('javac.exe'):
+        os.execv(java, [java] + JAVA10_EXPORTS + ['-processorpath', error_prone_tool] + [(' '.join(['-Xplugin:ErrorProne'] + ERROR_PRONE_FLAGS))] + javac_cmd)
+    else:
+        os.execv(java, [java, '-Xbootclasspath/p:' + error_prone_tool, 'com.google.errorprone.ErrorProneCompiler'] + ERROR_PRONE_FLAGS + javac_cmd)
 
 
 if __name__ == '__main__':
