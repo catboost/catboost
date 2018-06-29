04fbc8c22c5e267cd53d17e74450ad51225ce32b
exprmntr <exprmntr@yandex-team.ru>
2018-06-29 13:19:36 +0300

    Check exit code ref:5dcfec0e4b3edd76b601991c2e315a909fc07773

--- catboost/python-package/ut/medium/test_whl.py
+++ catboost/python-package/ut/medium/test_whl.py
@@ -2,7 +2,6 @@ import yatest.common
 import shutil
 import os
 import zipfile
-import subprocess
 
 
 PYTHON_PACKAGE_DIR = os.path.join("catboost", "python-package")
@@ -22,4 +21,4 @@ def test_wheel():
 
     python_binary = yatest.common.binary_path(os.path.join(PYTHON_PACKAGE_DIR, "ut", "medium", "python_binary", "catboost-python"))
     test_script = yatest.common.source_path(os.path.join(PYTHON_PACKAGE_DIR, "ut", "medium", "run_catboost.py"))
-    subprocess.Popen([python_binary, test_script], env={'PYTHONPATH': os.path.join(os.getcwd(), 'catboost')}).communicate()
+    yatest.common.execute([python_binary, test_script], env={'PYTHONPATH': os.path.join(os.getcwd(), 'catboost')})
