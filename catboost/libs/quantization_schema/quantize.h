154987495a76b38e439a55178661863713e85eda
Kostya Bazhanov <yazevnul@yandex-team.ru>
2018-07-27 12:33:37 +0300

    oh, wait, doesn't make any sence, in case of exception `nanMode` is always `ENanMode::Forbidden` ref:683e6e5d2844cf6ae3255da1f724ba46deefae87

--- catboost/libs/quantization_schema/quantize.h
+++ catboost/libs/quantization_schema/quantize.h
@@ -5,12 +5,11 @@
 
 #include <util/generic/array_ref.h>
 #include <util/generic/ymath.h>
-#include <util/stream/labeled.h>
 
 namespace NCB {
     inline size_t Quantize(const float value, const TConstArrayRef<float> borders, const ENanMode nanMode) {
         if (IsNan(value)) {
-            CB_ENSURE(ENanMode::Forbidden != nanMode, "NaNs are forbidden; " << LabeledOutput(nanMode));
+            CB_ENSURE(ENanMode::Forbidden != nanMode, "NaNs are forbidden");
             return ENanMode::Min == nanMode ? 0 : borders.size();
         }
 
