9f83c260c1019d9822263587e8b5d33ff10f43ec
Vasily Ershov <vasilij.ershov@gmail.com>
2018-08-07 18:52:01 +0300

    remove noise file ref:5b06cb519df9a13d4521f740f0c9b92ef18f899a

--- catboost/cuda/methods/greedy_subsets_searcher/select_leaves_to_split.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace NCatboostCuda {
-    void SelectLeavesToSplit(const NCattboconst TPointsSubsets& subsets,
-                             TVector<ui32>* leavesToSkip,
-                             TVector<ui32>* leavesToSplit);
-}
