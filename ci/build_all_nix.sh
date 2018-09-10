dc805f624757cdba3ab86e836532c8cef143a199
Stanislav Kirillov <staskirillov@gmail.com>
2018-09-10 03:54:14 +0300

    add build_all scripts for gocd and local building release artifacts ref:05f2cc680f25a2da8abc383bf09c06ee7d04caf7

---
+++ ci/build_all_nix.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#TODO(kizill): split this into subscripts to make it prettier
+
+eval "$(pyenv init -)"
+
+set -x
+set -e
+
+if [[ -z "${CUDA_ARG}" ]]; then
+  echo 'missing CUDA_ARG variable, should be something like "-DCUDA_ROOT=/usr/local/cuda-9.1/"'
+fi
+
+python ya make -r -DOS_SDK=local $CUDA_ARG -o . catboost/app
+strip catboost/app/catboost
+
+cd catboost/python-package
+
+PY27=2.7.14
+pyenv install -s $PY27
+pyenv shell $PY27
+python mk_wheel.py -DOS_SDK=local $CUDA_ARG -DPYTHON_CONFIG=python2-config
+
+PY35=3.5.5
+pyenv install -s $PY35
+pyenv shell $PY35
+python mk_wheel.py -DOS_SDK=local $CUDA_ARG -DPYTHON_CONFIG=python3-config
+
+PY36=3.6.6
+pyenv install -s $PY36
+pyenv shell $PY36
+python mk_wheel.py -DOS_SDK=local $CUDA_ARG -DPYTHON_CONFIG=python3-config
+
+PY37=3.7.0
+pyenv install -s $PY37
+pyenv shell $PY37
+python mk_wheel.py -DOS_SDK=local $CUDA_ARG -DPYTHON_CONFIG=python3-config
+
+echo "Starting R package build"
+cd ../R-package
+mkdir -pv catboost
+
+cp DESCRIPTION catboost
+cp NAMESPACE catboost
+cp README.md catboost
+
+cp -r R catboost
+
+cp -r inst catboost
+cp -r man catboost
+cp -r tests catboost
+
+../../ya make -r -T src
+
+mkdir -p catboost/inst/libs
+cp $(readlink src/libcatboostr.so) catboost/inst/libs
+
+tar -cvzf catboost-R-$(uname).tgz catboost
---
+++ ci/build_all_win64.bat
@@ -0,0 +1,64 @@
+@echo on
+
+call "%VS_VARS_PATH%\vcvars64.bat" -vcvars_ver=14.11
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+c:\Python27\python.exe ya make -r -DOS_SDK=local -DCUDA_ROOT="%CUDA_PATH%" -DHAVE_CUDA=yes -o . catboost\app
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cd catboost\python-package
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+set PyV=27
+echo c:\Python%PyV%\python.exe
+c:\Python%PyV%\python.exe mk_wheel.py -DOS_SDK=local -DCUDA_ROOT="%CUDA_PATH%" -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+set PyV=35
+echo c:\Python%PyV%\python.exe
+c:\Python%PyV%\python.exe mk_wheel.py -DOS_SDK=local -DCUDA_ROOT="%CUDA_PATH%" -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+set PyV=36
+echo c:\Python%PyV%\python.exe
+c:\Python%PyV%\python.exe mk_wheel.py -DOS_SDK=local -DCUDA_ROOT="%CUDA_PATH%" -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+set PyV=37
+echo c:\Python%PyV%\python.exe
+c:\Python%PyV%\python.exe mk_wheel.py -DOS_SDK=local -DCUDA_ROOT="%CUDA_PATH%" -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo Building R-package
+
+cd ..\..
+call ya.bat make -r -T -o . .\catboost\R-package\src\
+if %errorlevel% neq 0 exit /b %errorlevel%
+cd catboost\R-package
+if %errorlevel% neq 0 exit /b %errorlevel%
+mkdir catboost
+
+
+copy DESCRIPTION catboost
+if %errorlevel% neq 0 exit /b %errorlevel%
+copy NAMESPACE catboost
+if %errorlevel% neq 0 exit /b %errorlevel%
+copy README.md catboost
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+xcopy R catboost /S /E
+if %errorlevel% neq 0 exit /b %errorlevel%
+xcopy inst catboost /S /E
+if %errorlevel% neq 0 exit /b %errorlevel%
+xcopy man catboost /S /E
+if %errorlevel% neq 0 exit /b %errorlevel%
+xcopy tests catboost /S /E
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+mkdir catboost\inst\libs
+if %errorlevel% neq 0 exit /b %errorlevel%
+mkdir catboost\inst\libs\x64
+if %errorlevel% neq 0 exit /b %errorlevel%
+copy src\libcatboostr.dll catboost\inst\libs\x64
+if %errorlevel% neq 0 exit /b %errorlevel%
+7z -ttar a dummy catboost -so | 7z -si -tgzip a catboost-R-Windows.tgz
