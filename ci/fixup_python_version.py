f9fca19c4373093a53961b5fa408523e6fe3d963
Stanislav Kirillov <staskirillov@gmail.com>
2018-09-12 14:12:12 +0300

    add temporary script for python package version manipulation inside GoCD dev pipline ref:eed5662b6146b034d8e0f3f95f90521b9697d3eb

---
+++ ci/fixup_python_version.py
@@ -0,0 +1,7 @@
+import os
+
+version = os.environ.get('CATBOOST_PACKAGE_VERSION')
+counter = os.environ.get('GO_PIPELINE_COUNTER', '0')
+if version:
+    with open('catboost/python-package/catboost/version.py', 'w') as version_file:
+        version_file.write('VERSION = \'{}.{}\'\n'.format(version, counter))
