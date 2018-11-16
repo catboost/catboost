105bac282820d2f976bd6989e411b9519c77f601
Andrei Khropov <akhropov@yandex-team.ru>
2018-11-16 14:54:16 +0300

    Improve CD file parsing error messages. MLTOOLS-2068.
    ref:4146ffdb73552bd0f6f7f5dc211e13f9c4ede229

--- catboost/libs/column_description/cd_parser.cpp
+++ catboost/libs/column_description/cd_parser.cpp
@@ -5,6 +5,7 @@
 #include <catboost/libs/helpers/exception.h>
 #include <catboost/libs/logging/logging.h>
 
+#include <util/generic/cast.h>
 #include <util/generic/set.h>
 #include <util/stream/file.h>
 #include <util/stream/input.h>
@@ -17,8 +18,8 @@ using namespace NCB;
 
 namespace {
 
-    inline void CheckAllFeaturesPresent(const TVector<TColumn>& columns, const TSet<int>& parsedColumns) {
-        for (int i = 0; i < columns.ysize(); ++i) {
+    inline void CheckAllFeaturesPresent(const TVector<TColumn>& columns, const TSet<size_t>& parsedColumns) {
+        for (size_t i = 0; i < columns.size(); ++i) {
             CB_ENSURE(parsedColumns.has(i), "column not present in cd file: " << i);
         }
     }
@@ -26,47 +27,56 @@ namespace {
 
     template <class TReadLineFunc>
     TVector<TColumn> ReadCDImpl(TReadLineFunc readLineFunc, const TCdParserDefaults& defaults) {
-        int columnsCount = defaults.UseDefaultType ? defaults.ColumnCount : 0;
+        size_t columnsCount = defaults.UseDefaultType ? SafeIntegerCast<size_t>(defaults.ColumnCount) : 0;
 
         TVector<TColumn> columns(columnsCount, TColumn{defaults.DefaultColumnType, TString()});
-        TSet<int> parsedColumns;
+        TSet<size_t> parsedColumns;
 
         TString line;
         for (size_t lineNumber = 0; readLineFunc(&line); lineNumber++) {
             TVector<TString> tokens;
             try {
-                Split(line, "\t", tokens);
-            } catch (const yexception& e) {
-                CATBOOST_DEBUG_LOG << "Got exception " << e.what() << " while parsing feature descriptions line " << line << Endl;
-                break;
-            }
-            if (tokens.empty()) {
-                continue;
-            }
-            CB_ENSURE(tokens.ysize() == 2 || tokens.ysize() == 3,
-                "Incorrect CD file. Each line should have two or three columns. " <<
-                "Invalid line number #" << lineNumber << ": " << line);
-            int index = FromString<int>(tokens[0]);
-            CB_ENSURE(index >= 0, "Invalid column index " << index);
-            if (defaults.UseDefaultType) {
-                CB_ENSURE(
-                    index < columnsCount,
-                    "Invalid column index: " LabeledOutput(index, columnsCount));
-            }
-            CB_ENSURE(!parsedColumns.has(index), "column specified twice in cd file: " << index);
-            parsedColumns.insert(index);
-            columns.resize(Max(columns.ysize(), index + 1));
+                try {
+                    Split(line, "\t", tokens);
+                } catch (const yexception& e) {
+                    CATBOOST_DEBUG_LOG << "Got exception " << e.what() << " while parsing feature descriptions line " << line << Endl;
+                    break;
+                }
+                if (tokens.empty()) {
+                    continue;
+                }
+                CB_ENSURE(tokens.ysize() == 2 || tokens.ysize() == 3,
+                    "Each line should have two or three columns. This line has " << tokens.size()
+                );
 
-            TStringBuf type = tokens[1];
-            if (type == "QueryId") {
-                type = "GroupId";
-            }
-            if (type == "Target") {
-                type = "Label";
-            }
-            CB_ENSURE(TryFromString<EColumn>(type, columns[index].Type), "unsupported column type " << type);
-            if (tokens.ysize() == 3) {
-                columns[index].Id = tokens[2];
+                size_t index = 0;
+                CB_ENSURE(
+                    TryFromString(tokens[0], index),
+                    "Invalid column index: \"" << tokens[0] << "\""
+                );
+                if (defaults.UseDefaultType) {
+                    CB_ENSURE(
+                        index < columnsCount,
+                        "Invalid column index: " LabeledOutput(index, columnsCount));
+                }
+                CB_ENSURE(!parsedColumns.has(index), "column specified twice in cd file: " << index);
+                parsedColumns.insert(index);
+                columns.resize(Max(columns.size(), index + 1));
+
+                TStringBuf type = tokens[1];
+                if (type == "QueryId") {
+                    type = "GroupId";
+                }
+                if (type == "Target") {
+                    type = "Label";
+                }
+                CB_ENSURE(TryFromString<EColumn>(type, columns[index].Type), "unsupported column type " << type);
+                if (tokens.ysize() == 3) {
+                    columns[index].Id = tokens[2];
+                }
+            } catch (const TCatboostException& e) {
+                throw TCatboostException() << "Incorrect CD file. Invalid line number #" << lineNumber
+                    << ": " << e.what();
             }
         }
         if (!defaults.UseDefaultType) {
