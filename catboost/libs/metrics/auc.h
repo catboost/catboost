697c463fca0a6fb61133b0a63bbfbc515bf3bb77
Anton Trubakov <antontrubakov@gmail.com>
2018-12-02 17:30:33 +0300

    remove gini signature from auc.h

--- catboost/libs/metrics/auc.h
+++ catboost/libs/metrics/auc.h
@@ -3,4 +3,3 @@
 #include "sample.h"
 
 double CalcAUC(TVector<NMetrics::TSample>* samples, double* outWeightSum = nullptr, double* outPairWeightSum = nullptr);
-double CalcGINI(TVector<NMetrics::TSample>* samples, double* outWeightSum = nullptr, double* outPairWeightSum = nullptr);
