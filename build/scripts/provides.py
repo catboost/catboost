8f6c1aaf646b00df6a2f2bda895d8bebb00c1f54
orivej <orivej@yandex-team.ru>
2018-12-07 21:29:17 +0300

    Also rename NProvides namespace. DEVTOOLS-4976
    Note: mandatory check (NEED_CHECK) was skipped
    ref:e06bb025a418a55f598756b2091ad4a615d7610f

--- build/scripts/provides.py
+++ build/scripts/provides.py
@@ -4,7 +4,7 @@ import sys
 def main():
     out, names = sys.argv[1], sys.argv[2:]
     with open(out, 'w') as f:
-        f.write('namespace NProvide {\n')
+        f.write('namespace NProvides {\n')
         for name in sorted(names):
             f.write('    bool {} = true;\n'.format(name))
         f.write('}\n')
