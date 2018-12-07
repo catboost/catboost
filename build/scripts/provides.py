77de31af3ea405430901d2174f23eec474b2f4d7
orivej <orivej@yandex-team.ru>
2018-12-07 20:27:17 +0300

    Add PROVIDES macro.
    ref:f77ee63f10c11c650156933fd6077c23c747cbd5

---
+++ build/scripts/provides.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def main():
+    out, names = sys.argv[1], sys.argv[2:]
+    with open(out, 'w') as f:
+        f.write('namespace NProvide {\n')
+        for name in sorted(names):
+            f.write('    bool {} = true;\n'.format(name))
+        f.write('}\n')
+
+
+if __name__ == '__main__':
+    main()
