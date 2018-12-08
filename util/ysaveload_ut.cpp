bbe862381ea79432ebb9e450b1451b5f434cae78
danlark <danlark@yandex-team.ru>
2018-12-08 08:32:37 +0300

    remove + in TDeque
    ref:b03c00ddf5bfefb64c3772d67b82de0d5b0dcda8

--- util/ysaveload_ut.cpp
+++ util/ysaveload_ut.cpp
@@ -251,7 +251,7 @@ private:
             TVector<ui16> vec;
 
             Load(&S_, vec);
-            UNIT_ASSERT_EQUAL(+vec, 3);
+            UNIT_ASSERT_EQUAL(vec.size(), 3);
             UNIT_ASSERT_EQUAL(vec[0], 1);
             UNIT_ASSERT_EQUAL(vec[1], 2);
             UNIT_ASSERT_EQUAL(vec[2], 4);
@@ -304,7 +304,7 @@ private:
             TBuffer buf;
 
             Load(&S_, buf);
-            UNIT_ASSERT_EQUAL(+buf, 4);
+            UNIT_ASSERT_EQUAL(buf.size(), 4);
             UNIT_ASSERT_EQUAL(memcmp(~buf, "asdf", 4), 0);
         }
 
@@ -314,7 +314,7 @@ private:
 
             Load(&S_, vec, pool);
 
-            UNIT_ASSERT_EQUAL(+vec, 3);
+            UNIT_ASSERT_EQUAL(vec.size(), 3);
             UNIT_ASSERT_EQUAL(vec[0], TString("1"));
             UNIT_ASSERT_EQUAL(vec[1], TString("123"));
             UNIT_ASSERT_EQUAL(vec[2], TString("4567"));
@@ -325,7 +325,7 @@ private:
 
             Load(&S_, deq);
 
-            UNIT_ASSERT_EQUAL(+deq, 4);
+            UNIT_ASSERT_EQUAL(deq.size(), 4);
             UNIT_ASSERT_EQUAL(deq[0], 1);
             UNIT_ASSERT_EQUAL(deq[1], 2);
             UNIT_ASSERT_EQUAL(deq[2], 4);
