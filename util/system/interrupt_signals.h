a38267929673c6ce2dc4b5b8cec639b34ae2e1fe
Alexander Fokin <apfokin@gmail.com>
2019-01-11 16:56:37 +0300

    [util/system] Docs.
    ref:dce2c8cbbbd8c070e0979c81a921ff508a103612

--- util/system/interrupt_signals.h
+++ util/system/interrupt_signals.h
@@ -10,11 +10,13 @@
     #endif
 #endif
 
-/*
+/**
  * Set handler for interrupt signals.
  *
  * All OSes:      SIGINT, SIGTERM (defined by C++ standard)
  * UNIX variants: Also SIGHUP
  * Windows:       CTRL_C_EVENT handled as SIGINT, CTRL_BREAK_EVENT as SIGTERM, CTRL_CLOSE_EVENT as SIGHUP
+ *
+ * \param handler                       Signal handler to use. Pass nullptr to clear currently set handler.
  */
 void SetInterruptSignalsHandler(void (*handler)(int signum));
