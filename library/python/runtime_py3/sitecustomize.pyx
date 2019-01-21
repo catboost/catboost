9b42847252afcdc6d171730256467403e58ac239
heretic <heretic@yandex-team.ru>
2019-01-21 23:24:28 +0300

    test_tool global resource prototype
    ref:c7b1dc04923ad6f39184d111cbfefc3e41b523d0

--- library/python/runtime_py3/sitecustomize.pyx
+++
@@ -1,5 +0,0 @@
-import traceback
-
-# Use pure-python implementation of traceback printer.
-# Built-in printer (PyTraceBack_Print) does not support custom module loaders
-sys.excepthook = traceback.print_exception
--- library/python/runtime_py3/test/traceback/__main__.py
+++
@@ -1,4 +0,0 @@
-from library.python.runtime_py3.test.traceback.crash import main
-
-if __name__ == "__main__":
-    main()
