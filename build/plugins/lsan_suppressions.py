ccc4e207e9dee08a800d448d77687624df9dcbde
Aleksandr <ivansduck@gmail.com>
2019-02-22 13:47:17 +0300

    Added doctest description to the lsan_suppressions.py
    ref:b6a9094d603918613545863a5300453f41a0da99

--- build/plugins/lsan_suppressions.py
+++ build/plugins/lsan_suppressions.py
@@ -1,3 +1,8 @@
 def onlsan_suppressions(unit, *args):
+    """
+        LSAN_SUPPRESSIONS() - allows to specify files with suppression notation which will be used by default.
+        See https://clang.llvm.org/docs/AddressSanitizer.html#suppressing-memory-leaks
+        for details.
+    """
     if unit.get("SANITIZER_TYPE") in ("leak", "address"):
         unit.onsrcs(["GLOBAL"] + list(args))
