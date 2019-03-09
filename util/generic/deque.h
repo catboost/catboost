4e43c17fdcf01a7c8333b9eab9cdfeca59515765
thegeorg <thegeorg@yandex-team.ru>
2019-03-09 15:18:53 +0300

    util/generic: Use ctor inheritance for TStack, TList, TDeque
    ([arc::pullid] 2c2a4c16-66adf8ec-7770c18c-d68d7a39)
    
    ref:d3732d777e284adf7ebc2373cfe9021326bdbf86

--- util/generic/deque.h
+++ util/generic/deque.h
@@ -8,73 +8,14 @@
 #include <memory>
 #include <initializer_list>
 
-template <class T, class A /*= std::allocator<T>*/>
+template <class T, class A>
 class TDeque: public std::deque<T, TReboundAllocator<A, T>> {
-public:
     using TBase = std::deque<T, TReboundAllocator<A, T>>;
-    using TSelf = TDeque<T, A>;
-    using allocator_type = typename TBase::allocator_type;
-    using size_type = typename TBase::size_type;
-    using value_type = typename TBase::value_type;
-
-    inline TDeque() noexcept(std::is_nothrow_default_constructible<TBase>::value)
-        : TBase()
-    {
-    }
-
-    inline TDeque(const typename TBase::allocator_type& a)
-        : TBase(a)
-    {
-    }
-
-    explicit inline TDeque(size_type count)
-        : TBase(count)
-    {
-    }
-
-    inline TDeque(size_type count, const T& val)
-        : TBase(count, val)
-    {
-    }
-
-    template <class TIter>
-    inline TDeque(TIter first, TIter last)
-        : TBase(first, last)
-    {
-    }
-
-    inline TDeque(const TSelf& src)
-        : TBase(src)
-    {
-    }
-
-    inline TDeque(TSelf&& src) noexcept
-        : TBase(std::forward<TSelf>(src))
-    {
-    }
-
-    inline TDeque(std::initializer_list<value_type> il, const allocator_type& alloc = allocator_type())
-        : TBase(il, alloc)
-    {
-    }
-
-    inline TSelf& operator=(const TSelf& src) {
-        TBase::operator=(src);
-        return *this;
-    }
-
-    inline TSelf& operator=(TSelf&& src) noexcept {
-        TBase::operator=(std::forward<TSelf>(src));
-        return *this;
-    }
+public:
+    using TBase::TBase;
 
     inline yssize_t ysize() const noexcept {
         return (yssize_t)this->size();
-    }
-
-    Y_PURE_FUNCTION
-    inline bool empty() const noexcept {
-        return TBase::empty();
     }
 
     inline explicit operator bool() const noexcept {
