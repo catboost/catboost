c681c9b021df3d163981dd6cedd75eb7ad334861
thegeorg <thegeorg@yandex-team.ru>
2019-03-11 23:12:23 +0300

    Use ctor inheritance in TMap, TMultiMap
    ([arc::pullid] 5b76627a-54f983a4-66f63a6c-3ef974ca)
    
    ref:84c259f7b08da3509c91d19bd646e8300df2e3a0

--- util/generic/map.h
+++ util/generic/map.h
@@ -14,56 +14,9 @@
 template <class K, class V, class Less, class A>
 class TMap: public std::map<K, V, Less, TReboundAllocator<A, std::pair<const K, V>>>, public TMapOps<TMap<K, V, Less, A>> {
     using TBase = std::map<K, V, Less, TReboundAllocator<A, std::pair<const K, V>>>;
-    using TSelf = TMap<K, V, Less, A>;
-    using TAllocatorType = typename TBase::allocator_type;
-    using TKeyCompare = typename TBase::key_compare;
-    using TValueType = typename TBase::value_type;
 
 public:
-    inline TMap() {
-    }
-
-    template <typename TAllocParam>
-    inline explicit TMap(TAllocParam* allocator)
-        : TBase(Less(), allocator)
-    {
-    }
-
-    template <class It>
-    inline TMap(It f, It l)
-        : TBase(f, l)
-    {
-    }
-
-    inline TMap(std::initializer_list<TValueType> il, const TKeyCompare& comp = TKeyCompare(), const TAllocatorType& alloc = TAllocatorType())
-        : TBase(il, comp, alloc)
-    {
-    }
-
-    inline TMap(const TSelf& src)
-        : TBase(src)
-    {
-    }
-
-    inline TMap(TSelf&& src) noexcept
-        : TBase(std::forward<TSelf>(src))
-    {
-    }
-
-    inline TSelf& operator=(const TSelf& src) {
-        TBase::operator=(src);
-        return *this;
-    }
-
-    inline TSelf& operator=(TSelf&& src) noexcept {
-        TBase::operator=(std::forward<TSelf>(src));
-        return *this;
-    }
-
-    Y_PURE_FUNCTION
-    inline bool empty() const noexcept {
-        return TBase::empty();
-    }
+    using TBase::TBase;
 
     inline explicit operator bool() const noexcept {
         return !this->empty();
@@ -77,67 +30,12 @@ public:
 template <class K, class V, class Less, class A>
 class TMultiMap: public std::multimap<K, V, Less, TReboundAllocator<A, std::pair<const K, V>>> {
     using TBase = std::multimap<K, V, Less, TReboundAllocator<A, std::pair<const K, V>>>;
-    using TSelf = TMultiMap<K, V, Less, A>;
-    using TAllocatorType = typename TBase::allocator_type;
-    using TKeyCompare = typename TBase::key_compare;
-    using TValueType = typename TBase::value_type;
 
 public:
-    inline TMultiMap() {
-    }
-
-    template <typename TAllocParam>
-    inline explicit TMultiMap(TAllocParam* allocator)
-        : TBase(Less(), allocator)
-    {
-    }
-
-    inline explicit TMultiMap(const Less& less, const TAllocatorType& alloc = TAllocatorType())
-        : TBase(less, alloc)
-    {
-    }
-
-    template <class It>
-    inline TMultiMap(It f, It l)
-        : TBase(f, l)
-    {
-    }
-
-    inline TMultiMap(std::initializer_list<TValueType> il, const TKeyCompare& comp = TKeyCompare(), const TAllocatorType& alloc = TAllocatorType())
-        : TBase(il, comp, alloc)
-    {
-    }
-
-    inline TMultiMap(const TSelf& src)
-        : TBase(src)
-    {
-    }
-
-    inline TMultiMap(TSelf&& src) noexcept {
-        this->swap(src);
-    }
-
-    inline TSelf& operator=(const TSelf& src) {
-        TBase::operator=(src);
-        return *this;
-    }
-
-    inline TSelf& operator=(TSelf&& src) noexcept {
-        // Self-move assignment is undefined behavior in the Standard.
-        // This implementation ends up with zero-sized multimap.
-        this->clear();
-        this->swap(src);
-
-        return *this;
-    }
+    using TBase::TBase;
 
     inline explicit operator bool() const noexcept {
         return !this->empty();
-    }
-
-    Y_PURE_FUNCTION
-    inline bool empty() const noexcept {
-        return TBase::empty();
     }
 
     inline bool contains(const K& key) const {
