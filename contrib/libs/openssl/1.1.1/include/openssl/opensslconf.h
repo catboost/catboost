00c07e1a16685ea736282c06b380098467f06594
deshevoy <deshevoy@yandex-team.ru>
2019-03-15 22:03:45 +0300

    Put OpenSSL 1.1.1b to a subfolder
    ([arc::pullid] fa14e571-602ee198-b46b77c1-5f3515ba)
    
    ref:1028fbddd25ecc0d1103e44a455c13d7c2ff2122

---
+++ contrib/libs/openssl/1.1.1/include/openssl/opensslconf.h
@@ -0,0 +1,9 @@
+#if defined(__APPLE__) && defined(__x86_64__)
+#   include "opensslconf-osx.h"
+#elif defined(__linux__) && defined(__aarch64__)
+#   include "opensslconf-linux_aarch64.h"
+#elif defined(_MSC_VER) && defined(_M_X64)
+#   include "opensslconf-win.h"
+#else
+#   include "opensslconf-linux.h"
+#endif
