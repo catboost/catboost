6b00b7c06de1d99cbf66cf9b98384b6adc4c828e
deshevoy <deshevoy@yandex-team.ru>
2019-03-15 21:17:23 +0300

    Copy OpenSSL 1.0.2 to a subfolder
    ([arc::pullid] 5200be58-9e4f7c1f-f0445704-88c78160)
    
    ref:8f0f379c90ce79b5ca3516e62c6411e0b081ed9d

---
+++ contrib/libs/openssl/1.0.2/include/openssl/opensslconf.h
@@ -0,0 +1,13 @@
+#if defined(__APPLE__) && defined(__x86_64__)
+#   include "opensslconf-osx.h"
+#elif defined(__FreeBSD__) && defined(__x86_64__)
+#   include "opensslconf-freebsd-linux.h"
+#elif defined(__linux__) && defined(__aarch64__)
+#   include "opensslconf-linux_aarch64.h"
+#elif defined(_MSC_VER) && defined(_M_X64)
+#   include "opensslconf-win.h"
+#elif defined(_MSC_VER) && defined(_M_IX86)
+#   include "opensslconf-win32.h"
+#else
+#   include "opensslconf-freebsd-linux.h"
+#endif
