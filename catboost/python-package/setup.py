344f73b72c07566a76c9812003586902783512e8
David Waterworth <david.waterworth@cimenviro.com>
2019-03-15 12:34:57 +1100

    added setup.py to enable 'pip install -e .'

---
+++ catboost/python-package/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+PACKAGE = 'catboost'
+
+setup(
+    name=PACKAGE,
+    version="0.13",
+
+    author="CatBoost Developers",
+    description="Python package for catboost",
+    license="Apache License, Version 2.0",
+
+    packages=find_packages(),
+    install_requires=[
+        'enum34',
+        'six',
+        'numpy >= 1.11.1',
+        'pandas >= 0.19.1'
+    ],
+)
