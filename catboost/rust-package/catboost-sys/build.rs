bcba50f95c2aa82a3cc8bb7c097f40b5ee9de5c8
Sergey Kiselev <kiselev.sg@gmail.com>
2019-03-21 22:59:08 +0300

    Add initial rust package

---
+++ catboost/rust-package/catboost-sys/build.rs
@@ -0,0 +1,50 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+fn main() {
+    let target = env::var("TARGET").unwrap();
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let cb_model_interface_root = Path::new("../../libs/model_interface/")
+        .canonicalize()
+        .unwrap();
+
+    Command::new("../../../ya")
+        .args(&[
+            "make",
+            "-d",
+            cb_model_interface_root.to_str().unwrap(),
+            "--sanitize=address",
+            "-o",
+            out_dir.to_str().unwrap(),
+        ])
+        .status()
+        .unwrap_or_else(|e| {
+            panic!("Failed to yamake libcatboostmodel: {}", e);
+        });
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg(format!("-I{}", cb_model_interface_root.display()))
+        .rustfmt_bindings(true)
+        .generate()
+        .expect("Unable to generate bindings.");
+
+    bindings
+        .write_to_file(out_dir.join("bindings.rs"))
+        .expect("Couldn't write bindings.");
+
+    println!(
+        "cargo:rustc-link-search={}",
+        out_dir.join("catboost/libs/model_interface").display()
+    );
+
+    if target.contains("apple") {
+        println!("cargo:rustc-link-lib=c++");
+    } else {
+        println!("cargo:rustc-link-lib=stdc++");
+    }
+    println!("cargo:rustc-link-lib=dylib=catboostmodel");
+}
---
+++ catboost/rust-package/catboost-sys/wrapper.h
@@ -0,0 +1,1 @@
+#include <c_api.h>
