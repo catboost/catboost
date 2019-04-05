4dd01837e8b56eb0925eca6021ec098a6959fbe3
deshevoy <deshevoy@yandex-team.ru>
2019-04-05 18:56:32 +0300

    [openssl] Fix msan warning in aesni_cbc_cipher
    ([arc::pullid] fc768489-e087a3e-6f2ff9af-f59bf75f)
    
    ref:1d3e21c0546340417a8b3127675aa3e21a36322d

--- contrib/libs/openssl/1.1.1/crypto/evp/e_aes.c
+++ contrib/libs/openssl/1.1.1/crypto/evp/e_aes.c
@@ -305,6 +305,7 @@ static int aesni_cbc_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out,
     aesni_cbc_encrypt(in, out, len, &EVP_C_DATA(EVP_AES_KEY,ctx)->ks.ks,
                       EVP_CIPHER_CTX_iv_noconst(ctx),
                       EVP_CIPHER_CTX_encrypting(ctx));
+    __msan_unpoison(out, len);
 
     return 1;
 }
