d2a86ee3f40a0dd061527ab401ca18d54d8ea477
thegeorg <thegeorg@yandex-team.ru>
2019-04-11 19:38:54 +0300

    util: Get rid of TDataRegion typedef
    ([arc::pullid] ff9a85a4-81280d2-94302932-1a8886e1)
    
    ref:b5f9b2ede5c9d4e9ded658c20d6f89995cd73513

--- util/generic/region.h
+++ util/generic/region.h
@@ -2,10 +2,3 @@
 
 #include "array_ref.h"
 
-/**
- * These are legacy typedefs and methods. They should be removed.
- *
- * DEPRECATED. DO NOT USE.
- */
-using TDataRegion = TArrayRef<const char>;
-
