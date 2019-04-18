5e4dca76edb8f387b6bb285a8d44fba4fa4b020a
Stanislav Kirillov <staskirillov@gmail.com>
2019-04-18 17:50:42 +0300

    fix path in sed
    Note: mandatory check (NEED_CHECK) was skipped
    ref:f2f645481fda0d8f462068fca173c91e439bf622

--- ci/fixups_for_freebsd.sh
+++ ci/fixups_for_freebsd.sh
@@ -7,7 +7,7 @@ sed -i -e 's/x86_64\-linux\-gnu/x86_64\-unknown\-freebsd11\.2/' make/*.makefile
 # use clang 6.0, not 5.0
 sed -i -e "s/\'5 0\'/\'6 0\'/" make/*.makefile
 # endian.h => sys/endian.h
-sed -i -e "s/endian.h/sys\/endian.h/" catboost/contrib/python/numpy/numpy/core/include/numpy/npy_endian.h
+sed -i -e "s/<endian.h>/<sys\/endian.h>/" contrib/python/numpy/numpy/core/include/numpy/npy_endian.h
 # don't build AFALG on freebsd
 echo '#define OPENSSL_NO_AFALGENG' >> contrib/libs/openssl/1.1.1/include/openssl/opensslconf.h
 # hack
