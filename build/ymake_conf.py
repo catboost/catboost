1623298f123bfdc375919910ea39721878d8f4f9
spreis <spreis@yandex-team.ru>
2019-04-23 22:33:22 +0300

    Prepare to enable sse4.1,sse4.2,popcnt by default + inline some cpuid lookups
    SSE4 is not enabled by default here: this is preliminary refactoring of SSE support and introduction of all needed features. SSE4 will be enabled by default later, after thorough testing.
    - based on https://a.yandex-team.ru/review/684543/details
    - Moved all SSE handling from ymake.conf.py to ymake.core.conf
    - Added SSE4.2 and popcnt
    - Fixed some issues in Arcadia:
    - Incorrect SSE4 defines for Windows
    - YT's incorrect assumption that PCLMUL is SSE4.2: added separate feature for PCLMUL disabled by default (since it was introduced later than SSE4.2)
    - Register allocation with SSE on 32bit platforms in contrib/libs/crcutil
    - Proper Windows support in dict/mt/libs/nn/nn_util
    
    IGNIETFERRO-1080
    
    ref:254503db6759be8a38893a4b5c51c3915ccd8d15

--- build/ymake_conf.py
+++ build/ymake_conf.py
@@ -1149,23 +1149,6 @@ class GnuCompiler(Compiler):
         if self.target.is_x86_64:
             self.c_flags.append('-m64')
 
-        enable_sse = self.target.is_intel
-        if self.target.is_ios:
-            # TODO(somov): Расследовать.
-            # contrib/libs/crcutil не собирается под clang*-ios-i386 со включенным SSE.
-            # multiword_64_64_gcc_i386_mmx.cc:98:5: error: inline assembly requires more registers than available
-            enable_sse = False
-
-        if enable_sse:
-            # TODO(somov): Удалить define-ы и сборочный флаг
-            gcc_sse_opts = {'-msse': '-DSSE_ENABLED=1', '-msse2': '-DSSE2_ENABLED=1', '-msse3': '-DSSE3_ENABLED=1', '-mssse3': '-DSSSE3_ENABLED=1'}
-            if not is_positive('NOSSE'):
-                for opt, define in gcc_sse_opts.iteritems():
-                    self.c_flags.append(opt)
-                    self.c_defines.append(define)
-            else:
-                self.c_flags.append('-no-sse')
-
         self.debug_info_flags = ['-g']
         if self.target.is_linux:
             self.debug_info_flags.append('-ggnu-pubnames')
@@ -1823,10 +1806,6 @@ class MSVCCompiler(MSVC, Compiler):
             '__STDC_CONSTANT_MACROS',
             '__STDC_FORMAT_MACROS',
             '_USING_V110_SDK71_',
-            'SSE_ENABLED=1',
-            'SSE2_ENABLED=1',
-            'SSE3_ENABLED=1',
-            'SSSE3_ENABLED=1',
             '_LIBCPP_ENABLE_CXX17_REMOVED_FEATURES',
         ]
 
