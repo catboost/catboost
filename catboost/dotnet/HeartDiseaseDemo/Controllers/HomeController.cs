83ae3d48498ac1e0b3b95e451c42068563e76187
Konstantin Sidorov <kost2906@yandex.ru>
2019-05-04 23:22:46 +0400

    Adding .nupkg build + scaffold of the demo

---
+++ catboost/dotnet/HeartDiseaseDemo/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HeartDiseaseDemo.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace HeartDiseaseDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index([FromQuery]PatientModel model)
+        {
+            ViewBag.Sex = model.IsEmpty ? PatientModel.Gender.Male : model.Sex;
+            ViewBag.ChestPain = model.IsEmpty ? PatientModel.ChestPainType.Type1 : model.ChestPain;
+            ViewBag.ECGResults = model.IsEmpty ? PatientModel.ECGResultType.STTWaveAbnormality : model.ECGResults;
+            ViewBag.SlopeST = model.IsEmpty ? PatientModel.SlopeType.Uprising : model.SlopeST;
+            ViewBag.Thal = model.IsEmpty ? PatientModel.ThalType.Normal : model.Thal;
+            ViewBag.FlurMajorVessels = model.IsEmpty ? 0 : model.FlurMajorVessels;
+
+            ViewBag.Exang = model.IsEmpty ? false : model.Exang;
+            ViewBag.HighBloodSugar = model.IsEmpty ? false : model.HighBloodSugar;
+
+            if (!model.IsEmpty)
+            {
+                if (!ModelState.IsValid)
+                {
+                    ViewBag.Output = null;
+                }
+                else
+                {
+                    ViewBag.Output = 2.15d;
+                }
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ catboost/dotnet/HeartDiseaseDemo/Models/PatientModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeartDiseaseDemo.Models
+{
+    public class PatientModel
+    {
+        public enum Gender
+        {
+            Male = 1,
+            Female = 0
+        }
+
+        public enum ChestPainType
+        {
+            Type1, Type2, Type3, Type4
+        }
+
+        public enum ECGResultType
+        {
+            Normal, STTWaveAbnormality, LeftVentricularHypertrophy
+        }
+
+        public enum SlopeType
+        {
+            Uprising = 1, Flat, Downrising
+        }
+
+        public enum ThalType
+        {
+            Normal = 3, FixedDefect = 6, ReversableDefect = 7
+        }
+
+        public int Age { get; set; }
+
+        public Gender Sex { get; set; }
+
+        [DisplayName("Chest pain type")]
+        public ChestPainType ChestPain { get; set; }
+
+        [DisplayName("Resting blood pressure (mm Hg)")]
+        public int BloodPressure { get; set; }
+
+        [DisplayName("Serum cholestoral (mg/dl)")]
+        public int Cholesterol { get; set; }
+
+        [DisplayName("High blood sugar?")]
+        public bool HighBloodSugar { get; set; }
+
+        [DisplayName("ECG results")]
+        public ECGResultType ECGResults { get; set; }
+
+        [DisplayName("Max heart rate")]
+        public int MaxHeartRate { get; set; }
+
+        [DisplayName("Exerecise induced angina")]
+        public bool Exang { get; set; }
+
+        [DisplayName("ST depression")]
+        public int OldPeak { get; set; }
+
+        [DisplayName("The slope of the peak exercise ST segment")]
+        public SlopeType SlopeST { get; set; }
+
+        [DisplayName("# major vessels colored by flourosopy")]
+        public int FlurMajorVessels { get; set; }
+
+        [DisplayName("Thal type")]
+        public ThalType Thal { get; set; }
+
+        public double? Target { get; set; } = null;
+        public bool IsEmpty { get; set; } = true;
+    }
+}
