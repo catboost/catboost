96d9d73c5fab1bf1b4d279cfac8555c738be59c4
Dmitry Baksheev <dbakshee@yandex.ru>
2019-05-16 15:44:45 +0300

    Print learning rate to stdout instead of log. MLTOOLS-2491
    ref:fcb79339f44ec02466e20a0e762d89bc7563b085

--- catboost/libs/train_lib/options_helper.cpp
+++ catboost/libs/train_lib/options_helper.cpp
@@ -133,7 +133,7 @@ static void UpdateLearningRate(ui32 learnObjectCount, bool useBestModel, NCatboo
         learningRate = Min(defaultLearningRate * customIterationConstant / defaultIterationConstant, 0.5);
         learningRate = Round(learningRate, /*precision=*/6);
 
-        CATBOOST_NOTICE_LOG << "Learning rate set to " << learningRate << Endl;
+        Cout << "Learning rate set to " << learningRate << Endl;
     }
 }
 
