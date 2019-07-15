c6ef79abbaf31448620a3a0a5ba3b671bc54e181
yazevnul <yazevnul@yandex-team.ru>
2019-07-16 00:57:59 +0300

    [util/generic] fix typos from r5310929
    ref:6cf3bf00e44126df69cc967b8b86eef5b929795c

--- util/generic/scope.h
+++ util/generic/scope.h
@@ -44,16 +44,16 @@ namespace NPrivate {
 // function upon return from the current scope.
 //
 // @note expects `body` to provide no-throw guarantee, otherwise behaviour is unspecified (e.g
-// exception in d-tor may happen with typically results in a sigfault).
+// exception in d-tor may happen which typically results in a sigfault).
 // @see http://drdobbs.com/184403758 for detailed motivation.
 #define Y_SCOPE_EXIT(...) const auto Y_GENERATE_UNIQUE_ID(scopeGuard) Y_DECLARE_UNUSED = ::NPrivate::TMakeGuardHelper{} | [__VA_ARGS__]() mutable -> void
 
 // \brief `Y_DEFER { body };`
 //
-// Same as `Y_SCOPE_EXIT` but doesn't requires user to provide caputure-list explicitly (it
+// Same as `Y_SCOPE_EXIT` but doesn't require user to provide capture-list explicitly (it
 // implicitly uses `[u]` capture). Have same requirements for `body`.
 //
-// Insipired by `defer` statement in languages like Swift and Go.
+// Inspired by `defer` statement in languages like Swift and Go.
 //
 // \code
 // auto item = s.pop();
