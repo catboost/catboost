1d9fd57cff45e2e5e5823ce4ff92023cc566a83e
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2019-08-01 19:00:36 +0300

    Solution files and make files updated

--- catboost/dotnet/HeartDiseaseDemo/Startup.cs
+++ catboost/dotnet/HeartDiseaseDemo/Startup.cs
@@ -32,7 +32,7 @@ namespace HeartDiseaseDemo
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
