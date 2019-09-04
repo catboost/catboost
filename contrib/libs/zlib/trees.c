dedb354aad5ba45d8b4155124cd4cdfa42333b3b
Arseny Smalyuk <smalukav@yandex.ru>
2019-09-04 15:22:11 +0300

    KIKIMR-7846: Don't call memcpy with src==nullptr, n==0
    Note: mandatory check (NEED_CHECK) was skipped
    ref:0f730c1c84e97d76f634f2c1fa76ae2246ed544d

--- contrib/libs/zlib/trees.c
+++ contrib/libs/zlib/trees.c
@@ -870,8 +870,10 @@ void ZLIB_INTERNAL _tr_stored_block(s, buf, stored_len, last)
     bi_windup(s);        /* align on byte boundary */
     put_short(s, (ush)stored_len);
     put_short(s, (ush)~stored_len);
-    zmemcpy(s->pending_buf + s->pending, (Bytef *)buf, stored_len);
-    s->pending += stored_len;
+    if (stored_len) {
+        zmemcpy(s->pending_buf + s->pending, (Bytef *)buf, stored_len);
+        s->pending += stored_len;
+    }
 #ifdef ZLIB_DEBUG
     s->compressed_len = (s->compressed_len + 3 + 7) & (ulg)~7L;
     s->compressed_len += (stored_len + 4) << 3;
