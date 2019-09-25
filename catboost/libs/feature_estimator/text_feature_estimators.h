34ea64bbd6bbf1ddfaa0883cab16ab556ef49b4b
Dmitriy Kruchinin <kruch.dmitriy@gmail.com>
2019-09-25 16:38:50 +0300

    Change style in text_feature_estimators.h
    ref:da5ea12b62126caaef659115e0003e47cb5300d6

--- catboost/libs/feature_estimator/text_feature_estimators.h
+++ catboost/libs/feature_estimator/text_feature_estimators.h
@@ -6,18 +6,20 @@
 #include <catboost/libs/text_processing/embedding.h>
 #include <catboost/libs/text_processing/text_dataset.h>
 
-
 namespace NCB {
-
-    TVector<TOnlineFeatureEstimatorPtr> CreateEstimators(TConstArrayRef<EFeatureCalcerType> types,
-                                                         TEmbeddingPtr embedding,
-                                                         TTextClassificationTargetPtr target,
-                                                         TTextDataSetPtr learnTexts,
-                                                         TArrayRef<TTextDataSetPtr> testText);
+    TVector<TOnlineFeatureEstimatorPtr> CreateEstimators(
+        TConstArrayRef<EFeatureCalcerType> types,
+        TEmbeddingPtr embedding,
+        TTextClassificationTargetPtr target,
+        TTextDataSetPtr learnTexts,
+        TArrayRef<TTextDataSetPtr> testText
+    );
 
     //offline
-    TVector<TFeatureEstimatorPtr> CreateEstimators(TConstArrayRef<EFeatureCalcerType> types,
-                                                   TEmbeddingPtr embedding,
-                                                   TTextDataSetPtr learnTexts,
-                                                   TArrayRef<TTextDataSetPtr> testText);
+    TVector<TFeatureEstimatorPtr> CreateEstimators(
+        TConstArrayRef<EFeatureCalcerType> types,
+        TEmbeddingPtr embedding,
+        TTextDataSetPtr learnTexts,
+        TArrayRef<TTextDataSetPtr> testText
+    );
 }
