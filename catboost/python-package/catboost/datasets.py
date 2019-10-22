4e3baf4b3e982563d1589b5b1ffe030a7b454bb8
ilyzhin <ilyzhin@yandex-team.ru>
2019-10-22 18:10:35 +0300

    add higgs to datasets
    ref:f2d0d7eaed1f2c0498082049d9e1b9b4d18cb76e

--- catboost/python-package/catboost/datasets.py
+++ catboost/python-package/catboost/datasets.py
@@ -294,3 +294,22 @@ def adult():
     test_df = test_df.astype(dtype)
 
     return train_df, test_df
+
+
+def higgs():
+    """
+    Download "higgs" [1] data set.
+
+    Will return two pandas.DataFrame-s, first with train part and second with
+    test part of the dataset. Object class will be located in the first
+    column of dataset.
+
+    [1]: https://archive.ics.uci.edu/ml/datasets/HIGGS
+    """
+    url = 'https://storage.mds.yandex.net/get-devtools-opensource/250854/higgs.tar.gz'
+    md5 = 'ad59ba8328a9afa3837d7bf1a0e10e7b'
+    dataset_name, train_file, test_file = 'higgs', 'train.tsv', 'test.tsv'
+    train_path, test_path = _download_dataset(url, md5, dataset_name, train_file, test_file, cache=True)
+    return (
+        _load_numeric_only_dataset(train_path, 10500000, 29, sep='\t'),
+        _load_numeric_only_dataset(test_path, 500000, 29, sep='\t'))
