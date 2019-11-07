abad980c0736a3a9197ae33bb43814c66b225a51
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2019-11-07 04:01:40 +0300

    intermediate changes ref:acd5bf9651c43713d7f139a6805c5cd1dd77dbb5

--- contrib/python/pytz/pytz/__init__.py
+++ contrib/python/pytz/pytz/__init__.py
@@ -22,8 +22,8 @@ from pytz.tzfile import build_tzinfo
 
 
 # The IANA (nee Olson) database is updated several times a year.
-OLSON_VERSION = '2019b'
-VERSION = '2019.2'  # pip compatible version number.
+OLSON_VERSION = '2019c'
+VERSION = '2019.3'  # pip compatible version number.
 __version__ = VERSION
 
 OLSEN_VERSION = OLSON_VERSION  # Old releases had this misspelling
