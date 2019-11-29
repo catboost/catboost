ae7789d20cd48aefa09fac3ad3460651da8979cf
heretic <heretic@yandex-team.ru>
2019-11-29 22:48:40 +0300

    openssl 4 android
    Note: mandatory check (NEED_CHECK) was skipped
    ref:b53cb661712eba43312128b633af3f79ebacdb14

--- contrib/libs/openssl/include/openssl/opensslconf.h
+++ contrib/libs/openssl/include/openssl/opensslconf.h
@@ -8,6 +8,14 @@
 #   include "opensslconf-ios-arm64.h"
 #elif defined(__APPLE__) && defined(__x86_64__)
 #   include "opensslconf-osx.h"
+#elif defined(__ANDROID__) && defined(__x86_64__)
+#   include "opensslconf-android_x86_64.h"
+#elif defined(__ANDROID__) && defined(__i686__)
+#   include "opensslconf-android_i686.h"
+#elif defined(__ANDROID__) && defined(__aarch64__)
+#   include "opensslconf-android_arm64.h"
+#elif defined(__ANDROID__) && defined(__arm__)
+#   include "opensslconf-android_arm.h"
 #elif defined(__linux__) && defined(__aarch64__)
 #   include "opensslconf-linux_aarch64.h"
 #elif defined(_MSC_VER) && defined(_M_X64)
