ad5adab706abf04512ce2d1c42f89bff737f7b0f
dvshkurko <dvshkurko@yandex-team.ru>
2019-12-17 12:45:50 +0300

    Generate VCS-info for fat objects.
    ref:2b572b125b0d07de616324902308f8d41b434aa2

--- build/ymake_conf.py
+++ build/ymake_conf.py
@@ -1765,9 +1765,9 @@ class LD(Linker):
         # "Fat Object" : pre-linked global objects and static library with all dependencies
 
         # TODO(somov): Проверить, не нужны ли здесь все остальные флаги компоновки (LDFLAGS и т. д.).
-        emit('LINK_FAT_OBJECT', '$GENERATE_MF &&',
+        emit('LINK_FAT_OBJECT', '$GENERATE_MF && $GENERATE_VCS_C_INFO_NODEP &&',
              '$YMAKE_PYTHON ${input:"build/scripts/link_fat_obj.py"} --obj=$TARGET --build-root $ARCADIA_BUILD_ROOT --lib=${output:REALPRJNAME.a}', arch_flag, '$LINK_FAT_OBJECT_EXTENDED_FLAGS',
-             '-Ya,input $AUTO_INPUT $VCS_C_OBJ_WRAP -Ya,global_srcs ${rootrel:SRCS_GLOBAL} -Ya,peers $PEERS',
+             '-Ya,input $AUTO_INPUT $VCS_C_OBJ -Ya,global_srcs ${rootrel:SRCS_GLOBAL} -Ya,peers $PEERS',
              '-Ya,linker $CXX_COMPILER $C_FLAGS_PLATFORM', self.ld_sdk, '-Ya,archiver', archiver,
              '$TOOLCHAIN_ENV ${kv;hide:"p LD"} ${kv;hide:"pc light-blue"} ${kv;hide:"show_out"}')
 
@@ -2288,7 +2288,7 @@ class MSVCLinker(MSVC, Linker):
             ${qe;rootrel:SRCS_GLOBAL} ${VCS_C_OBJ_RR} ${qe;rootrel:AUTO_INPUT} ${qe;rootrel:PEERS} \
             $LINK_EXE_FLAGS $LINK_STDLIBS $LDFLAGS $LDFLAGS_GLOBAL $OBJADDE ${hide;kv:"soe"} ${hide;kv:"p LD"} ${hide;kv:"pc blue"}
 
-            LINK_FAT_OBJECT=${GENERATE_MF} && $YMAKE_PYTHON ${input:"build/scripts/touch.py"} $TARGET ${kv;hide:"p LD"} ${kv;hide:"pc light-blue"} ${kv;hide:"show_out"}''')
+            LINK_FAT_OBJECT=${GENERATE_MF} && $GENERATE_VCS_C_INFO_NODEP && $YMAKE_PYTHON ${input:"build/scripts/touch.py"} $TARGET ${kv;hide:"p LD"} ${kv;hide:"pc light-blue"} ${kv;hide:"show_out"}''')
 
 
 # TODO(somov): Rename!
