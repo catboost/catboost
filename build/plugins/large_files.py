ed30f7ed127b1e0302a708ef551d9f4129bb164f
spreis <spreis@yandex-team.ru>
2020-01-13 09:15:33 +0300

    Implement LARGE_FILE macro which may work either with local  or fetch  using .external descriptor
    + Implement FROM_ARCHIVE() macro wich is similar to FROM_SANDBOX but doesn't do any fetching
    
    ref:e715f8a12f618f305bfdf2486870dc3c73ff5843

---
+++ build/plugins/large_files.py
@@ -0,0 +1,27 @@
+import os
+import ymake
+from _common import strip_roots
+
+PLACEHOLDER_EXT = "external"
+
+
+def onlarge_files(unit, *args):
+    """
+        @usage LARGE_FILES(Files...)
+
+        Use alrge file ether from working copy or from remote storage via placeholder <File>.remote
+        If <File> is presemt locally (and not a symlink!) it will be copied to build directory.
+        Otherwise macro will try to locate <File>.remote, parse it retrieve ot during build phase.
+    """
+    args = list(args)
+    for arg in args:
+        src = unit.resolve_arc_path(arg)
+        if src.startswith("$S"):
+            msg = "Used local large file {}. Don't forget to run 'ya upload --update-external' and commit {}.{}".format(src, src, PLACEHOLDER_EXT)
+            unit.message(["warn", msg])
+            unit.oncopy_file([arg, arg])
+        else:
+            out_file = strip_roots(os.path.join(unit.path(), arg))
+            external = "{}.{}".format(arg, PLACEHOLDER_EXT)
+            unit.on_from_external([external, out_file, 'OUT_NOAUTO', arg])
+
