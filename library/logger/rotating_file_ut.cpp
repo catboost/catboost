f3a3fdb176393805a0d6fefa46ecd3e63699a6fb
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2020-02-03 20:38:42 +0300

    intermediate changes ref:226df2a447d98d6a9bec9e4ab69d3318920ae466

--- library/logger/rotating_file_ut.cpp
+++ library/logger/rotating_file_ut.cpp
@@ -8,7 +8,7 @@
 #include <library/unittest/registar.h>
 #include <library/unittest/tests_data.h>
 
-Y_UNIT_TEST_SUITE(RotatingFileSuite) {
+Y_UNIT_TEST_SUITE(OldRotatingFileSuite) {
     const TString PRE_ROTATION_PATH = GetWorkPath() + "/new.log";
     const TString POST_ROTATION_PATH = GetWorkPath() + "/old.log";
 
@@ -47,5 +47,53 @@ Y_UNIT_TEST_SUITE(RotatingFileSuite) {
         UNIT_ASSERT_C(TFileStat(PRE_ROTATION_PATH).Size > 0, "file " << PRE_ROTATION_PATH << " has zero size");
         UNIT_ASSERT_C(TFileStat(POST_ROTATION_PATH).Size > 0, "file " << POST_ROTATION_PATH << " has zero size");
         UNIT_ASSERT_C(TFileStat(PRE_ROTATION_PATH).Size > maxSize, "size of file " << PRE_ROTATION_PATH << " is lesser than was written");
+    }
+}
+
+Y_UNIT_TEST_SUITE(NewRotatingFileSuite) {
+    const TString PATH = GetWorkPath() + "/my.log";
+
+    Y_UNIT_TEST(TestFileWrite) {
+        TRotatingFileLogBackend log(PATH, 4000, 2);
+        TString data = "my data";
+        log.WriteData(TLogRecord(ELogPriority::TLOG_INFO, data.data(), data.size()));
+        UNIT_ASSERT_C(TFileStat(PATH).Size > 0, "file " << PATH << " has zero size");
+    }
+
+    Y_UNIT_TEST(TestFileRotate) {
+        const ui64 maxSize = 40;
+        TRotatingFileLogBackend log(PATH, maxSize, 2);
+        TStringBuilder data;
+        for (size_t i = 0; i < 10; ++i)
+            data << "data\n";
+        log.WriteData(TLogRecord(ELogPriority::TLOG_INFO, data.data(), data.size()));
+        UNIT_ASSERT_C(TFileStat(PATH).Size > 0, "file " << PATH << " has zero size");
+        data.clear();
+        data << "more data";
+        log.WriteData(TLogRecord(ELogPriority::TLOG_INFO, data.data(), data.size()));
+        UNIT_ASSERT_C(TFileStat(PATH).Size > 0, "file " << PATH << " has zero size");
+        UNIT_ASSERT_C(TFileStat(TStringBuilder{} << PATH << ".1").Size > 0, "file " << PATH << ".1 has zero size");
+        UNIT_ASSERT_C(TFileStat(PATH).Size < maxSize, "size of file " << PATH << " is greater than the size limit of " << maxSize << " bytes");
+    }
+
+    Y_UNIT_TEST(TestDoubleFileRotate) {
+        const ui64 maxSize = 40;
+        TRotatingFileLogBackend log(PATH, maxSize, 2);
+        TStringBuilder data;
+        for (size_t i = 0; i < 10; ++i)
+            data << "data\n";
+            log.WriteData(TLogRecord(ELogPriority::TLOG_INFO, data.data(), data.size()));
+            UNIT_ASSERT_C(TFileStat(PATH).Size > 0, "file " << PATH << " has zero size");
+            log.WriteData(TLogRecord(ELogPriority::TLOG_INFO, data.data(), data.size()));
+            UNIT_ASSERT_C(TFileStat(PATH).Size > 0, "file " << PATH << " has zero size");
+            UNIT_ASSERT_C(TFileStat(TStringBuilder{} << PATH << ".1").Size > 0, "file " << PATH << ".1 has zero size");
+            UNIT_ASSERT_C(TFileStat(PATH).Size > maxSize, "size of file " << PATH << " is lesser than was written");
+            data.clear();
+            data << "more data";
+            log.WriteData(TLogRecord(ELogPriority::TLOG_INFO, data.data(), data.size()));
+            UNIT_ASSERT_C(TFileStat(PATH).Size > 0, "file " << PATH << " has zero size");
+            UNIT_ASSERT_C(TFileStat(TStringBuilder{} << PATH << ".1").Size > 0, "file " << PATH << ".1 has zero size");
+            UNIT_ASSERT_C(TFileStat(TStringBuilder{} << PATH << ".2").Size > 0, "file " << PATH << ".2 has zero size");
+            UNIT_ASSERT_C(TFileStat(PATH).Size < maxSize, "size of file " << PATH << " is greater than the size limit of " << maxSize << " bytes");
     }
 }
