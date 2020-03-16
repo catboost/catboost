77d7d239d356e07cf1b755a0b66a8aa459a2cad2
svidyuk <svidyuk@yandex-team.ru>
2020-03-16 10:19:16 +0300

    Add Java dart variable for transitive closure of managed peers
    ref:10b75d37708882b6416f9ce3f08ed53b8ef417b9

--- build/plugins/java.py
+++ build/plugins/java.py
@@ -78,6 +78,7 @@ def onjava_module(unit, *args):
         'MODULE_ARGS': unit.get('MODULE_ARGS'),
         'PEERDIR': unit.get_module_dirs('PEERDIRS'),
         'MANAGED_PEERS': '${MANAGED_PEERS}',
+        'MANAGED_PEERS_CLOSURE': '${MANAGED_PEERS_CLOSURE}',
         'EXCLUDE': extract_macro_calls(unit, 'EXCLUDE_VALUE', args_delim),
         'JAVA_SRCS': extract_macro_calls(unit, 'JAVA_SRCS_VALUE', args_delim),
         'JAVAC_FLAGS': extract_macro_calls(unit, 'JAVAC_FLAGS_VALUE', args_delim),
