[catboost] canonize test_prediction_diff gpu and fix precision
ref:a44d946acb7a73b6c9ebc457940fd3fc6fc48002

catboost/python-package/ut/medium/test.py
@@ -3270,10 +3270,10 @@ def test_prediction_diff_feature_importance(task_type):
     model = CatBoostClassifier(iterations=110, task_type=task_type, learning_rate=0.03, max_ctr_complexity=1, devices='0')
     model.fit(pool)
     fimp_npy_path = test_output_path(FIMP_NPY_PATH)
-    np.save(fimp_npy_path, np.array(model.get_feature_importance(
+    np.save(fimp_npy_path, np.around(np.array(model.get_feature_importance(
         type=EFstrType.PredictionDiff,
         data=pool.get_features()[:2]
-    )))
+    )), 9))
     return local_canonical_file(fimp_npy_path)
 
 
@@ -3284,10 +3284,10 @@ def test_prediction_diff_nonsym_feature_importance(task_type, grow_policy):
     model = CatBoostClassifier(iterations=110, task_type=task_type, grow_policy=grow_policy, learning_rate=0.03, max_ctr_complexity=1, devices='0')
     model.fit(pool)
     fimp_txt_path = test_output_path(FIMP_TXT_PATH)
-    np.savetxt(fimp_txt_path, np.array(model.get_feature_importance(
+    np.savetxt(fimp_txt_path, np.around(np.array(model.get_feature_importance(
         type=EFstrType.PredictionDiff,
         data=pool.get_features()[:2]
-    )))
+    )), 9))
     return local_canonical_file(fimp_txt_path)
 
 