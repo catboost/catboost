a8f1e16bf77c74b4d0a8fe808223659d580c126a
ilyzhin <ilyzhin@yandex-team.ru>
2020-05-14 13:58:11 +0300

    [catboost] refactoring
    ref:a5222f5ade30401d58a16ef79e2623ea8fe2591d

--- catboost/libs/data/model_dataset_compatibility.cpp
+++ catboost/libs/data/model_dataset_compatibility.cpp
@@ -27,6 +27,10 @@ static constexpr const char* FeatureTypeName() {
     }
 }
 
+static inline TString GetFeatureName(const TString& featureId, int featureIndex) {
+    return featureId == "" ? ToString(featureIndex) : featureId;
+}
+
 template <class TFeature>
 static void CheckFeatureTypes(
     TConstArrayRef<TFeature> features,
@@ -49,40 +53,54 @@ static void CheckFeatureTypes(
     }
 }
 
-static THashSet<ui32> GetFloatFeatureIndexes(
-    ui32 featureCount,
-    const THashSet<ui32>& catFeatureIndexes,
-    const THashSet<ui32>& textFeatureIndexes
+template <class TFeature>
+static void CheckFeatureTypesAndNames(
+    TConstArrayRef<TFeature> modelFeatures,
+    const THashSet<ui32>& datasetFeaturesIndices,
+    TConstArrayRef<NCB::TFeatureMetaInfo> datasetFeaturesMetaInfo,
+    THashMap<ui32, ui32>* columnIndexesReorderMap
 ) {
-    THashSet<ui32> allIndexes = xrange(featureCount);
-    THashSet<ui32> floatFeatureIndexes;
-    SetDifference(
-        allIndexes.begin(),
-        allIndexes.end(),
-        catFeatureIndexes.begin(),
-        catFeatureIndexes.end(),
-        std::inserter(floatFeatureIndexes, floatFeatureIndexes.begin())
-    );
-    allIndexes = floatFeatureIndexes;
-    SetDifference(
-        allIndexes.begin(),
-        allIndexes.end(),
-        textFeatureIndexes.begin(),
-        textFeatureIndexes.end(),
-        std::inserter(floatFeatureIndexes, floatFeatureIndexes.begin())
-    );
-    return floatFeatureIndexes;
+    for (const TFeature& feature : modelFeatures) {
+        if (!feature.UsedInModel()) {
+            continue;
+        }
+        TString featureModelName = GetFeatureName(feature.FeatureId, feature.Position.FlatIndex);
+        CB_ENSURE(
+            SafeIntegerCast<size_t>(feature.Position.FlatIndex) < datasetFeaturesMetaInfo.size(),
+            "Feature " << featureModelName << " is present in model but not in pool.");
+        if (SafeIntegerCast<size_t>(feature.Position.FlatIndex) < datasetFeaturesMetaInfo.size()
+                && feature.FeatureId != ""
+                && datasetFeaturesMetaInfo[feature.Position.FlatIndex].Name != "")
+        {
+            CB_ENSURE(
+                feature.FeatureId == datasetFeaturesMetaInfo[feature.Position.FlatIndex].Name,
+                "At position " << feature.Position.FlatIndex
+                << " should be feature with name " << feature.FeatureId
+                << " (found " << datasetFeaturesMetaInfo[feature.Position.FlatIndex].Name << ").");
+        }
+        TString featurePoolName;
+        if (SafeIntegerCast<size_t>(feature.Position.FlatIndex) < datasetFeaturesMetaInfo.size()) {
+            featurePoolName = GetFeatureName(
+                datasetFeaturesMetaInfo[feature.Position.FlatIndex].Name,
+                feature.Position.FlatIndex);
+        } else {
+            featurePoolName = GetFeatureName("", feature.Position.FlatIndex);
+        }
+        CB_ENSURE(
+            datasetFeaturesIndices.contains(feature.Position.FlatIndex),
+            "Feature " << featurePoolName << " from pool must be " << FeatureTypeName<TFeature>() << ".");
+
+        columnIndexesReorderMap->insert(
+            {feature.Position.FlatIndex, feature.Position.FlatIndex});
+    }
 }
 
 namespace NCB {
 
-    static inline TString GetFeatureName(const TString& featureId, int featureIndex) {
-        return featureId == "" ? ToString(featureIndex) : featureId;
-    }
-
     bool CheckColumnRemappingPossible(
         const TFullModel& model,
         const TFeaturesLayout& datasetFeaturesLayout,
+        const THashSet<ui32>& datasetFloatFeatureFlatIndexes,
         const THashSet<ui32>& datasetCatFeatureFlatIndexes,
         const THashSet<ui32>& datasetTextFeatureFlatIndexes,
         THashMap<ui32, ui32>* columnIndexesReorderMap)
@@ -119,19 +137,12 @@ namespace NCB {
             return false;
         }
 
-        {
-            const auto& datasetFloatFeatureIndexes = GetFloatFeatureIndexes(
-                datasetFeaturesLayout.GetExternalFeatureCount(),
-                datasetCatFeatureFlatIndexes,
-                datasetTextFeatureFlatIndexes
-            );
-            CheckFeatureTypes(
-                model.ModelTrees->GetFloatFeatures(),
-                datasetFeatureNamesMap,
-                datasetFloatFeatureIndexes,
-                columnIndexesReorderMap
-            );
-        }
+        CheckFeatureTypes(
+            model.ModelTrees->GetFloatFeatures(),
+            datasetFeatureNamesMap,
+            datasetFloatFeatureFlatIndexes,
+            columnIndexesReorderMap
+        );
         CheckFeatureTypes(
             model.ModelTrees->GetCatFeatures(),
             datasetFeatureNamesMap,
@@ -157,6 +168,13 @@ namespace NCB {
         }
         const auto& datasetFeaturesLayout = *objectsData.GetFeaturesLayout();
 
+        const auto datasetFloatFeatureInternalIdxToExternalIdx =
+            datasetFeaturesLayout.GetFloatFeatureInternalIdxToExternalIdx();
+
+        THashSet<ui32> datasetFloatFeatures(
+            datasetFloatFeatureInternalIdxToExternalIdx.begin(),
+            datasetFloatFeatureInternalIdxToExternalIdx.end());
+
         const auto datasetCatFeatureInternalIdxToExternalIdx =
             datasetFeaturesLayout.GetCatFeatureInternalIdxToExternalIdx();
 
@@ -174,6 +192,7 @@ namespace NCB {
         if (CheckColumnRemappingPossible(
             model,
             datasetFeaturesLayout,
+            datasetFloatFeatures,
             datasetCatFeatures,
             datasetTextFeatures,
             columnIndexesReorderMap))
@@ -185,111 +204,26 @@ namespace NCB {
 
         const auto& datasetFeaturesMetaInfo = datasetFeaturesLayout.GetExternalFeaturesMetaInfo();
 
-        for (const TCatFeature& catFeature : model.ModelTrees->GetCatFeatures()) {
-            if (!catFeature.UsedInModel()) {
-                continue;
-            }
-            TString featureModelName = GetFeatureName(catFeature.FeatureId, catFeature.Position.FlatIndex);
-            CB_ENSURE(
-                SafeIntegerCast<size_t>(catFeature.Position.FlatIndex) < datasetFeaturesMetaInfo.size(),
-                "Feature " << featureModelName << " is present in model but not in pool.");
-            if (SafeIntegerCast<size_t>(catFeature.Position.FlatIndex) < datasetFeaturesMetaInfo.size()
-                    && catFeature.FeatureId != ""
-                    && datasetFeaturesMetaInfo[catFeature.Position.FlatIndex].Name != "")
-            {
-                CB_ENSURE(
-                    catFeature.FeatureId == datasetFeaturesMetaInfo[catFeature.Position.FlatIndex].Name,
-                    "At position " << catFeature.Position.FlatIndex
-                    << " should be feature with name " << catFeature.FeatureId
-                    << " (found " << datasetFeaturesMetaInfo[catFeature.Position.FlatIndex].Name << ").");
-            }
-            TString featurePoolName;
-            if (SafeIntegerCast<size_t>(catFeature.Position.FlatIndex) < datasetFeaturesMetaInfo.size()) {
-                featurePoolName = GetFeatureName(
-                    datasetFeaturesMetaInfo[catFeature.Position.FlatIndex].Name,
-                    catFeature.Position.FlatIndex);
-            } else {
-                featurePoolName = GetFeatureName("", catFeature.Position.FlatIndex);
-            }
-            CB_ENSURE(
-                datasetCatFeatures.contains(catFeature.Position.FlatIndex),
-                "Feature " << featurePoolName << " from pool must be categorical.");
+        CheckFeatureTypesAndNames(
+            model.ModelTrees->GetFloatFeatures(),
+            datasetFloatFeatures,
+            datasetFeaturesMetaInfo,
+            columnIndexesReorderMap
+        );
 
-            columnIndexesReorderMap->insert(
-                {catFeature.Position.FlatIndex, catFeature.Position.FlatIndex});
-        }
+        CheckFeatureTypesAndNames(
+            model.ModelTrees->GetCatFeatures(),
+            datasetCatFeatures,
+            datasetFeaturesMetaInfo,
+            columnIndexesReorderMap
+        );
 
-        for (const TFloatFeature& floatFeature : model.ModelTrees->GetFloatFeatures()) {
-            if (!floatFeature.UsedInModel()) {
-                continue;
-            }
-            TString featureModelName = GetFeatureName(floatFeature.FeatureId, floatFeature.Position.FlatIndex);
-            CB_ENSURE(
-                SafeIntegerCast<size_t>(floatFeature.Position.FlatIndex) < datasetFeaturesMetaInfo.size(),
-                "Feature " << featureModelName << " is present in model but not in pool.");
-            if (SafeIntegerCast<size_t>(floatFeature.Position.FlatIndex) < datasetFeaturesMetaInfo.size()
-                    && floatFeature.FeatureId != ""
-                    && datasetFeaturesMetaInfo[floatFeature.Position.FlatIndex].Name != "")
-            {
-                CB_ENSURE(
-                    floatFeature.FeatureId == datasetFeaturesMetaInfo[floatFeature.Position.FlatIndex].Name,
-                    "At position " << floatFeature.Position.FlatIndex
-                    << " should be feature with name " << floatFeature.FeatureId
-                    << " (found " << datasetFeaturesMetaInfo[floatFeature.Position.FlatIndex].Name << ").");
-            }
-            TString featurePoolName;
-            if (SafeIntegerCast<size_t>(floatFeature.Position.FlatIndex) < datasetFeaturesMetaInfo.size()) {
-                featurePoolName = GetFeatureName(
-                    datasetFeaturesMetaInfo[floatFeature.Position.FlatIndex].Name,
-                    floatFeature.Position.FlatIndex);
-            } else {
-                featurePoolName = GetFeatureName("", floatFeature.Position.FlatIndex);
-            }
-            CB_ENSURE(
-                !datasetCatFeatures.contains(floatFeature.Position.FlatIndex),
-                "Feature " << featurePoolName << " from pool must not be categorical.");
-            CB_ENSURE(
-                !datasetTextFeatures.contains(floatFeature.Position.FlatIndex),
-                "Feature " << featurePoolName << " from pool must not be text.");
-
-            columnIndexesReorderMap->insert(
-                {floatFeature.Position.FlatIndex, floatFeature.Position.FlatIndex});
-        }
-
-        // TODO(d-kruchinin, akhropov): refactor this - remove duplicates, create generic solution
-        for (const TTextFeature& textFeature : model.ModelTrees->GetTextFeatures()) {
-            if (!textFeature.UsedInModel()) {
-                continue;
-            }
-            TString featureModelName = GetFeatureName(textFeature.FeatureId, textFeature.Position.FlatIndex);
-            CB_ENSURE(
-                SafeIntegerCast<size_t>(textFeature.Position.FlatIndex) < datasetFeaturesMetaInfo.size(),
-                "Feature " << featureModelName << " is present in model but not in pool.");
-            if (SafeIntegerCast<size_t>(textFeature.Position.FlatIndex) < datasetFeaturesMetaInfo.size()
-                && textFeature.FeatureId != ""
-                && datasetFeaturesMetaInfo[textFeature.Position.FlatIndex].Name != "")
-            {
-                CB_ENSURE(
-                    textFeature.FeatureId == datasetFeaturesMetaInfo[textFeature.Position.FlatIndex].Name,
-                    "At position " << textFeature.Position.FlatIndex
-                    << " should be feature with name " << textFeature.FeatureId
-                    << " (found " << datasetFeaturesMetaInfo[textFeature.Position.FlatIndex].Name << ").");
-            }
-            TString featurePoolName;
-            if (SafeIntegerCast<size_t>(textFeature.Position.FlatIndex) < datasetFeaturesMetaInfo.size()) {
-                featurePoolName = GetFeatureName(
-                    datasetFeaturesMetaInfo[textFeature.Position.FlatIndex].Name,
-                    textFeature.Position.FlatIndex);
-            } else {
-                featurePoolName = GetFeatureName("", textFeature.Position.FlatIndex);
-            }
-            CB_ENSURE(
-                datasetTextFeatures.contains(textFeature.Position.FlatIndex),
-                "Feature " << featurePoolName << " from pool must be text.");
-
-            columnIndexesReorderMap->insert(
-                {textFeature.Position.FlatIndex, textFeature.Position.FlatIndex});
-        }
+        CheckFeatureTypesAndNames(
+            model.ModelTrees->GetTextFeatures(),
+            datasetTextFeatures,
+            datasetFeaturesMetaInfo,
+            columnIndexesReorderMap
+        );
     }
 
     void CheckModelAndDatasetCompatibility(
