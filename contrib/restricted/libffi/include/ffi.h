f842924395a6d0e31c6bb0c6bb6bfd452dea7028
Mikhail Borisov <borisov.mikhail@gmail.com>
2020-05-20 13:55:48 +0300

    CONTRIB-1869: Import libffi v3.3 via yamaker
    ref:38092315762a5dfaf361caf463493557de2900fb

--- contrib/restricted/libffi/include/ffi.h
+++
@@ -1,9 +0,0 @@
-#if defined(__arm64__)
-#include <ffi_arm64.h>
-#elif defined(__i386__)
-#include <ffi_i386.h>
-#elif defined(__arm__)
-#include <ffi_armv7.h>
-#else
-#include <ffi_x86_64.h>
-#endif
