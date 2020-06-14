bafbbf3af8afe1f552a75f470e181a0aca298f40
thegeorg <thegeorg@yandex-team.ru>
2020-06-14 13:47:58 +0300

    Unbundle strlcat from libmysql_r
    ref:a82075c66e85fd932fd50d78abd64fe65a25b69a

--- contrib/libs/libc_compat/string.h
+++ contrib/libs/libc_compat/string.h
@@ -2,5 +2,13 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 size_t strlcpy(char* dst, const char* src, size_t len);
 size_t strlcat(char* dst, const char* src, size_t len);
+
+#ifdef __cplusplus
+} //extern "C"
+#endif
