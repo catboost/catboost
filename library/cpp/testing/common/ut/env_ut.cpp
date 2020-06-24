f3cbf57114944fb19d8f4efe5bd1d35bed72ce13
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2020-06-24 18:52:41 +0300

    intermediate changes ref:74a09e31235983da708728124bbbb68c947044a1

--- library/cpp/testing/common/ut/env_ut.cpp
+++ library/cpp/testing/common/ut/env_ut.cpp
@@ -1,4 +1,5 @@
 #include <library/cpp/testing/common/env.h>
+#include <library/cpp/testing/common/scope.h>
 
 #include <util/folder/dirut.h>
 #include <util/system/env.h>
@@ -6,32 +7,14 @@
 
 #include <library/cpp/testing/gtest/gtest.h>
 
-namespace {
-    struct TEnvGuard {
-        TEnvGuard(const TString& name, const TString& value)
-            : Name_(name)
-            , Previous_(::GetEnv(name))
-        {
-            ::SetEnv(name, value);
-        }
-
-        ~TEnvGuard() {
-            ::SetEnv(Name_, Previous_);
-        }
-    private:
-        const TString Name_;
-        const TString Previous_;
-    };
-}
-
 TEST(Runtime, ArcadiaSourceRoot) {
     {
         auto tmpDir = ::GetSystemTempDir();
-        TEnvGuard guard("ARCADIA_SOURCE_ROOT", tmpDir);
+        NTesting::TScopedEnvironment guard("ARCADIA_SOURCE_ROOT", tmpDir);
         EXPECT_EQ(tmpDir, ArcadiaSourceRoot());
     }
     {
-        TEnvGuard guard("ARCADIA_SOURCE_ROOT", "");
+        NTesting::TScopedEnvironment guard("ARCADIA_SOURCE_ROOT", "");
         EXPECT_FALSE(ArcadiaSourceRoot().empty());
     }
 }
@@ -39,11 +22,11 @@ TEST(Runtime, ArcadiaSourceRoot) {
 TEST(Runtime, BuildRoot) {
     {
         auto tmpDir = ::GetSystemTempDir();
-        TEnvGuard guard("ARCADIA_BUILD_ROOT", tmpDir);
+        NTesting::TScopedEnvironment guard("ARCADIA_BUILD_ROOT", tmpDir);
         EXPECT_EQ(tmpDir, BuildRoot());
     }
     {
-        TEnvGuard guard("ARCADIA_BUILD_ROOT", "");
+        NTesting::TScopedEnvironment guard("ARCADIA_BUILD_ROOT", "");
         EXPECT_FALSE(BuildRoot().empty());
     }
 }
@@ -55,11 +38,11 @@ TEST(Runtime, BinaryPath) {
 TEST(Runtime, GetArcadiaTestsData) {
     {
         auto tmpDir = ::GetSystemTempDir();
-        TEnvGuard guard("ARCADIA_TESTS_DATA_DIR", tmpDir);
+        NTesting::TScopedEnvironment guard("ARCADIA_TESTS_DATA_DIR", tmpDir);
         EXPECT_EQ(tmpDir, GetArcadiaTestsData());
     }
     {
-        TEnvGuard guard("ARCADIA_TESTS_DATA_DIR", "");
+        NTesting::TScopedEnvironment guard("ARCADIA_TESTS_DATA_DIR", "");
         auto path = GetArcadiaTestsData();
         // it is not error if path is empty
         const bool ok = (path.empty() || GetBaseName(path) == "arcadia_tests_data");
@@ -70,11 +53,11 @@ TEST(Runtime, GetArcadiaTestsData) {
 TEST(Runtime, GetWorkPath) {
     {
         auto tmpDir = ::GetSystemTempDir();
-        TEnvGuard guard("TEST_WORK_PATH", tmpDir);
+        NTesting::TScopedEnvironment guard("TEST_WORK_PATH", tmpDir);
         EXPECT_EQ(tmpDir, GetWorkPath());
     }
     {
-        TEnvGuard guard("TEST_WORK_PATH", "");
+        NTesting::TScopedEnvironment guard("TEST_WORK_PATH", "");
         EXPECT_TRUE(!GetWorkPath().empty());
     }
 }
@@ -82,11 +65,11 @@ TEST(Runtime, GetWorkPath) {
 TEST(Runtime, GetYaPath) {
     {
         auto tmpDir = ::GetSystemTempDir();
-        TEnvGuard guard("YA_CACHE_DIR", tmpDir);
+        NTesting::TScopedEnvironment guard("YA_CACHE_DIR", tmpDir);
         EXPECT_EQ(tmpDir, GetYaPath().GetPath());
     }
     {
-        TEnvGuard guard("YA_CACHE_DIR", "");
+        NTesting::TScopedEnvironment guard("YA_CACHE_DIR", "");
         EXPECT_EQ(GetYaPath().Basename(), ".ya");
     }
 }
@@ -97,12 +80,12 @@ TEST(Runtime, GetOutputPath) {
 
 TEST(Runtime, GetRamDrivePath) {
     auto tmpDir = ::GetSystemTempDir();
-    TEnvGuard guard("YA_TEST_RAM_DRIVE_PATH", tmpDir);
+    NTesting::TScopedEnvironment guard("YA_TEST_RAM_DRIVE_PATH", tmpDir);
     EXPECT_EQ(tmpDir, GetRamDrivePath());
 }
 
 TEST(Runtime, GetOutputRamDrivePath) {
     auto tmpDir = ::GetSystemTempDir();
-    TEnvGuard guard("YA_TEST_OUTPUT_RAM_DRIVE_PATH", tmpDir);
+    NTesting::TScopedEnvironment guard("YA_TEST_OUTPUT_RAM_DRIVE_PATH", tmpDir);
     EXPECT_EQ(tmpDir, GetOutputRamDrivePath());
 }
--- library/cpp/testing/common/ya.make
+++ library/cpp/testing/common/ya.make
@@ -5,6 +5,7 @@ LIBRARY()
 SRCS(
     env.cpp
     env_tmpl.cpp.in
+    scope.cpp
 )
 
 END()
