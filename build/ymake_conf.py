2c3d0b959cc5ded1135616b2667d28da7ed7d70e
orivej <orivej@yandex-team.ru>
2020-07-17 06:37:41 +0300

    Support fuzzing, add generated arm_neon.h. DEVTOOLS-4987
    ref:c5dd0ac87a3b8a08b3264ab15d1836daac93bac3

--- build/ymake_conf.py
+++ build/ymake_conf.py
@@ -1505,6 +1505,7 @@ class GnuCompiler(Compiler):
         # fuzzing configuration
         if self.tc.is_clang and self.tc.version_at_least(5, 0):
             emit('LIBFUZZER_PATH',
+                 'contrib/libs/libfuzzer10' if self.tc.version_at_least(10) else
                  'contrib/libs/libfuzzer8' if self.tc.version_at_least(8) else
                  'contrib/libs/libfuzzer7' if self.tc.version_at_least(7) else
                  'contrib/libs/libfuzzer6' if self.tc.version_at_least(6) else
