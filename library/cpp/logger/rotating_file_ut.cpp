401841fd9f999ef761b601fffa34530c597f05bf
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2020-07-29 22:57:15 +0300

    intermediate changes ref:3fcedbaa26852e453bfb16601f241cf99beeea1a

--- library/cpp/logger/rotating_file_ut.cpp
+++ library/cpp/logger/rotating_file_ut.cpp
@@ -40,18 +40,18 @@ Y_UNIT_TEST_SUITE(NewRotatingFileSuite) {
         TStringBuilder data;
         for (size_t i = 0; i < 10; ++i)
             data << "data\n";
-            log.WriteData(TLogRecord(ELogPriority::TLOG_INFO, data.data(), data.size()));
-            UNIT_ASSERT_C(TFileStat(PATH).Size > 0, "file " << PATH << " has zero size");
-            log.WriteData(TLogRecord(ELogPriority::TLOG_INFO, data.data(), data.size()));
-            UNIT_ASSERT_C(TFileStat(PATH).Size > 0, "file " << PATH << " has zero size");
-            UNIT_ASSERT_C(TFileStat(TStringBuilder{} << PATH << ".1").Size > 0, "file " << PATH << ".1 has zero size");
-            UNIT_ASSERT_C(TFileStat(PATH).Size > maxSize, "size of file " << PATH << " is lesser than was written");
-            data.clear();
-            data << "more data";
-            log.WriteData(TLogRecord(ELogPriority::TLOG_INFO, data.data(), data.size()));
-            UNIT_ASSERT_C(TFileStat(PATH).Size > 0, "file " << PATH << " has zero size");
-            UNIT_ASSERT_C(TFileStat(TStringBuilder{} << PATH << ".1").Size > 0, "file " << PATH << ".1 has zero size");
-            UNIT_ASSERT_C(TFileStat(TStringBuilder{} << PATH << ".2").Size > 0, "file " << PATH << ".2 has zero size");
-            UNIT_ASSERT_C(TFileStat(PATH).Size < maxSize, "size of file " << PATH << " is greater than the size limit of " << maxSize << " bytes");
+        log.WriteData(TLogRecord(ELogPriority::TLOG_INFO, data.data(), data.size()));
+        UNIT_ASSERT_C(TFileStat(PATH).Size > 0, "file " << PATH << " has zero size");
+        log.WriteData(TLogRecord(ELogPriority::TLOG_INFO, data.data(), data.size()));
+        UNIT_ASSERT_C(TFileStat(PATH).Size > 0, "file " << PATH << " has zero size");
+        UNIT_ASSERT_C(TFileStat(TStringBuilder{} << PATH << ".1").Size > 0, "file " << PATH << ".1 has zero size");
+        UNIT_ASSERT_C(TFileStat(PATH).Size > maxSize, "size of file " << PATH << " is lesser than was written");
+        data.clear();
+        data << "more data";
+        log.WriteData(TLogRecord(ELogPriority::TLOG_INFO, data.data(), data.size()));
+        UNIT_ASSERT_C(TFileStat(PATH).Size > 0, "file " << PATH << " has zero size");
+        UNIT_ASSERT_C(TFileStat(TStringBuilder{} << PATH << ".1").Size > 0, "file " << PATH << ".1 has zero size");
+        UNIT_ASSERT_C(TFileStat(TStringBuilder{} << PATH << ".2").Size > 0, "file " << PATH << ".2 has zero size");
+        UNIT_ASSERT_C(TFileStat(PATH).Size < maxSize, "size of file " << PATH << " is greater than the size limit of " << maxSize << " bytes");
     }
 }
