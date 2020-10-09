c07f7ad6ac221b7d9e8b8949bc0938636e85de3b
thegeorg <thegeorg@yandex-team.ru>
2020-10-09 14:27:39 +0300

    Move TCppDemangler into NPrivate namespace
    ref:cd1f70a03491bb11016393f84e6208707b28443b

--- util/system/demangle.cpp
+++ util/system/demangle.cpp
@@ -1,4 +1,5 @@
 #include "platform.h"
+#include "demangle_impl.h"
 
 #ifdef __GNUC__
 #include <stdexcept>
@@ -7,7 +8,7 @@
 
 #include "demangle.h"
 
-const char* TCppDemangler::Demangle(const char* name) {
+const char* NPrivate::TCppDemangler::Demangle(const char* name) {
 #ifndef __GNUC__
     return name;
 #else
@@ -20,4 +21,8 @@ const char* TCppDemangler::Demangle(const char* name) {
 
     return TmpBuf_.Get();
 #endif
+}
+
+TString CppDemangle(const TString& name) {
+    return NPrivate::TCppDemangler().Demangle(name.data());
 }
