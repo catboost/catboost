4cd533491b29a2017225d3776f94d57a3f5aae24
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2020-10-22 19:21:09 +0300

    intermediate changes ref:a2b64b412e926e8f0e9ee0fa3f297eee7ec9280b

--- library/cpp/string_utils/base64/base64_ut.cpp
+++ library/cpp/string_utils/base64/base64_ut.cpp
@@ -16,6 +16,8 @@
 
 #include <array>
 
+using namespace std::string_view_literals;
+
 #define BASE64_UT_DECLARE_BASE64_IMPL(prefix, encFunction, decFunction)                                                        \
     Y_DECLARE_UNUSED                                                                                                           \
     static size_t prefix##Base64Decode(void* dst, const char* b, const char* e) {                                              \
@@ -296,7 +298,7 @@ Y_UNIT_TEST_SUITE(TBase64) {
     }
 
     Y_UNIT_TEST(TestAllPossibleOctets) {
-        const TString x(AsStringBuf("\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F"));
+        const TString x("\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F"sv);
         const TString xEnc = "AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0+P0BBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWltcXV5fYGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6e3x9fn8=";
         const TString y = Base64Decode(xEnc);
         const TString yEnc = Base64Encode(x);
@@ -305,7 +307,7 @@ Y_UNIT_TEST_SUITE(TBase64) {
     }
 
     Y_UNIT_TEST(TestTwoPaddingCharacters) {
-        const TString x(AsStringBuf("a"));
+        const TString x("a");
         const TString xEnc = "YQ==";
         const TString y = Base64Decode(xEnc);
         const TString yEnc = Base64Encode(x);
@@ -314,7 +316,7 @@ Y_UNIT_TEST_SUITE(TBase64) {
     }
 
     Y_UNIT_TEST(TestOnePaddingCharacter) {
-        const TString x(AsStringBuf("aa"));
+        const TString x("aa");
         const TString xEnc = "YWE=";
         const TString y = Base64Decode(xEnc);
         const TString yEnc = Base64Encode(x);
@@ -323,7 +325,7 @@ Y_UNIT_TEST_SUITE(TBase64) {
     }
 
     Y_UNIT_TEST(TestNoPaddingCharacters) {
-        const TString x(AsStringBuf("aaa"));
+        const TString x("aaa");
         const TString xEnc = "YWFh";
         const TString y = Base64Decode(xEnc);
         const TString yEnc = Base64Encode(x);
@@ -332,7 +334,7 @@ Y_UNIT_TEST_SUITE(TBase64) {
     }
 
     Y_UNIT_TEST(TestTrailingZero) {
-        const TString x(AsStringBuf("foo\0"));
+        const TString x("foo\0"sv);
         const TString xEnc = "Zm9vAA==";
         const TString y = Base64Decode(xEnc);
         const TString yEnc = Base64Encode(x);
@@ -341,7 +343,7 @@ Y_UNIT_TEST_SUITE(TBase64) {
     }
 
     Y_UNIT_TEST(TestTwoTrailingZeroes) {
-        const TString x(AsStringBuf("foo\0\0"));
+        const TString x("foo\0\0"sv);
         const TString xEnc = "Zm9vAAA=";
         const TString y = Base64Decode(xEnc);
         const TString yEnc = Base64Encode(x);
@@ -350,7 +352,7 @@ Y_UNIT_TEST_SUITE(TBase64) {
     }
 
     Y_UNIT_TEST(TestZero) {
-        const TString x(AsStringBuf("\0"));
+        const TString x("\0"sv);
         const TString xEnc = "AA==";
         const TString y = Base64Decode(xEnc);
         const TString yEnc = Base64Encode(x);
@@ -359,7 +361,7 @@ Y_UNIT_TEST_SUITE(TBase64) {
     }
 
     Y_UNIT_TEST(TestSymbolsAfterZero) {
-        const TString x(AsStringBuf("\0a"));
+        const TString x("\0a"sv);
         const TString xEnc = "AGE=";
         const TString y = Base64Decode(xEnc);
         const TString yEnc = Base64Encode(x);
