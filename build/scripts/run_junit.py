b4c1d4362761035088a5b56e8cdcc6f7432fd7d6
Aleksandr <ivansduck@gmail.com>
2020-11-12 23:51:24 +0300

    [ya test] Use jstack and SIGKILL to obtain stacktrace and stop timeouted junit test
    issue:DEVTOOLS-7636
    
    ref:e8fd00d08f98fb6e98229bd1b46a56a7f1eef29d

---
+++ build/scripts/run_junit.py
@@ -0,0 +1,55 @@
+import os
+import sys
+
+SHUTDOWN_SIGNAL = 'SIGUSR1'
+
+
+class SignalInterruptionError(Exception):
+    pass
+
+
+def on_shutdown(s, f):
+    raise SignalInterruptionError()
+
+
+def main():
+    args = sys.argv[1:]
+
+    def execve():
+        os.execve(args[0], args, os.environ)
+
+    jar_binary = args[args.index('--jar-binary') + 1]
+    java_bin_dir = os.path.dirname(jar_binary)
+    jstack_binary = os.path.join(java_bin_dir, 'jstack')
+
+    if not os.path.exists(jstack_binary):
+        sys.stderr.write("jstack is missing: {}\n".format(jstack_binary))
+        execve()
+
+    import signal
+
+    signum = getattr(signal, SHUTDOWN_SIGNAL, None)
+
+    if signum is None:
+        execve()
+
+    import subprocess
+
+    proc = subprocess.Popen(args)
+    signal.signal(signum, on_shutdown)
+
+    try:
+        proc.wait()
+    except SignalInterruptionError:
+        sys.stderr.write("\nGot {} signal: going to shutdown junit\n".format(signum))
+        # Dump stack traces
+        subprocess.check_call([jstack_binary, str(proc.pid)], stdout=sys.stderr)
+        # Kill junit - for more info see DEVTOOLS-7636
+        os.kill(proc.pid, signal.SIGKILL)
+        proc.wait()
+
+    return proc.returncode
+
+
+if __name__ == '__main__':
+    main()
