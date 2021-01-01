a692ba42e5c0f62e5da82b2f6fccfa77deb3419c
Dmitry Kozlyuk <PlushBeaver@users.noreply.github.com>
2021-01-02 01:40:39 +0300

    Fix warning in lfalloc
    Variable format string in fprintf() triggers -Wformat-security warning.
    Replace with fputs(), as there's not formatting involved.

--- library/cpp/lfalloc/lf_allocX64.h
+++ library/cpp/lfalloc/lf_allocX64.h
@@ -1670,7 +1670,7 @@ static void DebugTraceMMgr(const char* pszFormat, ...) // __cdecl
 #ifdef _win_
     OutputDebugStringA(buff);
 #else
-    fprintf(stderr, buff);
+    fputs(buff, stderr);
 #endif
 }
 
