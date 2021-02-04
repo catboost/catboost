24d57ced351359f47472f4bd33a995d88e6a89f9
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2021-02-04 23:31:46 +0300

    intermediate changes ref:e658fee09bb91f6fb9870bf7239b241498f2c424

--- library/cpp/neh/listen_ut.cpp
+++ library/cpp/neh/listen_ut.cpp
@@ -76,6 +76,10 @@ Y_UNIT_TEST_SUITE(THttpListen) {
                 : UnixSocketPath_(unixSocketPath)
             {}
 
+            ~TUnixSocketServer() {
+                unlink(UnixSocketPath_.Path.data());
+            }
+
             virtual void Process(void*) {
                 SOCKET socketFd = socket(AF_UNIX, SOCK_STREAM, 0);
                 DestructIfTrue(socketFd == -1, {socketFd}, "socket");
