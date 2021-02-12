6ec762af52287c220aafa99ed0fcbe71bab368da
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2021-02-12 03:40:33 +0300

    intermediate changes ref:4abf7a5262afa5fc745352709d345870f95827cc

--- library/cpp/testing/common/ut/env_ut.cpp
+++ library/cpp/testing/common/ut/env_ut.cpp
@@ -4,8 +4,11 @@
 #include <util/folder/dirut.h>
 #include <util/system/env.h>
 #include <util/system/execpath.h>
+#include <util/system/fs.h>
 
 #include <library/cpp/testing/gtest/gtest.h>
+
+#include <fstream>
 
 TEST(Runtime, ArcadiaSourceRoot) {
     NTesting::TScopedEnvironment contextGuard("YA_TEST_CONTEXT_FILE", ""); // remove context filename
@@ -96,4 +99,32 @@ TEST(Runtime, GetOutputRamDrivePath) {
     NTesting::TScopedEnvironment guard("YA_TEST_OUTPUT_RAM_DRIVE_PATH", tmpDir);
     Singleton<NPrivate::TTestEnv>()->ReInitialize();
     EXPECT_EQ(tmpDir, GetOutputRamDrivePath());
+}
+
+#ifdef _linux_
+TEST(Runtime, GdbPath) {
+    Singleton<NPrivate::TTestEnv>()->ReInitialize();
+    EXPECT_TRUE(NFs::Exists(::GdbPath()));
+}
+#endif
+
+TEST(Runtime, GetTestParam) {
+    auto tmpDir = ::GetSystemTempDir();
+    std::string newContextName = std::string(tmpDir) + std::string("/new_context.json");
+    std::ofstream fout(newContextName);
+    std::string newContext = R"json({
+        "runtime": {
+            "test_params": {
+                "a": "b",
+                "c": "d"
+            }
+        }
+    })json";
+    fout.write(newContext.data(), newContext.size());
+    fout.flush();
+    NTesting::TScopedEnvironment contextGuard("YA_TEST_CONTEXT_FILE", newContextName.data()); // remove context filename
+    Singleton<NPrivate::TTestEnv>()->ReInitialize();
+    EXPECT_EQ("b", GetTestParam("a"));
+    EXPECT_EQ("d", GetTestParam("c"));
+    EXPECT_EQ("", GetTestParam("e"));
 }
