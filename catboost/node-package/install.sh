2ede15a9adf4fe7f4dfd6bb0df85b4654b43f363
Igor Kholopov <igor.kholopov@phystech.edu>
2021-03-18 02:41:06 +0100

    Download fixed release

--- catboost/node-package/install.sh
+++ catboost/node-package/install.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
+CATBOOST_RELEASE="0.24.4"
+
 if [ ! -f "../../ya" ]
 then
-  git clone https://github.com/catboost/catboost && \
-    CATBOOST_SRC_PATH="./catboost" ./build_model.sh && \
-    rm -rf ./catboost && \
+  curl -L https://github.com/catboost/catboost/archive/v${CATBOOST_RELEASE}.tar.gz --output ./catboost.tar.gz && \
+    tar -xf ./catboost.tar.gz && \
+    CATBOOST_SRC_PATH="./catboost-${CATBOOST_RELEASE}" ./build_model.sh && \
+    rm -rf ./catboost-${CATBOOST_RELEASE} ./catboost.tar.gz && \
     node-gyp build
 else
   ./build.sh &&
