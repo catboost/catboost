e20c92affe2e7246c5c1f4dd67ad33334ce6f591
Ksenia Korovina <ks.korovina@gmail.com>
2021-05-03 20:55:38 +0200

    Fix generated metrics with default None values

--- catboost/python-package/catboost/metrics.py
+++ catboost/python-package/catboost/metrics.py
@@ -10,18 +10,18 @@ class BuiltinMetric(object):
     @staticmethod
     def params_with_defaults():
         """
-        Get valid metric parameters with defaults, if any.
+        For each valid metric parameter, returns its default value and if this parameter is mandatory.
         Implemented in child classes.
 
         Returns
         ----------
-        valid_params: dict: param_name -> default value or None.
+        valid_params: dict: param_name -> {'default_value': default value or None, 'is_mandatory': bool}
         """
         raise NotImplementedError('Should be overridden by the child class.')
 
     def __str__(self):
         """
-        Get the representation of the metric object with overridden parameters.
+        Gets the representation of the metric object with overridden parameters.
         Implemented in child classes.
 
         Returns
@@ -32,7 +32,7 @@ class BuiltinMetric(object):
 
     def set_hints(self, **hints):
         """
-        Set hints for the metric. Hints are not validated.
+        Sets hints for the metric. Hints are not validated.
         Implemented in child classes.
 
         Returns
@@ -48,30 +48,35 @@ class _MetricGenerator(type):
             attrs[k] = property(
                 partial(_get_param, name=k),
                 partial(_set_param, name=k),
-                partial(_set_param, name=k, value=None),
+                partial(_del_param, name=k),
                 'Parameter {} of metric {}'.format(k, name),
             )
 
-        attrs['params_with_defaults'] = staticmethod(lambda: copy(attrs['_valid_params']))
-
+        attrs['params_with_defaults'] = staticmethod(lambda: {param: {'default_value': default_value,
+                                                                      'is_mandatory': attrs['_is_mandatory_param'][param]}
+                                                              for param, default_value in attrs['_valid_params'].items()})
         # Set the serialization function.
-        fmt = ['Builtin metric: \'{}\''.format(name), 'Parameters:']
+        docstring = ['Builtin metric: \'{}\''.format(name), 'Parameters:']
         if not attrs['_valid_params']:
-            fmt[-1] += ' none'
-        for k, v in attrs['_valid_params'].items():
-            if v is not None:
-                fmt.append(' ' * 4 + '{} = {} (default)'.format(k, v))
+            docstring[-1] += ' none'
+        for param, value in attrs['_valid_params'].items():
+            if not attrs['_is_mandatory_param'][param]:
+                docstring.append(' ' * 4 + '{} = {} (default value)'.format(param, value))
             else:
-                fmt.append(' ' * 4 + '{} (no default)'.format(k))
-        attrs['__doc__'] = '\n'.join(fmt)
-        attrs['__repr__'] = lambda self: _to_string(self, True)
-        attrs['__str__'] = lambda self: _to_string(self, False)
+                docstring.append(' ' * 4 + '{} (mandatory)'.format(param))
+        attrs['__doc__'] = '\n'.join(docstring)
+        attrs['__repr__'] = lambda self: '{}({})'.format(
+            self._underlying_metric_name,
+            "\n".join(['{}={} [mandatory={}]'.format(param, repr(value), self._is_mandatory_param[param])
+                      for param, value in _current_params(self, False).items()]),
+        )
+        attrs['__str__'] = _to_string
 
         def set_hints(self, **hints):
-            for k, v in hints.items():
-                if isinstance(v, bool):
-                    hints[k] = str(v).lower()
-            setattr(self, 'hints',  '|'.join(['{}~{}'.format(k, v) for k, v in hints.items()]))
+            for hint_key, hint_value in hints.items():
+                if isinstance(hint_value, bool):
+                    hints[hint_key] = str(hint_value).lower()
+            setattr(self, 'hints',  '|'.join(['{}~{}'.format(hint_key, hint_value) for hint_key, hint_value in hints.items()]))
             if 'hints' not in self._params:
                 self._params.append('hints')
             return self
@@ -83,67 +88,85 @@ class _MetricGenerator(type):
     def __call__(cls, **kwargs):
         metric_obj = cls.__new__(cls)
         params = {k: v for k, v in cls._valid_params.items()}
+        param_is_set = {param: not mandatory for param, mandatory in cls._is_mandatory_param.items()}
 
         # Overwrite default parameters and check that all passed parameters are valid.
-        for k, v in kwargs.items():
-            if k not in cls._valid_params:
-                raise ValueError('Unexpected parameter {}'.format(k))
-            params[k] = v
+        for param, value in kwargs.items():
+            if param not in cls._valid_params:
+                raise ValueError('Unexpected parameter {}'.format(param))
+            params[param] = value
+            param_is_set[param] = True
 
         # Check that no parameters are left unset.
-        for param, value in params.items():
-            if value is None:
+        for param, is_set in param_is_set.items():
+            if not is_set:
                 raise ValueError('Parameter {} is mandatory and must be specified.'.format(param))
 
-        for k, v in params.items():
-            setattr(metric_obj, '_' + k, v)
+        for param, value in params.items():
+            _set_param(metric_obj, value, param)
         metric_obj._params = list(params.keys())
 
         return metric_obj
 
     def __setattr__(cls, name, value):
         # Protect property fields from being mutated.
-        if name == '_valid_params':
+        if name in ('_valid_params', '_is_mandatory_param'):
             raise ValueError('Metric\'s `{}` shouldn\'t be modified or deleted.'.format(name))
         type.__setattr__(cls, name, value)
 
     def __delattr__(cls, name):
         # Protect property fields from being mutated.
-        if name == '_valid_params':
+        if name in ('_valid_params', '_is_mandatory_param'):
             raise ValueError('Metric\'s `{}` shouldn\'t be modified or deleted.'.format(name))
         type.__delattr__(cls, name)
 
 
 def _get_param(metric_obj, name):
+    if name not in metric_obj._valid_params:
+        raise ValueError('Metric {} doesn\'t have a parameter {}.'.format(metric_obj.__name__, name))
     return getattr(metric_obj, '_'+name)
 
 
 def _set_param(metric_obj, value, name):
     """Validate a new parameter value in a created metric object."""
-    if value is None:
-        value = metric_obj._valid_params[name]
-        if value is None:
-            raise ValueError('Parameter {} is mandatory, cannot reset.'.format(name))
+    if name not in metric_obj._valid_params:
+        raise ValueError('Metric {} doesn\'t have a parameter {}.'.format(metric_obj.__name__, name))
     setattr(metric_obj, '_' + name, value)
 
 
-def _to_string(metric_obj, with_defaults):
-    s = metric_obj._underlying_metric_name
-    valid_params = metric_obj.params_with_defaults()
-    ps = []
+def _del_param(metric_obj, name):
+    """Validate a new parameter value in a created metric object."""
+    if name not in metric_obj._valid_params:
+        raise ValueError('Metric {} doesn\'t have a parameter {}.'.format(metric_obj.__name__, name))
+    if metric_obj._is_mandatory_param[name]:
+        raise ValueError('Parameter {} is mandatory, cannot reset.'.format(name))
+    value = metric_obj._valid_params[name]
+    setattr(metric_obj, '_' + name, value)
+
+
+def _current_params(metric_obj, override_only):
+    params_with_defaults = metric_obj.params_with_defaults()
+    param_info = {}
     for param in sorted(metric_obj._params):
-        val = getattr(metric_obj, param)
-        if param == 'hints' and val == '':
+        value = getattr(metric_obj, param)  # current value
+        if param == 'hints' and value == '':
+            # Skip unset hints.
             continue
-        if not with_defaults:
+        if override_only:
             # Skip reporting parameters which are set to their default values.
-            default_val = valid_params[param]
-            if default_val == val:
+            if params_with_defaults[param]['default_value'] == value:
                 continue
-        ps.append(param + '=' + str(val))
-    if len(ps) == 0:
-        return s
-    return s + ':' + ';'.join(ps)
+        param_info[param] = value
+    return param_info
+
+
+def _to_string(metric_obj):
+    param_info = _current_params(metric_obj, True)
+    # E.g. AUC for both AUC and AUCMulticlass:
+    underlying_name = metric_obj._underlying_metric_name
+    if len(param_info) == 0:
+        return underlying_name
+    return underlying_name + ':' + ';'.join([param + '=' + str(value) for param, value in param_info.items()])
 
 
 def _generate_metric_classes():
@@ -153,9 +176,12 @@ def _generate_metric_classes():
             del param_set['_name_suffix']
             valid_params = {param: param_value['default_value'] if not param_value['is_mandatory'] else None
                             for param, param_value in param_set.items()}
+            is_mandatory_param = {param: param_value['is_mandatory'] for param, param_value in param_set.items()}
             valid_params.update({'hints': ''})
+            is_mandatory_param.update({'hints': False})
             globals()[derived_name] = _MetricGenerator(str(derived_name), (BuiltinMetric,), {
                 '_valid_params': valid_params,
+                '_is_mandatory_param': is_mandatory_param,
                 '_underlying_metric_name': metric_name,
             })
             globals()['__all__'].append(derived_name)
