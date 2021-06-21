46210d7ce01d8d07e5f2e4554aebce52793fc8ff
Evgueni Petrov <evgueni.s.petrov@gmail.com>
2021-06-21 07:39:49 +0300

    Do not calculate checksums for hash maps because order of elements is undefined
    ref:b072e173a17592c572f10fdd5378b16246f4d84b

--- catboost/libs/helpers/ut/checksum_ut.cpp
+++ catboost/libs/helpers/ut/checksum_ut.cpp
@@ -24,14 +24,6 @@ Y_UNIT_TEST_SUITE(CheckSum) {
         );
         UNIT_ASSERT_VALUES_EQUAL(UpdateCheckSum(0, TVector<int>{10, 12}), 3507297803);
         UNIT_ASSERT_VALUES_EQUAL(
-            UpdateCheckSum(0, THashMap<ui32, THashMap<ui32, ui32>>{{0, {{12, 10}}}, {2, {{0, 5}}}}),
-            855997978
-        );
-        UNIT_ASSERT_VALUES_EQUAL(
-            UpdateCheckSum(0, THashMap<ui32, THashMap<ui32, ui32>>{{2, {{0, 5}}}, {0, {{12, 10}}}}),
-            855997978
-        );
-        UNIT_ASSERT_VALUES_EQUAL(
             UpdateCheckSum(0, TVector<TMap<ui32, ui32>>{{{0, 1}, {2, 3}}, {{2, 11}, {3, 8}}}),
             288846871
         );
