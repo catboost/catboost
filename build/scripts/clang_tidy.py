1da8acded7637aa2edf0cbc8c51255a8aca1b201
Aleksandr <ivansduck@gmail.com>
2021-07-09 02:17:27 +0300

    [build/scripts/clang_tidy] Save profile data
    ref:e70f3e0d0ab0b7ba8515f1985d4c645f2f57e1bd

--- build/scripts/clang_tidy.py
+++ build/scripts/clang_tidy.py
@@ -1,7 +1,10 @@
-import re
-import os
 import argparse
+import contextlib
 import json
+import os
+import re
+import shutil
+import tempfile
 
 import subprocess
 
@@ -16,34 +19,83 @@ def parse_args():
     return parser.parse_known_args()
 
 
-def generate_compilation_database(clang_cmd, source_root, filename):
-    compile_database = [{"file": filename, "command": ' '.join(clang_cmd), "directory": source_root}]
-    compilation_database_json = "compile_commands.json"
-    with open(compilation_database_json, 'w') as afile:
+def generate_compilation_database(clang_cmd, source_root, filename, path):
+    compile_database = [
+        {
+            "file": filename,
+            "command": " ".join(clang_cmd),
+            "directory": source_root,
+        }
+    ]
+    compilation_database_json = os.path.join(path, "compile_commands.json")
+    with open(compilation_database_json, "w") as afile:
         json.dump(compile_database, afile)
     return compilation_database_json
+
+
+@contextlib.contextmanager
+def gen_tmpdir():
+    path = tempfile.mkdtemp()
+    yield path
+    shutil.rmtree(path)
+
+
+def load_profile(path):
+    if os.path.exists(path):
+        files = os.listdir(path)
+        if len(files) == 1:
+            with open(os.path.join(path, files[0])) as afile:
+                return json.load(afile)["profile"]
+        elif len(files) > 1:
+            return {
+                "error": "found several profile files: {}".format(files),
+            }
+    return {
+        "error": "profile file is missing",
+    }
 
 
 def main():
     args, clang_cmd = parse_args()
     clang_tidy_bin = args.clang_tidy_bin
     output_json = args.tidy_json
-    header_filter = r'^(' + r'|'.join(map(re.escape, [os.path.dirname(args.testing_src)])) + r').*(?<!\.pb\.h)$'
+    header_filter = r"^(" + r"|".join(map(re.escape, [os.path.dirname(args.testing_src)])) + r").*(?<!\.pb\.h)$"
 
-    compile_command_path = generate_compilation_database(clang_cmd, args.source_root, args.testing_src)
-    cmd = [clang_tidy_bin, args.testing_src]
-    cmd += ["-p", compile_command_path]
-    cmd += ["--warnings-as-errors", "*"]
-    cmd += ["--config-file", os.path.join(args.source_root, "build/config/tests/clang_tidy.yaml")]
-    cmd += ['--header-filter', header_filter]
-    cmd += ['--use-color']
-    res = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = res.communicate()
-    exit_code = res.returncode
-    with open(output_json, 'wb') as afile:
-        json.dump({"stdout": out, "stderr": err, "exit_code": exit_code}, afile)
+    with gen_tmpdir() as tmpdir:
+        compile_command_path = generate_compilation_database(clang_cmd, args.source_root, args.testing_src, tmpdir)
+        cmd = [
+            clang_tidy_bin,
+            args.testing_src,
+            "-p",
+            compile_command_path,
+            "--warnings-as-errors",
+            "*",
+            "--config-file",
+            os.path.join(args.source_root, "build/config/tests/clang_tidy.yaml"),
+            "--header-filter",
+            header_filter,
+            "--use-color",
+            "--enable-check-profile",
+            "--store-check-profile={}".format(tmpdir),
+        ]
+        res = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = res.communicate()
+        exit_code = res.returncode
+        profile = load_profile(tmpdir)
+
+    with open(output_json, "wb") as afile:
+        json.dump(
+            {
+                "exit_code": exit_code,
+                "profile": profile,
+                "stderr": err,
+                "stdout": out,
+            },
+            afile,
+        )
+
     output_obj = os.path.splitext(output_json)[0] + ".o"
-    open(output_obj, 'w').close()
+    open(output_obj, "w").close()
 
 
 if __name__ == "__main__":
