8c22dd3cf2db7955951012b97460e9f2421149a0
iaz1607 <iaz1607@yandex-team.ru>
2021-08-24 20:11:32 +0300

    Add --export-fixes support
    ref:20d16b646ef363ba59ce0aaf3b6fbfc04a85568a

--- build/scripts/clang_tidy.py
+++ build/scripts/clang_tidy.py
@@ -17,6 +17,7 @@ def parse_args():
     parser.add_argument("--source-root", required=True)
     parser.add_argument("--build-root", required=True)
     parser.add_argument("--config-file", required=True)
+    parser.add_argument("--export-fixes", required=True)
     return parser.parse_known_args()
 
 
@@ -41,6 +42,13 @@ def gen_tmpdir():
     shutil.rmtree(path)
 
 
+@contextlib.contextmanager
+def gen_tmpfile():
+    _, path = tempfile.mkstemp()
+    yield path
+    os.remove(path)
+
+
 def load_profile(path):
     if os.path.exists(path):
         files = os.listdir(path)
@@ -54,6 +62,14 @@ def load_profile(path):
     return {
         "error": "profile file is missing",
     }
+
+
+def load_fixes(path):
+    if os.path.exists(path):
+        with open(path, 'r') as afile:
+            return afile.read()
+    else:
+        return ""
 
 
 def is_generated(testing_src, build_root):
@@ -74,7 +90,7 @@ def main():
     if is_generated(args.testing_src, args.build_root):
         return
     header_filter = r"^(" + r"|".join(map(re.escape, [os.path.dirname(args.testing_src)])) + r").*(?<!\.pb\.h)$"
-    with gen_tmpdir() as profile_tmpdir, gen_tmpdir() as db_tmpdir:
+    with gen_tmpdir() as profile_tmpdir, gen_tmpdir() as db_tmpdir, gen_tmpfile() as fixes_file:
         compile_command_path = generate_compilation_database(clang_cmd, args.source_root, args.testing_src, db_tmpdir)
         cmd = [
             clang_tidy_bin,
@@ -91,11 +107,14 @@ def main():
             "--enable-check-profile",
             "--store-check-profile={}".format(profile_tmpdir),
         ]
+        if args.export_fixes == "yes":
+            cmd += ["--export-fixes", fixes_file]
         res = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = res.communicate()
         exit_code = res.returncode
         profile = load_profile(profile_tmpdir)
         testing_src = os.path.relpath(args.testing_src, args.source_root)
+        tidy_fixes = load_fixes(fixes_file)
 
     with open(output_json, "wb") as afile:
         json.dump(
@@ -105,12 +124,10 @@ def main():
                 "profile": profile,
                 "stderr": err,
                 "stdout": out,
+                "fixes": tidy_fixes,
             },
             afile,
         )
-
-    output_obj = os.path.splitext(output_json)[0] + ".o"
-    open(output_obj, "w").close()
 
 
 if __name__ == "__main__":
