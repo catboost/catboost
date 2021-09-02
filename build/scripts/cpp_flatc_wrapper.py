2141d255e4506e6684ee3a2aef7f24169393e08d
snermolaev <snermolaev@yandex-team.ru>
2021-09-02 06:13:02 +0300

    fix reporting errors by flatc for c++
    ref:c8e9da5bb22c0db208d6c28c6b037a6caf94e260

--- build/scripts/cpp_flatc_wrapper.py
+++ build/scripts/cpp_flatc_wrapper.py
@@ -10,7 +10,14 @@ def main():
         cmd[index+1] = os.path.dirname(cmd[index+1])
     except (ValueError, IndexError):
         pass
-    sys.exit(subprocess.Popen(cmd, stdout=sys.stdout, stderr=sys.stderr).wait())
+    p = subprocess.Popen(cmd, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if p.returncode:
+        if out:
+            sys.stderr.write('stdout:\n{}\n'.format(out))
+        if err:
+            sys.stderr.write('stderr:\n{}\n'.format(err))
+    sys.exit(p.returncode)
 
 
 if __name__ == '__main__':
