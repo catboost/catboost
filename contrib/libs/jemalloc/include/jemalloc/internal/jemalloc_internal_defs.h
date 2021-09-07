ad7ede5ad5981b658cc32d690d9a4fec225dfb4a
bugaevskiy <bugaevskiy@yandex-team.ru>
2021-09-07 08:08:17 +0300

    TAXICOMMON-4285 Add Apple M1 compat
    ref:17bcc9adec58478e1b9e8c34dcb2baaed45391c3

--- contrib/libs/jemalloc/include/jemalloc/internal/jemalloc_internal_defs.h
+++ contrib/libs/jemalloc/include/jemalloc/internal/jemalloc_internal_defs.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#if defined(__APPLE__)
+#if defined(__APPLE__) && defined(__arm64__)
+#   include "jemalloc_internal_defs-osx-arm64.h"
+#elif defined(__APPLE__)
 #   include "jemalloc_internal_defs-osx.h"
 #elif defined(_MSC_VER)
 #   include "jemalloc_internal_defs-win.h"
