3b3024ac390c58b8ed9ecf27ae698d0babda8ee9
eeight <eeight@yandex-team.ru>
2021-09-14 00:45:10 +0300

    IGNIETFERRO-1337 Prepare to disable implicit THolder(ptr)
    ref:6da2745874c5f61f670c1ca1b3d13d9e60f0ffce

--- catboost/cuda/methods/dynamic_boosting.h
+++ catboost/cuda/methods/dynamic_boosting.h
@@ -255,7 +255,7 @@ namespace NCatboostCuda {
             TMetricCalcer<TObjective> metricCalcer(target.GetTarget(estimationPermutation), LocalExecutor);
             THolder<TMetricCalcer<TObjective>> testMetricCalcer;
             if (testTarget) {
-                testMetricCalcer = new TMetricCalcer<TObjective>(*testTarget, LocalExecutor);
+                testMetricCalcer = StealToHolder(new TMetricCalcer<TObjective>(*testTarget, LocalExecutor));
             }
 
             auto snapshotSaver = [&](IOutputStream* out) {
