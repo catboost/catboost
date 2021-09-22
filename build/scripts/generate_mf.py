dd3d2463c497dd8e00856ed43f28a6f0247491d6
heretic <heretic@yandex-team.ru>
2021-09-22 16:36:21 +0300

    CREDITS generation DTCC-481
    ref:4feff97e1ba9fd5c30b51f5d6f33d9ae67ec5119

--- build/scripts/generate_mf.py
+++ build/scripts/generate_mf.py
@@ -16,13 +16,15 @@ class GplNotAllowed(Exception):
 
 def parse_args():
     args = pcf.get_args(sys.argv[1:])
-    lics, peers, free_args = [], [], []
+    lics, peers, free_args, credits = [], [], [], []
     current_list = free_args
     for a in args:
         if a == '-Ya,lics':
             current_list = lics
         elif a == '-Ya,peers':
             current_list = peers
+        elif a == '-Ya,credits':
+            current_list = credits
         elif a and a.startswith('-'):
             current_list = free_args
             current_list.append(a)
@@ -35,9 +37,10 @@ def parse_args():
     parser.add_option('--build-root')
     parser.add_option('--module-name')
     parser.add_option('-o', '--output')
+    parser.add_option('-c', '--credits-output')
     parser.add_option('-t', '--type')
     opts, _ = parser.parse_args(free_args)
-    return lics, peers, opts
+    return lics, peers, credits, opts,
 
 
 def validate_mf(mf, module_type):
@@ -64,10 +67,22 @@ def validate_mf(mf, module_type):
             logging.warning("[[warn]]Can't check NO_GPL properly[[rst]] because the following project(s) has no [[imp]]LICENSE[[rst]] macro:\n%s", '\n'.join(bad_contribs))
 
 
-def generate_mf():
-    lics, peers, options = parse_args()
+def generate_header(meta):
+    return '-' * 20 + meta.get('path', 'Unknown module') + '-' * 20
 
-    meta = {'module_name': options.module_name, 'path': os.path.dirname(options.output), 'licenses': lics, 'dependencies': [], 'no_gpl': options.no_gpl, 'allow_lgpl': options.allow_lgpl}
+
+def generate_mf():
+    lics, peers, credits, options = parse_args()
+
+    meta = {
+        'module_name': options.module_name,
+        'path': os.path.dirname(options.output),
+        'licenses': lics,
+        'dependencies': [],
+        'no_gpl': options.no_gpl,
+        'allow_lgpl': options.allow_lgpl,
+        'license_texts': ''
+    }
 
     build_root = options.build_root
     file_name = os.path.join(build_root, options.output)
@@ -77,6 +92,28 @@ def generate_mf():
             with open(os.path.join(build_root, rel_filename + '.mf')) as peer_file:
                 peer_meta = json.load(peer_file)
                 meta['dependencies'].append(peer_meta)
+
+    if credits:
+        union_texts = []
+        for texts_file in credits:
+            with open(texts_file) as f:
+                union_texts.append(f.read())
+        meta['license_texts'] = '\n\n'.join(union_texts)
+
+    if options.credits_output:
+        final_credits = []
+        if meta['license_texts']:
+            final_credits.append(generate_header(meta) + '\n' + meta['license_texts'])
+        for peer in peers:
+            candidate = peer + '.mf'
+            with open(candidate) as src:
+                data = json.loads(src.read())
+                texts = data.get('license_texts')
+                if texts:
+                    final_credits.append(generate_header(data) + '\n' + texts)
+
+        with open(options.credits_output, 'w') as f:
+            f.write('\n\n'.join(final_credits))
 
     with open(file_name, 'w') as mf_file:
         json.dump(meta, mf_file, indent=4)
