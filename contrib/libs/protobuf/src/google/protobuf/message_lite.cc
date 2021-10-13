c286a6c81f2bf402218a20a5a2b9b10b91d1d232
Ilnur Khuziev <ilnur.khuziev@yandex.ru>
2021-10-13 14:45:56 +0300

    IGNIETFERRO-1911 add IsInitialized cheking in serialization in release build
    https://clubs.at.yandex-team.ru/arcadia/26699/26712#reply-arcadia-26712
    
    ref:11e4109226a9c74cbdaab5dac7714c94b25e43e6

--- contrib/libs/protobuf/src/google/protobuf/message_lite.cc
+++ contrib/libs/protobuf/src/google/protobuf/message_lite.cc
@@ -526,6 +526,13 @@ void MessageLite::ParseFromStringOrThrow(std::string_view s) {
 #if PROTOBUF_USE_EXCEPTIONS
 TProtoStringType NProtoBuf::MessageLite::SerializeAsStringOrThrow() const {
   TProtoStringType s;
+  if (!IsInitialized()) {
+    //NOTE: SerializeToString (called inside SerializeAsString too) does not perform this check in release build
+    //    so SerializeToString in release build return false only if result size is greater than 2gb
+    //    but in debug build not properly inited message (without required filds) will lead to an exception
+    //    different control flow in debug and build release look like a bug
+    throw FatalException("message_lite.cc", __LINE__, "Some required fileds are not set in message " + GetTypeName());
+  }
   const bool isOk = SerializeToString(&s);
   if (!isOk) {
     throw FatalException("message_lite.cc", __LINE__, "Failed to serialize protobuf message " + GetTypeName());
