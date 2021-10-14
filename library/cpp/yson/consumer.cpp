3683e10e260a33980f9db3754fb23c7d3d616a02
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2021-10-15 02:55:53 +0300

    intermediate changes ref:8b0e970ee0a3dc3f0a4ced9474785830bca1b752

--- library/cpp/yson/consumer.cpp
+++ library/cpp/yson/consumer.cpp
@@ -1,9 +1,14 @@
 #include "consumer.h"
-
+#include "string.h"
 #include "parser.h"
 
 namespace NYson {
     ////////////////////////////////////////////////////////////////////////////////
+
+
+    void IYsonConsumer::OnRaw(const TYsonStringBuf& yson) {
+        OnRaw(yson.AsStringBuf(), yson.GetType());
+    }
 
     void TYsonConsumerBase::OnRaw(const TStringBuf& yson, EYsonType type) {
         ParseYsonStringBuffer(yson, this, type);
