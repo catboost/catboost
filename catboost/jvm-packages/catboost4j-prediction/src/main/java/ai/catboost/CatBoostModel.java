dac1d6d81e297d8539cd9ab77ee4dd533f9ea85d
Kamil Jedrzejuk <kamil.jedrzejuk@allegro.pl>
2021-11-05 09:29:37 +0100

    First implementation without test, which add support for loading large files in Java.

--- catboost/jvm-packages/catboost4j-prediction/src/main/java/ai/catboost/CatBoostModel.java
+++ catboost/jvm-packages/catboost4j-prediction/src/main/java/ai/catboost/CatBoostModel.java
@@ -6,8 +6,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
