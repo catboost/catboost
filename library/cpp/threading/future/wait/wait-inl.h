13d568da64c75541462f76b0e08c5b54e903ee8f
eeight <eeight@yandex-team.ru>
2021-11-12 12:28:26 +0300

    Avoid instantiating all of WaitGroup templates on every inclusion of future.h
    This saves about 150ms of compile time for each of the files that include future.h
    
    ref:b5e45a39acced68549a046cd7606ab913ed47901

--- library/cpp/threading/future/wait/wait-inl.h
+++ library/cpp/threading/future/wait/wait-inl.h
@@ -4,86 +4,33 @@
 #error "you should never include wait-inl.h directly"
 #endif // INCLUDE_FUTURE_INL_H
 
-#include "wait_group.h"
-#include "wait_policy.h"
-
 namespace NThreading {
     namespace NImpl {
-        template <class WaitPolicy>
-        inline TFuture<void> WaitGeneric(const TFuture<void>& f1) {
-            return f1;
-        }
+        template <typename TContainer>
+        TVector<TFuture<void>> ToVoidFutures(const TContainer& futures) {
+            TVector<TFuture<void>> voidFutures;
+            voidFutures.reserve(futures.size());
 
-        template <class WaitPolicy>
-        inline TFuture<void> WaitGeneric(const TFuture<void>& f1, const TFuture<void>& f2) {
-            TWaitGroup<WaitPolicy> wg;
-
-            wg.Add(f1).Add(f2);
-
-            return std::move(wg).Finish();
-        }
-
-        template <class WaitPolicy, class TContainer>
-        inline TFuture<void> WaitGeneric(const TContainer& futures) {
-            if (futures.empty()) {
-                return MakeFuture();
-            }
-            if (futures.size() == 1) {
-                return futures.front().IgnoreResult();
+            for (const auto& future: futures) {
+                voidFutures.push_back(future.IgnoreResult());
             }
 
-            TWaitGroup<WaitPolicy> wg;
-            for (const auto& fut : futures) {
-                wg.Add(fut);
-            }
-
-            return std::move(wg).Finish();
+            return voidFutures;
         }
     }
 
-    ////////////////////////////////////////////////////////////////////////////////
-
-    inline TFuture<void> WaitAll(const TFuture<void>& f1) {
-        return NImpl::WaitGeneric<TWaitPolicy::TAll>(f1);
-    }
-
-    inline TFuture<void> WaitAll(const TFuture<void>& f1, const TFuture<void>& f2) {
-        return NImpl::WaitGeneric<TWaitPolicy::TAll>(f1, f2);
+    template <typename TContainer>
+    [[nodiscard]] NImpl::EnableGenericWait<TContainer> WaitAll(const TContainer& futures) {
+        return WaitAll(NImpl::ToVoidFutures(futures));
     }
 
     template <typename TContainer>
-    inline TFuture<void> WaitAll(const TContainer& futures) {
-        return NImpl::WaitGeneric<TWaitPolicy::TAll>(futures);
-    }
-
-
-    ////////////////////////////////////////////////////////////////////////////////
-
-    inline TFuture<void> WaitExceptionOrAll(const TFuture<void>& f1) {
-        return NImpl::WaitGeneric<TWaitPolicy::TExceptionOrAll>(f1);
-    }
-
-    inline TFuture<void> WaitExceptionOrAll(const TFuture<void>& f1, const TFuture<void>& f2) {
-        return NImpl::WaitGeneric<TWaitPolicy::TExceptionOrAll>(f1, f2);
+    [[nodiscard]] NImpl::EnableGenericWait<TContainer> WaitExceptionOrAll(const TContainer& futures) {
+        return WaitExceptionOrAll(NImpl::ToVoidFutures(futures));
     }
 
     template <typename TContainer>
-    inline TFuture<void> WaitExceptionOrAll(const TContainer& futures) {
-        return NImpl::WaitGeneric<TWaitPolicy::TExceptionOrAll>(futures);
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////
-
-    inline TFuture<void> WaitAny(const TFuture<void>& f1) {
-        return NImpl::WaitGeneric<TWaitPolicy::TAny>(f1);
-    }
-
-    inline TFuture<void> WaitAny(const TFuture<void>& f1, const TFuture<void>& f2) {
-        return NImpl::WaitGeneric<TWaitPolicy::TAny>(f1, f2);
-    }
-
-    template <typename TContainer>
-    inline TFuture<void> WaitAny(const TContainer& futures) {
-        return NImpl::WaitGeneric<TWaitPolicy::TAny>(futures);
+    [[nodiscard]] NImpl::EnableGenericWait<TContainer> WaitAny(const TContainer& futures) {
+        return WaitAny(NImpl::ToVoidFutures(futures));
     }
 }
