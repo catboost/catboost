73a3142a8a30c8db32607b8e102109b1cf2e0f47
kmerenkov <kmerenkov@yandex-team.ru>
2022-01-11 21:44:39 +0300

    PASSP-35976 Перенос passport/python/oauth в passport/backend/oauth
    ref:b37b54872cc07c8a8b80404cd895d0196bf570a4

--- build/plugins/pybuild.py
+++ build/plugins/pybuild.py
@@ -129,6 +129,7 @@ def add_python_lint_checks(unit, py_ver, files):
             "taxi/uservices/",
             "travel/",
             "market/report/lite/",  # MARKETOUT-38662, deadline: 2021-08-12
+            "passport/backend/oauth/",  # PASSP-35982
         )
 
         upath = unit.path()[3:]
