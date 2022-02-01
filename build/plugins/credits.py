97f4330577f1bb3d7d56fa19a45dea12c326f952
heretic <heretic@yandex-team.ru>
2022-02-01 17:45:52 +0300

    Check license texts in contrib/deprecated DTCC-767
    ref:d3df364df2525f05bef2080f2f5b37d7a8d41a37

--- build/plugins/credits.py
+++ build/plugins/credits.py
@@ -7,6 +7,16 @@ def oncredits_disclaimer(unit, *args):
 
 def oncheck_contrib_credits(unit, *args):
     module_path = rootrel_arc_src(unit.path(), unit)
+    excepts = set()
+    if 'EXCEPT' in args:
+        args = list(args)
+        except_pos = args.index('EXCEPT')
+        excepts = set(args[except_pos + 1:])
+        args = args[:except_pos]
     for arg in args:
         if module_path.startswith(arg) and not unit.get('CREDITS_TEXTS_FILE') and not unit.get('NO_CREDITS_TEXTS_FILE'):
-            unit.message(["error", "License texts not found. See https://st.yandex-team.ru/DTCC-324"])
+            for ex in excepts:
+                if module_path.startswith(ex):
+                    break
+            else:
+                unit.message(["error", "License texts not found. See https://st.yandex-team.ru/DTCC-324"])
