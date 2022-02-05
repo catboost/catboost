822ed51e5ad97089290d1cbc36616778d7927923
Andrey Khalyavin <halyavin@gmail.com>
2022-02-05 18:51:18 +0300

    Update libc++ to 968e2739 (12 Oct 2021).
    Notable changes:
    * add option to disable wide character support
    * implement conditional noexcept for std::exchange
    * implement std::compare_three_way
    
    ref:ecedc01c380ac67d9abc5dfa1f6b379e216724fc

--- contrib/libs/cxxsupp/libcxx/src/iostream.cpp
+++ contrib/libs/cxxsupp/libcxx/src/iostream.cpp
@@ -30,6 +30,7 @@ __asm__("?cin@" _LIBCPP_ABI_NAMESPACE_STR "@std@@3V?$basic_istream@DU?$char_trai
 _ALIGNAS_TYPE (__stdinbuf<char> ) static char __cin[sizeof(__stdinbuf <char>)];
 static mbstate_t mb_cin;
 
+#ifndef _LIBCPP_HAS_NO_WIDE_CHARACTERS
 #if defined(_LIBCPP_ABI_MICROSOFT) && !defined(__clang__)
 _ALIGNAS_TYPE (wistream) char _wcin [sizeof(wistream)];
 _LIBCPP_FUNC_VIS wistream& wcin = *reinterpret_cast<wistream*>(_wcin);
@@ -42,6 +43,7 @@ __asm__("?wcin@" _LIBCPP_ABI_NAMESPACE_STR "@std@@3V?$basic_istream@_WU?$char_tr
 #endif
 _ALIGNAS_TYPE (__stdinbuf<wchar_t> ) static char __wcin[sizeof(__stdinbuf <wchar_t>)];
 static mbstate_t mb_wcin;
+#endif // _LIBCPP_HAS_NO_WIDE_CHARACTERS
 
 #if defined(_LIBCPP_ABI_MICROSOFT) && !defined(__clang__)
 _ALIGNAS_TYPE (ostream)  char _cout[sizeof(ostream)];
@@ -56,6 +58,7 @@ __asm__("?cout@" _LIBCPP_ABI_NAMESPACE_STR "@std@@3V?$basic_ostream@DU?$char_tra
 _ALIGNAS_TYPE (__stdoutbuf<char>) static char __cout[sizeof(__stdoutbuf<char>)];
 static mbstate_t mb_cout;
 
+#ifndef _LIBCPP_HAS_NO_WIDE_CHARACTERS
 #if defined(_LIBCPP_ABI_MICROSOFT) && !defined(__clang__)
 _ALIGNAS_TYPE (wostream) char _wcout[sizeof(wostream)];
 _LIBCPP_FUNC_VIS wostream& wcout = *reinterpret_cast<wostream*>(_wcout);
@@ -68,6 +71,7 @@ __asm__("?wcout@" _LIBCPP_ABI_NAMESPACE_STR "@std@@3V?$basic_ostream@_WU?$char_t
 #endif
 _ALIGNAS_TYPE (__stdoutbuf<wchar_t>) static char __wcout[sizeof(__stdoutbuf<wchar_t>)];
 static mbstate_t mb_wcout;
+#endif // _LIBCPP_HAS_NO_WIDE_CHARACTERS
 
 #if defined(_LIBCPP_ABI_MICROSOFT) && !defined(__clang__)
 _ALIGNAS_TYPE (ostream)  char _cerr[sizeof(ostream)];
@@ -82,6 +86,7 @@ __asm__("?cerr@" _LIBCPP_ABI_NAMESPACE_STR "@std@@3V?$basic_ostream@DU?$char_tra
 _ALIGNAS_TYPE (__stdoutbuf<char>) static char __cerr[sizeof(__stdoutbuf<char>)];
 static mbstate_t mb_cerr;
 
+#ifndef _LIBCPP_HAS_NO_WIDE_CHARACTERS
 #if defined(_LIBCPP_ABI_MICROSOFT) && !defined(__clang__)
 _ALIGNAS_TYPE (wostream) char _wcerr[sizeof(wostream)];
 _LIBCPP_FUNC_VIS wostream& wcerr = *reinterpret_cast<wostream*>(_wcerr);
@@ -94,6 +99,7 @@ __asm__("?wcerr@" _LIBCPP_ABI_NAMESPACE_STR "@std@@3V?$basic_ostream@_WU?$char_t
 #endif
 _ALIGNAS_TYPE (__stdoutbuf<wchar_t>) static char __wcerr[sizeof(__stdoutbuf<wchar_t>)];
 static mbstate_t mb_wcerr;
+#endif // _LIBCPP_HAS_NO_WIDE_CHARACTERS
 
 #if defined(_LIBCPP_ABI_MICROSOFT) && !defined(__clang__)
 _ALIGNAS_TYPE (ostream)  char _clog[sizeof(ostream)];
@@ -106,6 +112,7 @@ __asm__("?clog@" _LIBCPP_ABI_NAMESPACE_STR "@std@@3V?$basic_ostream@DU?$char_tra
 ;
 #endif
 
+#ifndef _LIBCPP_HAS_NO_WIDE_CHARACTERS
 #if defined(_LIBCPP_ABI_MICROSOFT) && !defined(__clang__)
 _ALIGNAS_TYPE (wostream) char _wclog[sizeof(wostream)];
 _LIBCPP_FUNC_VIS wostream& wclog = *reinterpret_cast<wostream*>(_wclog);
@@ -116,6 +123,7 @@ __asm__("?wclog@" _LIBCPP_ABI_NAMESPACE_STR "@std@@3V?$basic_ostream@_WU?$char_t
 #endif
 ;
 #endif
+#endif // _LIBCPP_HAS_NO_WIDE_CHARACTERS
 
 // Hacky way to make the compiler believe that we're inside a system header so
 // it doesn't flag the use of the init_priority attribute with a value that's
@@ -157,33 +165,38 @@ DoIOSInit::DoIOSInit()
     force_locale_initialization();
 
     istream* cin_ptr  = ::new(&cin)  istream(::new(__cin)  __stdinbuf <char>(stdin, &mb_cin));
-    wistream* wcin_ptr  = ::new(&wcin)  wistream(::new(__wcin)  __stdinbuf <wchar_t>(stdin, &mb_wcin));
     ostream* cout_ptr = ::new(&cout) ostream(::new(__cout) __stdoutbuf<char>(stdout, &mb_cout));
-    wostream* wcout_ptr = ::new(&wcout) wostream(::new(__wcout) __stdoutbuf<wchar_t>(stdout, &mb_wcout));
     ostream* cerr_ptr = ::new(&cerr) ostream(::new(__cerr) __stdoutbuf<char>(stderr, &mb_cerr));
                         ::new(&clog) ostream(cerr_ptr->rdbuf());
+    cin_ptr->tie(cout_ptr);
+    _VSTD::unitbuf(*cerr_ptr);
+    cerr_ptr->tie(cout_ptr);
+
+#ifndef _LIBCPP_HAS_NO_WIDE_CHARACTERS
+    wistream* wcin_ptr  = ::new(&wcin)  wistream(::new(__wcin)  __stdinbuf <wchar_t>(stdin, &mb_wcin));
+    wostream* wcout_ptr = ::new(&wcout) wostream(::new(__wcout) __stdoutbuf<wchar_t>(stdout, &mb_wcout));
     wostream* wcerr_ptr = ::new(&wcerr) wostream(::new(__wcerr) __stdoutbuf<wchar_t>(stderr, &mb_wcerr));
                           ::new(&wclog) wostream(wcerr_ptr->rdbuf());
 
-    cin_ptr->tie(cout_ptr);
     wcin_ptr->tie(wcout_ptr);
-    _VSTD::unitbuf(*cerr_ptr);
     _VSTD::unitbuf(*wcerr_ptr);
-    cerr_ptr->tie(cout_ptr);
     wcerr_ptr->tie(wcout_ptr);
+#endif
 }
 
 DoIOSInit::~DoIOSInit()
 {
     ostream* cout_ptr = reinterpret_cast<ostream*>(&cout);
-    wostream* wcout_ptr = reinterpret_cast<wostream*>(&wcout);
     cout_ptr->flush();
-    wcout_ptr->flush();
-
     ostream* clog_ptr = reinterpret_cast<ostream*>(&clog);
-    wostream* wclog_ptr = reinterpret_cast<wostream*>(&wclog);
     clog_ptr->flush();
+
+#ifndef _LIBCPP_HAS_NO_WIDE_CHARACTERS
+    wostream* wcout_ptr = reinterpret_cast<wostream*>(&wcout);
+    wcout_ptr->flush();
+    wostream* wclog_ptr = reinterpret_cast<wostream*>(&wclog);
     wclog_ptr->flush();
+#endif
 }
 
 ios_base::Init::Init()
