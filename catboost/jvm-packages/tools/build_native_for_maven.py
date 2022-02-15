9a176cf3d3aecfcb47005872f61708c82f77f9de
akhropov <akhropov@yandex-team.ru>
2022-02-15 18:11:39 +0300

    Support universal binaries for JVM libs. MLTOOLS-6206.
    ref:a8705b8a9bd2f56c081aa2c8e761bb8fb57b576f

--- catboost/jvm-packages/tools/build_native_for_maven.py
+++ catboost/jvm-packages/tools/build_native_for_maven.py
@@ -15,6 +15,7 @@ import contextlib
 import errno
 import os
 import platform
+import re
 import shutil
 import subprocess
 import sys
@@ -83,8 +84,77 @@ def _ensure_dir_exists(path):
             raise
 
 
-def _get_current_machine_resources_dir():
-    return ''.join((_get_platform(), '-', _get_arch()))
+def _transform_yamake_platform(target_platform):
+    parts = target_platform.lower().split('-')
+    platform = parts[-2]
+    arch = parts[-1]
+    if arch == 'amd64':
+        arch = 'x86_64'
+    if platform == 'win':
+        # TODO(akhropov): legacy logic, switch to 'win'
+        platform = 'win32'
+    return platform + '-' + arch
+
+
+def _find_target_platforms(yamake_args):
+    target_platforms = []
+
+    arg_idx = 0
+    while arg_idx < len(yamake_args):
+        arg = yamake_args[arg_idx]
+        if arg.startswith('--target-platform'):
+            if len(arg) == len('--target-platform'):
+                target_platforms.append(yamake_args[arg_idx + 1])
+                arg_idx += 2
+            elif arg[len('--target-platform')] == '=':
+                target_platforms.append(arg[(len('--target-platform') + 1):])
+                arg_idx += 1
+        else:
+            arg_idx += 1
+
+    return [_transform_yamake_platform(platform) for platform in target_platforms]
+
+
+def _get_platform_resources_dir(yamake_args):
+    target_platforms = _find_target_platforms(yamake_args)
+    if not target_platforms:
+        return ''.join((_get_platform(), '-', _get_arch()))
+    if all([platform.startswith('darwin') for platform in target_platforms]) and ('--lipo' in yamake_args):
+        return 'darwin-universal2'
+    if len(target_platforms) > 1:
+        raise Exception("Can't build for multiple target platforms without `--lipo`")
+    return target_platforms[0]
+
+
+def _is_dll_java(project_path):
+    with open(os.path.join(project_path, 'ya.make')) as ya_make_file:
+        for line in ya_make_file.readlines():
+            if re.match('\W*DLL_JAVA\(.*\)$', line[:-1]) is not None:
+                return True
+    return False
+
+def _get_single_target_ya_make_args(yamake_args):
+    other_args = []
+    single_target_args = []
+
+    arg_idx = 0
+    while arg_idx < len(yamake_args):
+        arg = yamake_args[arg_idx]
+        if arg.startswith('--target-platform'):
+            if len(arg) == len('--target-platform'):
+                if not single_target_args:
+                    single_target_args = ['--target-platform', yamake_args[arg_idx + 1]]
+                arg_idx += 2
+            elif arg[len('--target-platform')] == '=':
+                if not single_target_args:
+                    single_target_args = ['--target-platform', arg[(len('--target-platform') + 1):]]
+                arg_idx += 1
+        else:
+            if arg != '--lipo':
+                other_args.append(arg)
+            arg_idx += 1
+
+    return other_args + single_target_args
 
 
 def _extract_classes_from_jar(jar_file, dst_dir):
@@ -107,7 +177,7 @@ def _main():
     _ensure_dir_exists(resources_dir)
     shared_lib_dir = os.path.join(
         resources_dir,
-        _get_current_machine_resources_dir(),
+        _get_platform_resources_dir(sys.argv[3:]),
         'lib')
     _ensure_dir_exists(shared_lib_dir)
     native_lib_dir = _get_native_lib_dir(_get_arcadia_root(), package_arcadia_path)
@@ -117,17 +187,33 @@ def _main():
     sys.stderr.flush()
 
     with _tempdir(prefix='catboost_build-') as build_output_dir:
-        ya_make = ([sys.executable, ya_path, 'make', native_lib_dir]
+        ya_make_commands = []
+
+        common_ya_make_args = ([sys.executable, ya_path, 'make', native_lib_dir]
             + ['--output', build_output_dir]
             + ['-D', 'CATBOOST_OPENSOURCE=yes']
-            + ['-D', 'CFLAGS=-DCATBOOST_OPENSOURCE=yes']
-            + sys.argv[3:])
-        print (' '.join(ya_make))
-        subprocess.check_call(
-            ya_make,
-            env=env,
-            stdout=sys.stdout,
-            stderr=sys.stderr)
+            + ['-D', 'CFLAGS=-DCATBOOST_OPENSOURCE=yes'])
+
+        extra_ya_make_args = sys.argv[3:]
+        if ('--lipo' in extra_ya_make_args and _is_dll_java(native_lib_dir)):
+            # TODO(akhropov): run separate commands until DLL_JAVA is fully compatible with '--lipo' (YA-60).
+
+            ya_make_commands.append(common_ya_make_args + extra_ya_make_args + ['-D', 'BUILD_LANGUAGES=CPP'])
+            ya_make_commands.append(
+                common_ya_make_args
+                + _get_single_target_ya_make_args(extra_ya_make_args)
+                + ['-D', 'BUILD_LANGUAGES=JAVA']
+            )
+        else:
+            ya_make_commands.append(common_ya_make_args + extra_ya_make_args)
+
+        for ya_make_command in ya_make_commands:
+            print (' '.join(ya_make_command))
+            subprocess.check_call(
+                ya_make_command,
+                env=env,
+                stdout=sys.stdout,
+                stderr=sys.stderr)
 
         native_lib_build_dir = _get_native_lib_dir(build_output_dir, package_arcadia_path)
         jar_name = lib_name + '.jar'
