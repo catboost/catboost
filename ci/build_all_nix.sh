82e9e726b5166654bde5b0dfac0361d7207c6c36
akhropov <akhropov@yandex-team.ru>
2022-02-21 06:07:37 +0300

    Add YA_MAKE_EXTRA_ARGS to build scripts. MLTOOLS-6206.
    ref:61e243674ce23abc191811ae7dcf6388c3d5b72d

--- ci/build_all_nix.sh
+++ ci/build_all_nix.sh
@@ -39,8 +39,8 @@ function os_sdk {
 
 lnx_common_flags="-DNO_DEBUGINFO $CUDA_ARG"
 
-python ya make -r $lnx_common_flags $(os_sdk) -o . catboost/app
-python ya make -r $lnx_common_flags $(os_sdk) -o . catboost/libs/model_interface
+python ya make -r $lnx_common_flags $(os_sdk) $YA_MAKE_EXTRA_ARGS -o . catboost/app
+python ya make -r $lnx_common_flags $(os_sdk) $YA_MAKE_EXTRA_ARGS -o . catboost/libs/model_interface
 
 echo "Starting R package build"
 cd catboost/R-package
@@ -56,7 +56,7 @@ cp -r inst catboost
 cp -r man catboost
 cp -r tests catboost
 
-python ../../ya make -r $lnx_common_flags $(os_sdk) -T src
+python ../../ya make -r $lnx_common_flags $(os_sdk) $YA_MAKE_EXTRA_ARGS -T src
 
 mkdir -p catboost/inst/libs
 [ -s "src/libcatboostr.so" ] && cp $(readlink src/libcatboostr.so) catboost/inst/libs
@@ -69,27 +69,27 @@ cd ../python-package
 PY36=3.6.6
 pyenv install -s $PY36
 pyenv shell $PY36
-python mk_wheel.py $lnx_common_flags $(os_sdk) -DPYTHON_CONFIG=$(pyenv prefix)/bin/python3-config
+python mk_wheel.py $lnx_common_flags $(os_sdk) $YA_MAKE_EXTRA_ARGS -DPYTHON_CONFIG=$(pyenv prefix)/bin/python3-config
 
 PY37=3.7.0
 pyenv install -s $PY37
 pyenv shell $PY37
-python mk_wheel.py $lnx_common_flags $(os_sdk) -DPYTHON_CONFIG=$(pyenv prefix)/bin/python3-config
+python mk_wheel.py $lnx_common_flags $(os_sdk) $YA_MAKE_EXTRA_ARGS -DPYTHON_CONFIG=$(pyenv prefix)/bin/python3-config
 
 PY38=3.8.0
 pyenv install -s $PY38
 pyenv shell $PY38
-python mk_wheel.py $lnx_common_flags $(os_sdk) -DPYTHON_CONFIG=$(pyenv prefix)/bin/python3-config
+python mk_wheel.py $lnx_common_flags $(os_sdk) $YA_MAKE_EXTRA_ARGS -DPYTHON_CONFIG=$(pyenv prefix)/bin/python3-config
 
 PY39=3.9.0
 pyenv install -s $PY39
 pyenv shell $PY39
-python mk_wheel.py $lnx_common_flags $(os_sdk) -DPYTHON_CONFIG=$(pyenv prefix)/bin/python3-config
+python mk_wheel.py $lnx_common_flags $(os_sdk) $YA_MAKE_EXTRA_ARGS -DPYTHON_CONFIG=$(pyenv prefix)/bin/python3-config
 
 PY310=3.10.0
 pyenv install -s $PY310
 pyenv shell $PY310
-python mk_wheel.py $lnx_common_flags $(os_sdk) -DPYTHON_CONFIG=$(pyenv prefix)/bin/python3-config
+python mk_wheel.py $lnx_common_flags $(os_sdk) $YA_MAKE_EXTRA_ARGS -DPYTHON_CONFIG=$(pyenv prefix)/bin/python3-config
 
 
 # JVM prediction native shared library
@@ -97,12 +97,12 @@ python mk_wheel.py $lnx_common_flags $(os_sdk) -DPYTHON_CONFIG=$(pyenv prefix)/b
 cd ../jvm-packages/catboost4j-prediction
 
 python ../tools/build_native_for_maven.py . catboost4j-prediction --build release --no-src-links \
--DOS_SDK=local -DHAVE_CUDA=no -DUSE_SYSTEM_JDK=$JAVA_HOME -DJAVA_HOME=$JAVA_HOME
+-DOS_SDK=local -DHAVE_CUDA=no -DUSE_SYSTEM_JDK=$JAVA_HOME -DJAVA_HOME=$JAVA_HOME $YA_MAKE_EXTRA_ARGS
 
 # Spark native shared library
 
 cd ../../spark/catboost4j-spark/core
 
 python ../../../jvm-packages/tools/build_native_for_maven.py . catboost4j-spark-impl --build release --no-src-links \
--DOS_SDK=local -DHAVE_CUDA=no -DUSE_LOCAL_SWIG=yes -DUSE_SYSTEM_JDK=$JAVA_HOME -DJAVA_HOME=$JAVA_HOME
+-DOS_SDK=local -DHAVE_CUDA=no -DUSE_LOCAL_SWIG=yes -DUSE_SYSTEM_JDK=$JAVA_HOME -DJAVA_HOME=$JAVA_HOME $YA_MAKE_EXTRA_ARGS
 
--- ci/build_all_win64.bat
+++ ci/build_all_win64.bat
@@ -5,10 +5,10 @@ set WIN_COMMON_FLAGS=-k -DOS_SDK=local -DCUDA_ROOT="%CUDA_PATH%" -DUSE_ARCADIA_C
 call "%VS_VARS_PATH%\vcvars64.bat" -vcvars_ver=14.28
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-c:\Python36\python.exe ya make -r -DNO_DEBUGINFO %WIN_COMMON_FLAGS% -DHAVE_CUDA=yes -o . catboost\app
+c:\Python36\python.exe ya make -r -DNO_DEBUGINFO %WIN_COMMON_FLAGS% %YA_MAKE_EXTRA_ARGS% -DHAVE_CUDA=yes -o . catboost\app
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-c:\Python36\python.exe ya make -r -DNO_DEBUGINFO %WIN_COMMON_FLAGS% -DHAVE_CUDA=yes -o . catboost\libs\model_interface\
+c:\Python36\python.exe ya make -r -DNO_DEBUGINFO %WIN_COMMON_FLAGS% %YA_MAKE_EXTRA_ARGS% -DHAVE_CUDA=yes -o . catboost\libs\model_interface\
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 cd catboost\python-package
@@ -19,37 +19,37 @@ set ORIG_PATH=%PATH%
 set PyV=36
 echo c:\Python%PyV%\python.exe
 set PATH=c:\Python%PyV%\Scripts;%ORIG_PATH%
-c:\Python%PyV%\python.exe mk_wheel.py %WIN_COMMON_FLAGS% -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
+c:\Python%PyV%\python.exe mk_wheel.py %WIN_COMMON_FLAGS% %YA_MAKE_EXTRA_ARGS% -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 set PyV=37
 echo c:\Python%PyV%\python.exe
 set PATH=c:\Python%PyV%\Scripts;%ORIG_PATH%
-c:\Python%PyV%\python.exe mk_wheel.py %WIN_COMMON_FLAGS% -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
+c:\Python%PyV%\python.exe mk_wheel.py %WIN_COMMON_FLAGS% %YA_MAKE_EXTRA_ARGS% -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 set PyV=38
 echo c:\Python%PyV%\python.exe
 set PATH=c:\Python%PyV%\Scripts;%ORIG_PATH%
-c:\Python%PyV%\python.exe mk_wheel.py %WIN_COMMON_FLAGS% -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
+c:\Python%PyV%\python.exe mk_wheel.py %WIN_COMMON_FLAGS% %YA_MAKE_EXTRA_ARGS% -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 set PyV=39
 echo c:\Python%PyV%\python.exe
 set PATH=c:\Python%PyV%\Scripts;%ORIG_PATH%
-c:\Python%PyV%\python.exe mk_wheel.py %WIN_COMMON_FLAGS% -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
+c:\Python%PyV%\python.exe mk_wheel.py %WIN_COMMON_FLAGS% %YA_MAKE_EXTRA_ARGS% -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 set PyV=310
 echo c:\Python%PyV%\python.exe
 set PATH=c:\Python%PyV%\Scripts;%ORIG_PATH%
-c:\Python%PyV%\python.exe mk_wheel.py %WIN_COMMON_FLAGS% -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
+c:\Python%PyV%\python.exe mk_wheel.py %WIN_COMMON_FLAGS% %YA_MAKE_EXTRA_ARGS% -DPYTHON_INCLUDE="/I c:/Python%PyV%/include/" -DPYTHON_LIBRARIES="c:/Python%PyV%/libs/python%PyV%.lib"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo Building R-package
 
 cd ..\..
-call ya.bat make -r -DNO_DEBUGINFO -T %WIN_COMMON_FLAGS% -o . .\catboost\R-package\src\
+call ya.bat make -r -DNO_DEBUGINFO -T %WIN_COMMON_FLAGS% %YA_MAKE_EXTRA_ARGS% -o . .\catboost\R-package\src\
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd catboost\R-package
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -90,7 +90,7 @@ cd catboost\jvm-packages\catboost4j-prediction
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 c:\Python36\python.exe ..\tools\build_native_for_maven.py . catboost4j-prediction --build release --no-src-links^
- -DOS_SDK=local -DHAVE_CUDA=no -DUSE_SYSTEM_JDK=%JAVA_HOME% -DJAVA_HOME=%JAVA_HOME%
+ -DOS_SDK=local -DHAVE_CUDA=no -DUSE_SYSTEM_JDK=%JAVA_HOME% -DJAVA_HOME=%JAVA_HOME% %YA_MAKE_EXTRA_ARGS%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 cd ..\..\..
@@ -101,5 +101,5 @@ echo Building Spark native shared library
 cd catboost\spark\catboost4j-spark\core
 
 c:\Python36\python.exe  ..\..\..\jvm-packages\tools\build_native_for_maven.py . catboost4j-spark-impl --build release --no-src-links^
- -DOS_SDK=local -DHAVE_CUDA=no -DUSE_LOCAL_SWIG=yes -DUSE_SYSTEM_JDK=%JAVA_HOME% -DJAVA_HOME=%JAVA_HOME%
+ -DOS_SDK=local -DHAVE_CUDA=no -DUSE_LOCAL_SWIG=yes -DUSE_SYSTEM_JDK=%JAVA_HOME% -DJAVA_HOME=%JAVA_HOME% %YA_MAKE_EXTRA_ARGS%
 if %errorlevel% neq 0 exit /b %errorlevel%
