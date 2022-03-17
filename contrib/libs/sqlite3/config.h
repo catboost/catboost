9b1b87cb9f7d5234bb01bf537d0fd203bbc874e8
robot-contrib <robot-contrib@yandex-team.ru>
2022-03-17 10:42:46 +0300

    Update contrib/libs/sqlite3 to 3.38.1
    ref:3807ccce63e75841f81639b5fab585ba8db3082c

--- contrib/libs/sqlite3/config.h
+++ contrib/libs/sqlite3/config.h
@@ -114,13 +114,13 @@
 #define PACKAGE_NAME "sqlite"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "sqlite 3.38.0"
+#define PACKAGE_STRING "sqlite 3.38.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "sqlite"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.38.0"
+#define PACKAGE_VERSION "3.38.1"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
