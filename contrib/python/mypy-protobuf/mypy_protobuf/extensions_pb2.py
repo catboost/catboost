e826b3be6fcf51da6a8be3d372dc298ce9261d6d
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2022-04-28 14:23:43 +0300

    intermediate changes ref:a5ec02ec151db759fec27a86c694c21aedebdc3a

--- contrib/python/mypy-protobuf/mypy_protobuf/extensions_pb2.py
+++ contrib/python/mypy-protobuf/mypy_protobuf/extensions_pb2.py
@@ -3,6 +3,7 @@
 # source: mypy_protobuf/extensions.proto
 """Generated protocol buffer code."""
 from google.protobuf import descriptor as _descriptor
+from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
@@ -14,49 +15,20 @@ _sym_db = _symbol_database.Default()
 from google.protobuf import descriptor_pb2 as google_dot_protobuf_dot_descriptor__pb2
 
 
-DESCRIPTOR = _descriptor.FileDescriptor(
-  name='mypy_protobuf/extensions.proto',
-  package='mypy_protobuf',
-  syntax='proto2',
-  serialized_options=None,
-  create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x1emypy_protobuf/extensions.proto\x12\rmypy_protobuf\x1a google/protobuf/descriptor.proto:1\n\x08\x63\x61sttype\x12\x1d.google.protobuf.FieldOptions\x18\xe0\xd4\x03 \x01(\t:0\n\x07keytype\x12\x1d.google.protobuf.FieldOptions\x18\xe2\xd4\x03 \x01(\t:2\n\tvaluetype\x12\x1d.google.protobuf.FieldOptions\x18\xe3\xd4\x03 \x01(\t'
-  ,
-  dependencies=[google_dot_protobuf_dot_descriptor__pb2.DESCRIPTOR,])
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1emypy_protobuf/extensions.proto\x12\rmypy_protobuf\x1a google/protobuf/descriptor.proto:1\n\x08\x63\x61sttype\x12\x1d.google.protobuf.FieldOptions\x18\xe0\xd4\x03 \x01(\t:0\n\x07keytype\x12\x1d.google.protobuf.FieldOptions\x18\xe2\xd4\x03 \x01(\t:2\n\tvaluetype\x12\x1d.google.protobuf.FieldOptions\x18\xe3\xd4\x03 \x01(\t')
 
 
 CASTTYPE_FIELD_NUMBER = 60000
-casttype = _descriptor.FieldDescriptor(
-  name='casttype', full_name='mypy_protobuf.casttype', index=0,
-  number=60000, type=9, cpp_type=9, label=1,
-  has_default_value=False, default_value=b"".decode('utf-8'),
-  message_type=None, enum_type=None, containing_type=None,
-  is_extension=True, extension_scope=None,
-  serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key)
+casttype = DESCRIPTOR.extensions_by_name['casttype']
 KEYTYPE_FIELD_NUMBER = 60002
-keytype = _descriptor.FieldDescriptor(
-  name='keytype', full_name='mypy_protobuf.keytype', index=1,
-  number=60002, type=9, cpp_type=9, label=1,
-  has_default_value=False, default_value=b"".decode('utf-8'),
-  message_type=None, enum_type=None, containing_type=None,
-  is_extension=True, extension_scope=None,
-  serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key)
+keytype = DESCRIPTOR.extensions_by_name['keytype']
 VALUETYPE_FIELD_NUMBER = 60003
-valuetype = _descriptor.FieldDescriptor(
-  name='valuetype', full_name='mypy_protobuf.valuetype', index=2,
-  number=60003, type=9, cpp_type=9, label=1,
-  has_default_value=False, default_value=b"".decode('utf-8'),
-  message_type=None, enum_type=None, containing_type=None,
-  is_extension=True, extension_scope=None,
-  serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key)
+valuetype = DESCRIPTOR.extensions_by_name['valuetype']
 
-DESCRIPTOR.extensions_by_name['casttype'] = casttype
-DESCRIPTOR.extensions_by_name['keytype'] = keytype
-DESCRIPTOR.extensions_by_name['valuetype'] = valuetype
-_sym_db.RegisterFileDescriptor(DESCRIPTOR)
+if _descriptor._USE_C_DESCRIPTORS == False:
+  google_dot_protobuf_dot_descriptor__pb2.FieldOptions.RegisterExtension(casttype)
+  google_dot_protobuf_dot_descriptor__pb2.FieldOptions.RegisterExtension(keytype)
+  google_dot_protobuf_dot_descriptor__pb2.FieldOptions.RegisterExtension(valuetype)
 
-google_dot_protobuf_dot_descriptor__pb2.FieldOptions.RegisterExtension(casttype)
-google_dot_protobuf_dot_descriptor__pb2.FieldOptions.RegisterExtension(keytype)
-google_dot_protobuf_dot_descriptor__pb2.FieldOptions.RegisterExtension(valuetype)
-
+  DESCRIPTOR._options = None
 # @@protoc_insertion_point(module_scope)
