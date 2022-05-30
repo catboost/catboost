c74b0a2d487a0d57d579f0f9a93db14868517ef7
thegeorg <thegeorg@yandex-team.ru>
2022-05-30 13:34:15 +0300

    Remove libcxx/include/cxxabi.h header as it does not belong to libcxx
    ref:41409f9f23e3d642b672473fef566fb0b1ed03af

--- contrib/libs/cxxsupp/libcxx/include/cxxabi.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#if defined(__IOS__) || defined(__ANDROID__)
-#include_next <cxxabi.h>
-#elif defined(_WIN32)
-// pass
-#else
-#include <contrib/libs/cxxsupp/libcxxrt/cxxabi.h>
-#endif
