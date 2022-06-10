1d3c87d70f67861aa34ff011edd934ab7906e408
eeight <eeight@yandex-team.ru>
2022-06-10 06:42:54 +0300

    IGNIETFERRO-1105  ->  Top level t-z
    ref:64ce91662401125bdbac13392d2a5bfb49d314f6

--- util/draft/datetime.cpp
+++ util/draft/datetime.cpp
@@ -2,7 +2,7 @@
 
 #include <util/ysaveload.h>
 
-#include <util/system/atomic.h>
+#include <library/cpp/deprecated/atomic/atomic.h>
 #include <util/system/fasttime.h>
 #include <util/datetime/base.h>
 #include <util/datetime/systime.h>
