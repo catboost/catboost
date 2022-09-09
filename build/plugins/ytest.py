30413d082cf2a519014d6bafd527daaca10cddff
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2022-09-09 23:29:39 +0300

    intermediate changes ref:05e0de906d33a9413684eb01c60010599e5861ea

--- build/plugins/ytest.py
+++ build/plugins/ytest.py
@@ -135,7 +135,7 @@ def validate_test(unit, kw):
             errors.append("BOOSTTEST is not allowed here")
     elif valid_kw.get('SCRIPT-REL-PATH') == 'gtest':
         project_path = valid_kw.get('BUILD-FOLDER-PATH', "")
-        if not project_path.startswith(("contrib", "devtools", "mail", "mds", "taxi")):
+        if not project_path.startswith(("contrib", "devtools", "mail", "mds")):
             errors.append("GTEST_UGLY is not allowed here, use GTEST instead")
 
     size_timeout = collections.OrderedDict(sorted(consts.TestSize.DefaultTimeouts.items(), key=lambda t: t[1]))
