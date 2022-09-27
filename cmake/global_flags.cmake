5a769854f2327c6db4def9d073a324b9507c8eba
galtsev <galtsev@yandex-team.ru>
2022-09-27 08:24:07 +0300

    Move controls out of assigned unicode codepoint set to allow any unicode codepoint in PIRE's lexer.
    Issue: CLEANWEB-1512
    
    ref:83171c92dac2c9bea6ed99b76c91c7b1b2010f2e

--- cmake/global_flags.cmake
+++ cmake/global_flags.cmake
@@ -76,6 +76,8 @@ endif()
 
 if (ANDROID)
   include_directories(SYSTEM ${CMAKE_ANDROID_NDK}/sources/cxx-stl/llvm-libc++abi/include)
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsigned-char")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsigned-char")  
 else()
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_FILE_OFFSET_BITS=64")
 endif()
