3cb7c530ac9dbae1ecdf08037534687e43d2f3ae
iaz1607 <iaz1607@yandex-team.ru>
2022-09-29 10:17:41 +0300

    [DEVTOOLS-9717]: Fix ktlint for projects with generated srcs
    ref:a44e65d99749ae8875e32218f54c7cf8caf3dbb0

--- build/scripts/kt_copy.py
+++ build/scripts/kt_copy.py
@@ -5,8 +5,13 @@ if __name__ == '__main__':
     source = sys.argv[1]
     destination = sys.argv[2]
     source_root = sys.argv[3]
+    build_root = sys.argv[4]
     with open(source, 'r') as afile:
         src_content = afile.read()
     src_content = src_content.replace(source_root + '/', "")
+    result_srcs = ""
+    for line in src_content.split("\n"):
+        if not line.startswith(build_root):
+            result_srcs += line + "\n"
     with open(destination, 'w') as afile:
-        afile.write(src_content)
+        afile.write(result_srcs)
