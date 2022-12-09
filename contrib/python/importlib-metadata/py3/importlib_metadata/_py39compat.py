5cba8e41866b17e3f6022bd1afac3eb0aa3d30c4
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2022-12-09 14:47:15 +0300

    intermediate changes ref:a056d4996272fd0f32f0e9a788d2f36b604a74c6

--- contrib/python/importlib-metadata/py3/importlib_metadata/_py39compat.py
+++ contrib/python/importlib-metadata/py3/importlib_metadata/_py39compat.py
@@ -1,7 +1,7 @@
 """
 Compatibility layer with Python 3.8/3.9
 """
-from typing import TYPE_CHECKING, Any, Optional, Tuple
+from typing import TYPE_CHECKING, Any, Optional
 
 if TYPE_CHECKING:  # pragma: no cover
     # Prevent circular imports on runtime.
@@ -22,27 +22,14 @@ def normalized_name(dist: Distribution) -> Optional[str]:
         return Prepared.normalize(getattr(dist, "name", None) or dist.metadata['Name'])
 
 
-def ep_matches(ep: EntryPoint, **params) -> Tuple[EntryPoint, bool]:
+def ep_matches(ep: EntryPoint, **params) -> bool:
     """
     Workaround for ``EntryPoint`` objects without the ``matches`` method.
-    For the sake of convenience, a tuple is returned containing not only the
-    boolean value corresponding to the predicate evalutation, but also a compatible
-    ``EntryPoint`` object that can be safely used at a later stage.
-
-    For example, the following sequences of expressions should be compatible:
-
-        # Sequence 1: using the compatibility layer
-        candidates = (_py39compat.ep_matches(ep, **params) for ep in entry_points)
-        [ep for ep, predicate in candidates if predicate]
-
-        # Sequence 2: using Python 3.9+
-        [ep for ep in entry_points if ep.matches(**params)]
     """
     try:
-        return ep, ep.matches(**params)
+        return ep.matches(**params)
     except AttributeError:
         from . import EntryPoint  # -> delay to prevent circular imports.
 
         # Reconstruct the EntryPoint object to make sure it is compatible.
-        _ep = EntryPoint(ep.name, ep.value, ep.group)
-        return _ep, _ep.matches(**params)
+        return EntryPoint(ep.name, ep.value, ep.group).matches(**params)
