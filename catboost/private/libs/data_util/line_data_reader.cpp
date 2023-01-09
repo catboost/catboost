3c60cca89de8ae02b84827ad97bd1a7da843e396
akhropov <akhropov@yandex-team.ru>
2023-01-09 10:51:42 +0300

    Support calculating statistics for data subset in the style used for resource estimation on Nirvana. MLTOOLS-6838
    ref:df88c3ce02732864847a4cf077d872d6f5630307

--- catboost/private/libs/data_util/line_data_reader.cpp
+++ catboost/private/libs/data_util/line_data_reader.cpp
@@ -1,5 +1,6 @@
 #include "line_data_reader.h"
 
+#include <util/system/compiler.h>
 #include <util/system/fs.h>
 
 
@@ -24,6 +25,57 @@ namespace NCB {
         }
         return count;
     }
+
+
+    TBlocksSubsetLineDataReader::TBlocksSubsetLineDataReader(THolder<ILineDataReader>&& lineDataReader, TVector<TIndexRange<ui64>>&& subsetBlocks)
+        : LineDataReader(std::move(lineDataReader))
+        , SubsetBlocks(std::move(subsetBlocks))
+        , CurrentSubsetBlock(0)
+        , EnclosingLineIdx(0)
+        , LineIdx(0)
+    {}
+
+    ui64 TBlocksSubsetLineDataReader::GetDataLineCount(bool estimate) {
+        Y_UNUSED(estimate);
+
+        ui64 result = 0;
+        for (const auto& block : SubsetBlocks) {
+            result += block.GetSize();
+        }
+        return result;
+    }
+
+    TMaybe<TString> TBlocksSubsetLineDataReader::GetHeader() {
+        return LineDataReader->GetHeader();
+    }
+
+    bool TBlocksSubsetLineDataReader::ReadLine(TString* line, ui64* lineIdx) {
+        while (true) {
+            if (CurrentSubsetBlock >= SubsetBlocks.size()) {
+                return false;
+            }
+            if (EnclosingLineIdx < SubsetBlocks[CurrentSubsetBlock].End) {
+                bool enclosingReadResult = LineDataReader->ReadLine(&LineBuffer);
+                Y_ASSERT(enclosingReadResult);
+
+                if (EnclosingLineIdx >= SubsetBlocks[CurrentSubsetBlock].Begin) {
+                    // in a subset
+                    *line = std::move(LineBuffer);
+                    if (lineIdx) {
+                        *lineIdx = LineIdx;
+                    }
+                    ++LineIdx;
+                    ++EnclosingLineIdx;
+                    return true;
+                } else {
+                    ++EnclosingLineIdx;
+                }
+            } else {
+                ++CurrentSubsetBlock;
+            }
+        }
+    }
+
 
     TLineDataReaderFactory::TRegistrator<TFileLineDataReader> DefLineDataReaderReg("");
     TLineDataReaderFactory::TRegistrator<TFileLineDataReader> FileLineDataReaderReg("file");
