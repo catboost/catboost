092e9503a760ee746318edbfec238740c29b458f
akhropov <akhropov@yandex-team.ru>
2023-01-15 10:54:21 +0300

    Add TDumper.
    ref:2a723c4c64c1bbec7570618873409bca3b855e45

--- catboost/libs/data/meta_info.h
+++ catboost/libs/data/meta_info.h
@@ -6,6 +6,7 @@
 #include <catboost/libs/column_description/column.h>
 
 #include <library/cpp/binsaver/bin_saver.h>
+#include <library/cpp/dbg_output/dump.h>
 #include <library/cpp/json/json_value.h>
 
 #include <util/generic/maybe.h>
@@ -94,3 +95,54 @@ namespace NCB {
 
     void AddWithShared(IBinSaver* binSaver, TDataMetaInfo* data);
 }
+
+template <>
+struct TDumper<NCB::TDataMetaInfo> {
+    template <class S>
+    static inline void Dump(S& s, const NCB::TDataMetaInfo& metaInfo) {
+#define PRINT_META_INFO_FIELD(field) \
+        s << #field"=" << metaInfo.field << Endl;
+
+        PRINT_META_INFO_FIELD(ObjectCount);
+        s << "FeaturesLayout=";
+        if (metaInfo.FeaturesLayout) {
+            s << DbgDump(metaInfo.FeaturesLayout);
+        } else {
+            s << "null";
+        }
+        s << Endl;
+        PRINT_META_INFO_FIELD(MaxCatFeaturesUniqValuesOnLearn);
+        PRINT_META_INFO_FIELD(TargetType);
+        PRINT_META_INFO_FIELD(TargetCount);
+        s << "TargetStats=";
+        if (metaInfo.TargetStats.Defined()) {
+            s << "{MinValue=" << metaInfo.TargetStats->MinValue
+                << ",MaxValue=" << metaInfo.TargetStats->MaxValue << "}\n";
+        } else {
+            s << "undefined\n";
+        }
+
+        PRINT_META_INFO_FIELD(BaselineCount);
+        PRINT_META_INFO_FIELD(HasGroupId);
+        PRINT_META_INFO_FIELD(HasGroupWeight);
+        PRINT_META_INFO_FIELD(HasSubgroupIds);
+        PRINT_META_INFO_FIELD(HasWeights);
+        PRINT_META_INFO_FIELD(HasTimestamp);
+        PRINT_META_INFO_FIELD(HasPairs);
+
+        s << "ClassLabels=" << DbgDump(metaInfo.ClassLabels) << Endl;
+
+        s << "ColumnsInfo=";
+        if (metaInfo.ColumnsInfo.Defined()) {
+            s << "[\n";
+            for (const auto& column : metaInfo.ColumnsInfo->Columns) {
+                s << "\t{Type=" << column.Type << ",Id=" << column.Id << "}\n";
+            }
+            s << "]\n";
+        } else {
+            s << "undefined\n";
+        }
+
+#undef PRINT_META_INFO_FIELD
+    }
+};
