02b01eeac4a92e2e76d833547203083daed98a7f
thegeorg <thegeorg@yandex-team.ru>
2023-01-15 22:29:13 +0300

    Deconfigure legacy libfuzzers in ymake
    ref:ce5c8d090b2092c7c71c72551dd7ba00e1f4ae15

--- build/ymake_conf.py
+++ build/ymake_conf.py
@@ -1647,15 +1647,11 @@ class GnuCompiler(Compiler):
         print 'macro _SRC_masm(SRC, SRCFLAGS...) {\n}'
 
         # fuzzing configuration
-        if self.tc.is_clang and self.tc.version_at_least(5, 0):
-            emit('LIBFUZZER_PATH',
-                 'contrib/libs/libfuzzer12' if self.tc.version_at_least(12) else
-                 'contrib/libs/libfuzzer11' if self.tc.version_at_least(11) else
-                 'contrib/libs/libfuzzer10' if self.tc.version_at_least(10) else
-                 'contrib/libs/libfuzzer8' if self.tc.version_at_least(8) else
-                 'contrib/libs/libfuzzer7' if self.tc.version_at_least(7) else
-                 'contrib/libs/libfuzzer6' if self.tc.version_at_least(6) else
-                 'contrib/libs/libfuzzer-5.0')
+        if self.tc.is_clang:
+            if self.tc.version_at_least(12):
+                emit('LIBFUZZER_PATH', 'contrib/libs/libfuzzer12')
+            elif self.tc.version_at_least(11):
+                emit('LIBFUZZER_PATH', 'contrib/libs/libfuzzer11')
 
 
 class SwiftCompiler(object):
