29a9997baebd890bca3ebd9936170b8a1eb7b905
gleb-kov <gleb-kov@yandex-team.ru>
2023-01-16 00:02:46 +0300

    IGNIETFERRO-1522 add BetterTypeName implementation
    ref:553bcddd1ac2a958db326b40f511e4715a8b1246

--- util/system/type_name_ut.cpp
+++ util/system/type_name_ut.cpp
@@ -2,6 +2,9 @@
 
 #include <library/cpp/testing/unittest/registar.h>
 
+#include <util/generic/yexception.h>
+#include <util/generic/fwd.h>
+
 #include <stdexcept>
 #include <string>
 
@@ -41,6 +44,13 @@ namespace NUtil::NTypeNameTest {
     public:
         virtual ~TRombicTail() = default;
     };
+
+    class TFromThis {
+    public:
+        TString GetTypeName() const {
+            return BetterTypeName(*this);
+        }
+    };
 }
 
 using namespace NUtil::NTypeNameTest;
@@ -66,6 +76,16 @@ Y_UNIT_TEST_SUITE(TypeName) {
 #endif
     }
 
+    Y_UNIT_TEST(FromArcadiaTypes) {
+#ifdef _MSC_VER
+        UNIT_ASSERT_VALUES_EQUAL(TypeName<yexception>(), "class yexception");
+        UNIT_ASSERT_VALUES_EQUAL(TypeName<TString>(), "class TBasicString<char,struct std::__y1::char_traits<char> >");
+#else
+        UNIT_ASSERT_VALUES_EQUAL(TypeName<yexception>(), "yexception");
+        UNIT_ASSERT_VALUES_EQUAL(TypeName<TString>(), "TBasicString<char, std::__y1::char_traits<char> >");
+#endif
+    }
+
     Y_UNIT_TEST(FromUserTypes) {
 #ifdef _MSC_VER
         UNIT_ASSERT_VALUES_EQUAL(TypeName<TSonde>(), "class NUtil::NTypeNameTest::TSonde");
@@ -78,47 +98,88 @@ Y_UNIT_TEST_SUITE(TypeName) {
 
     Y_UNIT_TEST(FromWellKnownValues) {
         void* value = (void*)"123";
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(value), "void");
+        const void* constValue = (const void*)"456";
+
 #ifdef _MSC_VER
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&value), "void * __ptr64");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(value), "void * __ptr64");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(&value), "void * __ptr64 * __ptr64");
+
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(constValue), "void const * __ptr64");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(&constValue), "void const * __ptr64 * __ptr64");
 #else
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&value), "void*");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(value), "void*");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(&value), "void**");
+
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(constValue), "void const*");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(&constValue), "void const**");
 #endif
 
         int zero = 0;
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&zero), "int");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(zero), "int");
 
         double pi = M_PI;
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&pi), "double");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(pi), "double");
 
         std::string string;
         std::runtime_error err("This is awful");
 #ifdef _MSC_VER
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&string), "class std::__y1::basic_string<char,struct std::__y1::char_traits<char>,class std::__y1::allocator<char> >");
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&err), "class std::runtime_error");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(string), "class std::__y1::basic_string<char,struct std::__y1::char_traits<char>,class std::__y1::allocator<char> >");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(err), "class std::runtime_error");
 #else
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&string), "std::__y1::basic_string<char, std::__y1::char_traits<char>, std::__y1::allocator<char> >");
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&err), "std::runtime_error");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(string), "std::__y1::basic_string<char, std::__y1::char_traits<char>, std::__y1::allocator<char> >");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(err), "std::runtime_error");
 #endif
     }
 
     Y_UNIT_TEST(FromUserValues) {
         TSonde sonde;
+        const TSonde constSonde;
+
         TRombicTail rombicTail;
         TRombicHead& castedTail = rombicTail;
+
+        TFromThis obj;
+
 #ifdef _MSC_VER
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&sonde), "class NUtil::NTypeNameTest::TSonde");
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&rombicTail), "class NUtil::NTypeNameTest::TRombicTail");
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&castedTail), "class NUtil::NTypeNameTest::TRombicTail");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(sonde), "class NUtil::NTypeNameTest::TSonde");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(constSonde), "class NUtil::NTypeNameTest::TSonde");
+
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(rombicTail), "class NUtil::NTypeNameTest::TRombicTail");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(castedTail), "class NUtil::NTypeNameTest::TRombicTail");
+
+        UNIT_ASSERT_VALUES_EQUAL(obj.GetTypeName(), "class NUtil::NTypeNameTest::TFromThis");
 #else
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&sonde), "NUtil::NTypeNameTest::TSonde");
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&rombicTail), "NUtil::NTypeNameTest::TRombicTail");
-        UNIT_ASSERT_VALUES_EQUAL(TypeName(&castedTail), "NUtil::NTypeNameTest::TRombicTail");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(sonde), "NUtil::NTypeNameTest::TSonde");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(constSonde), "NUtil::NTypeNameTest::TSonde");
+
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(rombicTail), "NUtil::NTypeNameTest::TRombicTail");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(castedTail), "NUtil::NTypeNameTest::TRombicTail");
+
+        UNIT_ASSERT_VALUES_EQUAL(obj.GetTypeName(), "NUtil::NTypeNameTest::TFromThis");
 #endif
     }
 
     Y_UNIT_TEST(FromTypeInfo) {
         UNIT_ASSERT_VALUES_EQUAL(TypeName(typeid(int)), "int");
         UNIT_ASSERT_VALUES_EQUAL(TypeName(std::type_index(typeid(int))), "int");
+    }
+
+    Y_UNIT_TEST(DistinguishPointerQualifiers) {
+        char *simplePtr = nullptr;
+        const char *constPtr = nullptr;
+        volatile char *volatilePtr = nullptr;
+        const volatile char *cvPtr = nullptr;
+
+#ifdef _MSC_VER
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(simplePtr), "char * __ptr64");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(constPtr), "char const * __ptr64");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(volatilePtr), "char volatile * __ptr64");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(cvPtr), "char const volatile * __ptr64");
+#else
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(simplePtr), "char*");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(constPtr), "char const*");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(volatilePtr), "char volatile*");
+        UNIT_ASSERT_VALUES_EQUAL(BetterTypeName(cvPtr), "char const volatile*");
+#endif
     }
 }
