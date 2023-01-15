a3e998d40a29c96a70228d1174c578c8d65abdd5
miplot <miplot@yandex-team.ru>
2023-01-15 06:12:51 +0300

    Define strlcat in .c file in util
    ref:471560ed982ee31766355a6d35ac469d0bab6244

--- util/system/compat_c.h
+++ util/system/compat_c.h
@@ -3,16 +3,5 @@
 #if !defined(__FreeBSD__)
 size_t strlcpy(char* dst, const char* src, size_t len);
 
-inline size_t strlcat(char* dst, const char* src, size_t len) {
-    size_t dstlen = strlen(dst);
-    size_t srclen = strlen(src);
-
-    if (dstlen < len) {
-        len -= dstlen;
-        dst += dstlen;
-        strlcpy(dst, src, len);
-    }
-
-    return dstlen + srclen;
-}
+size_t strlcat(char* dst, const char* src, size_t len);
 #endif
