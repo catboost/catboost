90e62242b45f413bae647ff00042b4ff010f227c
exprmntr <exprmntr@yandex-team.ru>
2023-01-16 00:54:31 +0300

    Сheck PY_MAIN module in import test
    DEVTOOLS-8527
    
    ****
    ![review](https://codereview.in.yandex-team.ru/badges/review-complete-green.svg) [![anechiporuk](https://codereview.in.yandex-team.ru/badges/anechiporuk-...-yellow.svg)](https://staff.yandex-team.ru/anechiporuk) [![vvgolubev](https://codereview.in.yandex-team.ru/badges/vvgolubev-...-yellow.svg)](https://staff.yandex-team.ru/vvgolubev) [![prettyboy](https://codereview.in.yandex-team.ru/badges/prettyboy-ok-green.svg)](https://staff.yandex-team.ru/prettyboy)
    
    ref:07ea15e2d004742ef5a5a10d0881ce386e029912

--- library/python/testing/import_test/import_test.py
+++ library/python/testing/import_test/import_test.py
@@ -10,7 +10,7 @@ import __res
 from __res import importer
 
 
-def check_imports(no_check=(), extra=(), skip_func=None):
+def check_imports(no_check=(), extra=(), skip_func=None, py_main=None):
     """
     tests all bundled modules are importable
     just add
@@ -35,7 +35,13 @@ def check_imports(no_check=(), extra=(), skip_func=None):
     import_times = {}
 
     norm = lambda s: s[:-9] if s.endswith('.__init__') else s
-    for module in sorted(sys.extra_modules | set(extra), key=norm):
+
+    modules = sys.extra_modules | set(extra)
+    if py_main:
+        modules.add(py_main)
+    modules = sorted(modules, key=norm)
+
+    for module in modules:
         if module not in extra and (rx.search(module) or skip_func and skip_func(module)):
             print('SKIP', module)
             continue
@@ -105,7 +111,14 @@ def main():
         else:
             django.setup()
 
+    py_main = __res.find('PY_MAIN')
+
+    if py_main:
+        py_main_module = py_main.split(b':', 1)[0].decode('UTF-8')
+    else:
+        py_main_module = None
+
     try:
-        check_imports(no_check=skip_names)
+        check_imports(no_check=skip_names, py_main=py_main_module)
     except:
         sys.exit(1)
