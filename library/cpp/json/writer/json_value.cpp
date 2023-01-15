3a7987ec44f42a2a51bd473d52f693fa79cbcb95
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2023-01-15 11:43:58 +0300

    intermediate changes ref:eeeef429f2d29a5a824887a98a3a457cd5dfe2d9

--- library/cpp/json/writer/json_value.cpp
+++ library/cpp/json/writer/json_value.cpp
@@ -531,7 +531,7 @@ namespace NJson {
             case JSON_NULL:
             case JSON_UNDEFINED:
             default:
-                return 0;
+                return false;
             case JSON_BOOLEAN:
                 return Value.Boolean;
         }
