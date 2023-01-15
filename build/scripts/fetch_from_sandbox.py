3b360a9d7ded54b48330e2ad31b1b4f1d7a7e278
Aleksandr <ivansduck@gmail.com>
2023-01-15 21:48:36 +0300

    [build/scripts] Reuse is_temporary_error function from devtools/ya/core/error/__init__.py
    issue:https://st.yandex-team.ru/CI-1483
    
    ref:bd63f59349e029930639b1eb735cb9164e2fe623

--- build/scripts/fetch_from_sandbox.py
+++ build/scripts/fetch_from_sandbox.py
@@ -136,9 +136,10 @@ def fetch(resource_id, custom_fetcher):
     try:
         resource_info = get_resource_info(resource_id, touch=True, no_links=True)
     except Exception as e:
-        raise ResourceInfoError(
-            "Failed to fetch resource {}: {}".format(resource_id, str(e))
+        sys.stderr.write(
+            "Failed to fetch resource {}: {}\n".format(resource_id, str(e))
         )
+        raise
 
     if resource_info.get('state', 'DELETED') != 'READY':
         raise ResourceInfoError("Resource {} is not READY".format(resource_id))
@@ -263,4 +264,6 @@ if __name__ == '__main__':
         logging.exception(e)
         print >>sys.stderr, open(args.abs_log_path).read()
         sys.stderr.flush()
-        sys.exit(fetch_from.INFRASTRUCTURE_ERROR if fetch_from.is_temporary(e) else 1)
+
+        import error
+        sys.exit(error.ExitCodes.INFRASTRUCTURE_ERROR if fetch_from.is_temporary(e) else 1)
