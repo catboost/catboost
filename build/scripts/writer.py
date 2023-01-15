4a5cd51f215f6ed6789069bfbd7668135eb0d89a
svidyuk <svidyuk@yandex-team.ru>
2023-01-15 18:57:24 +0300

    Suport addig resources to the jar targets
    DEVTOOLS-7981
    
    ref:03a1e6b609c5c579a77ebfb2865ea84dd611e605

--- build/scripts/writer.py
+++ build/scripts/writer.py
@@ -1,14 +1,18 @@
+import sys
 import optparse
+
+import process_command_files as pcf
 
 
 def parse_args():
+    args = pcf.get_args(sys.argv[1:])
     parser = optparse.OptionParser()
     parser.add_option('-f', '--file', dest='file_path')
     parser.add_option('-a', '--append', action='store_true', default=False)
     parser.add_option('-Q', '--quote', action='store_true', default=False)
     parser.add_option('-s', '--addspace', action='store_true', default=False)
     parser.add_option('-c', '--content', dest='content')
-    return parser.parse_args()
+    return parser.parse_args(args)
 
 
 def smart_shell_quote(v):
