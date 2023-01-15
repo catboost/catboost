d5eb2e68ee5f9c8636f000025aedc6805b7b3ab9
Stanislav Kirillov <staskirillov@gmail.com>
2023-01-15 05:08:25 +0300

    fix per float feature defaults initialization
    ref:3d197e4a1179ca6f44b664e2b6b5bd3c3e630d8a

--- catboost/private/libs/options/option.h
+++ catboost/private/libs/options/option.h
@@ -31,6 +31,10 @@ namespace NCatboostOptions {
             }
         }
 
+        const TValue& GetDefaultValue() const {
+            return DefaultValue;
+        }
+
         template <class T>
         void Set(T&& value) {
             Value = std::forward<T>(value);
