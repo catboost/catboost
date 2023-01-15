78a5bc3a0759abc2bf1f4b75a425afa4029f1ddb
shadchin <shadchin@yandex-team.ru>
2023-01-15 13:36:48 +0300

    Switch on CYTHON_DIRECTIVE and fix small bug
    ref:1fa7d72dc6dd0980b0fdc52e2808922b0a0e6c8f

--- library/python/runtime_py3/entry_points.py
+++ library/python/runtime_py3/entry_points.py
@@ -19,7 +19,7 @@ def repl():
 
         if func_name and '__main__' not in user_ns:
             def run(args):
-                if isinstance(args, basestring):
+                if isinstance(args, str):
                     import shlex
                     args = shlex.split(args)
 
