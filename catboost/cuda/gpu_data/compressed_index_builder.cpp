ee3d8727df23096628c64d6c9fec7bbc3566728a
Evgueni Petrov <evgueni.s.petrov@gmail.com>
2023-01-15 14:51:05 +0300

    Remove dead code from TCudaFeaturesLayoutHelper
    MLTOOLS-5175
    
    ref:92e4943e4a57f7d67afd3328e52d4cfcf418dd3b

--- catboost/cuda/gpu_data/compressed_index_builder.cpp
+++ catboost/cuda/gpu_data/compressed_index_builder.cpp
@@ -1,12 +1,9 @@
 #include "compressed_index_builder.h"
 #include "feature_layout_doc_parallel.h"
 #include "feature_layout_feature_parallel.h"
-#include "feature_layout_single.h"
 
 namespace NCatboostCuda {
     template class TSharedCompressedIndexBuilder<TFeatureParallelLayout>;
 
     template class TSharedCompressedIndexBuilder<TDocParallelLayout>;
-
-    template class TSharedCompressedIndexBuilder<TSingleDevLayout>;
 }
