8af797a8af44053222d1671a6b59fa6deb9ccfb1
orivej <orivej@yandex-team.ru>
2023-01-15 15:37:34 +0300

    Revert r7721776 to restore r7721263. IGNIETFERRO-1433
    yabs build failures are not going to reoccur since r7722152.
    
    ref:13b1656102d6a1ffc6e36c95d48e01de2568eaca

--- util/folder/dirut.cpp
+++ util/folder/dirut.cpp
@@ -604,7 +604,7 @@ TString GetBaseName(const TString& path) {
 }
 
 static bool IsAbsolutePath(const char* str) {
-    return str && TPathSplitTraitsLocal::IsAbsolutePath(TStringBuf(str, TCharTraits<char>::GetLength(str, 3)));
+    return str && TPathSplitTraitsLocal::IsAbsolutePath(TStringBuf(str, NStringPrivate::GetStringLengthWithLimit(str, 3)));
 }
 
 int ResolvePath(const char* rel, const char* abs, char res[/*MAXPATHLEN*/], bool isdir) {
--- util/generic/fwd.h
+++ util/generic/fwd.h
@@ -4,18 +4,17 @@
 
 #include <stlfwd>
 
-//strings
 template <class TCharType>
-class TCharTraits;
+using TCharTraits = std::char_traits<TCharType>;
 
-template <typename TCharType, typename TTraits = TCharTraits<TCharType>>
+template <typename TCharType, typename TTraits = std::char_traits<TCharType>>
 class TBasicString;
 
 using TString = TBasicString<char>;
 using TUtf16String = TBasicString<wchar16>;
 using TUtf32String = TBasicString<wchar32>;
 
-template <typename TCharType, typename TTraits = TCharTraits<TCharType>>
+template <typename TCharType, typename TTraits = std::char_traits<TCharType>>
 class TBasicStringBuf;
 
 using TStringBuf = TBasicStringBuf<char>;
