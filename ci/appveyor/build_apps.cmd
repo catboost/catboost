0405592b772a0741e106b17c2aac467e9b31ecb1
Stanislav Kirillov <staskirillov@gmail.com>
2023-01-15 23:21:31 +0300

    remove travis and appveyor configs and switch to arcadian python 3.9. Closes #1700
    ref:9df5c0cf2614c4772b2f060f9267fa79bcddf033

--- ci/appveyor/build_apps.cmd
+++
@@ -1,7 +0,0 @@
-call ya.bat make -o . --verbose --stat -r catboost\app -DCUDA_ROOT="C:\\CUDA\\v8.0"
-mv catboost\app\catboost.exe catboost\app\catboost-cuda.exe
-python ci\webdav_upload.py catboost\app\catboost-cuda.exe
-
-call ya.bat make -o . --verbose --stat -r catboost\app
-python ci\webdav_upload.py catboost\app\catboost.exe
-
--- ci/appveyor/build_whls.cmd
+++
@@ -1,9 +0,0 @@
-cd catboost\python-package
-C:\Python27-x64\python.exe mk_wheel.py -DCUDA_ROOT="C:\\CUDA\\v8.0"
-C:\Python34-x64\python.exe mk_wheel.py -DCUDA_ROOT="C:\\CUDA\\v8.0"
-C:\Python35-x64\python.exe mk_wheel.py -DCUDA_ROOT="C:\\CUDA\\v8.0"
-C:\Python36-x64\python.exe mk_wheel.py -DCUDA_ROOT="C:\\CUDA\\v8.0"
-cd ..\..
-dir catboost\python-package
-python ci\webdav_upload.py catboost\python-package\*.whl
-
