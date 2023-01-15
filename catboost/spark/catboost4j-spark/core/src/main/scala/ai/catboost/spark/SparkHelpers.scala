ed3d127a0ac1018ddf7684f084fa11767ec4a5d1
akhropov <akhropov@yandex-team.ru>
2023-01-15 11:18:33 +0300

    More configuration params to set thread count and max memory for driver and worker nodes. MLTOOLS-1517.
    ref:0b3b846352cb32b978b2c7e282fe1dba88a3fea8

--- catboost/spark/catboost4j-spark/core/src/main/scala/ai/catboost/spark/SparkHelpers.scala
+++ catboost/spark/catboost4j-spark/core/src/main/scala/ai/catboost/spark/SparkHelpers.scala
@@ -2,9 +2,53 @@ package ai.catboost.spark;
 
 import org.apache.spark.sql.SparkSession
 
+import ai.catboost.CatBoostError
+
+
 object SparkHelpers {
+  def parseMemoryOverHeadOption(memoryOverhead: String) : Long = {
+    val withUnitRE = "^([\\d]+)(k|m|g|t|K|M|G|T)$".r
+    val withoutUnitRE = "^([\\d]+)$".r
+    memoryOverhead match {
+      case withUnitRE(value, unit) => { 
+        value.toLong << (
+          unit match {
+            case "k" | "K" => 10
+            case "m" | "M" => 20
+            case "g" | "G" => 30
+            case "t" | "T" => 40
+            case _ => throw new java.lang.RuntimeException("Internal error: Incorrect regex matching")
+          }
+        )
+      }
+      case withoutUnitRE(value) => { value.toLong << 20 } // default unit is Megabytes
+      case _ => throw new CatBoostError(s"bad format for memory overhead string: $memoryOverhead")
+    }
+  }
+  
+  def getThreadCountForDriver(spark : SparkSession) : Int = {
+    val taskCpusConfig = spark.sparkContext.getConf.getOption("spark.driver.cores")
+    taskCpusConfig.getOrElse("1").toInt
+  }
+  
   def getThreadCountForTask(spark : SparkSession) : Int = {
     val taskCpusConfig = spark.sparkContext.getConf.getOption("spark.task.cpus")
     taskCpusConfig.getOrElse("1").toInt
+  }
+  
+  def getWorkerCount(spark: SparkSession) : Int = {
+    spark.sparkContext.statusTracker.getExecutorInfos.length - 1 // one is the driver
+  }
+  
+  def getDriverHost(spark: SparkSession) : String = {
+    spark.sparkContext.getConf.getOption("spark.driver.host").get
+  }
+  
+  def getDriverNativeMemoryLimit(spark: SparkSession) : Long = {
+    parseMemoryOverHeadOption(spark.sparkContext.getConf.getOption("spark.driver.memoryOverhead").get)
+  }
+  
+  def getExecutorNativeMemoryLimit(spark: SparkSession) : Long = {
+    parseMemoryOverHeadOption(spark.sparkContext.getConf.getOption("spark.executor.memoryOverhead").get)
   }
 }
