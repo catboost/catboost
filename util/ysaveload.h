e4f7190ec3eb2b432f347ef8da51eb956ccb7586
swarmer <swarmer@yandex-team.ru>
2023-01-16 20:33:53 +0300

    [util][ysaveload] Класс-помощник для сериализации иерархии классов
    https://clubs.at.yandex-team.ru/velo/1637/1642#reply-velo-1647
    
    ref:82382ad98795b5e0ef1c1c1d2286302cf7f3bf8e

--- util/ysaveload.h
+++ util/ysaveload.h
@@ -720,3 +720,17 @@ static inline void LoadMany(S* s, Ts&... t) {
     void Load(IInputStream* s) override {        \
         ::LoadMany(s, __VA_ARGS__);              \
     }
+
+template <class T>
+struct TNonVirtualSaver {
+    const T* Data;
+    void Save(IOutputStream* out) const {
+        Data->T::Save(out);
+    }
+};
+
+template <typename S, typename T, typename... R>
+inline void LoadMany(S* s, TNonVirtualSaver<T> t, R&... r) {
+    const_cast<T*>(t.Data)->T::Load(s);
+    ::LoadMany(s, r...);
+}
