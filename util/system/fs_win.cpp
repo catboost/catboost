68b7fbd27493f3f747dca11c2187162b431f21b4
kikht <kikht@yandex-team.ru>
2023-01-16 23:59:24 +0300

    [util] fix fstat for archive directories on windows
    Currently TFileStat has a bunch of problems on Windows:
    1. Mode computation ORs different types, but file attributes it checks are not mutually exclusive. E.g. it is possible for directory to have both `FILE_ATTRIBUTE_DIRECTORY` and `FILE_ATTRIBUTE_ARCHIVE`, but it will currently lead to invalid `_S_IFDIR | _S_IFREG` mode.
    2. Any file with `FILE_ATTRIBUTE_REPARSE_POINT` flag is considered to be symlink. But there are many other types of reparse points even user-defined ones. For more info see https://docs.microsoft.com/en-us/windows/win32/fileio/reparse-point-tags
    
    To fix all this we do the following:
    1. Add `ReparseTag` field to `TSystemFStat` and fill it for reparse points in `GetStatByHandle`.
    2. Refactor `GetWinFileType` out of `GetFileMode` to ensure that we always select only single `S_IFMT` value.
    3. Pass reparse tag value into `GetWinFileType` and select `S_IFLNK` only for `IO_REPARSE_TAG_SYMLINK` and `IO_REPARSE_TAG_MOUNT_POINT` tags. The latter one is a bit strange, but this behaviour is aligned with current implementation of `WinReadLink`.
    4. Factor `ReadReparsePoint` out of `WinReadLink`. This function uses `DeviceIoControl` to read reparse point data into structures copied from WDK.
    5. Add `WinReadReparseTag` that uses `ReadReparsePoint` to extract reparse point tag. We use this approach instead of MSDN-recommended `FindFirstFile` because latter requires file path, but we have file handle at this point. AFAIK golang and python use similar approach for this.
    6. Add test for archived directory case.
    7. Make symlink nofollow tests run only when symlink creation is possible for current user.
    
    Discovered in https://st.yandex-team.ru/ARC-3931
    
    ref:56e3914f6369caab3bbbd0f335c278c3aa082b21

--- util/system/fs_win.cpp
+++ util/system/fs_win.cpp
@@ -180,34 +180,52 @@ namespace NFsPrivate {
 
     // the end of edited part of <Ntifs.h>
 
-    TString WinReadLink(const TString& name) {
-        TFileHandle h = CreateFileWithUtf8Name(name, GENERIC_READ, FILE_SHARE_READ, OPEN_EXISTING,
-                                               FILE_FLAG_OPEN_REPARSE_POINT | FILE_FLAG_BACKUP_SEMANTICS, true);
-        TTempBuf buf;
+    // For more info see:
+    // * https://docs.microsoft.com/en-us/windows/win32/fileio/reparse-points
+    // * https://docs.microsoft.com/en-us/windows-hardware/drivers/ifs/fsctl-get-reparse-point
+    // * https://docs.microsoft.com/en-us/windows-hardware/drivers/ddi/ntifs/ns-ntifs-_reparse_data_buffer
+    REPARSE_DATA_BUFFER* ReadReparsePoint(HANDLE h, TTempBuf& buf) {
         while (true) {
             DWORD bytesReturned = 0;
             BOOL res = DeviceIoControl(h, FSCTL_GET_REPARSE_POINT, nullptr, 0, buf.Data(), buf.Size(), &bytesReturned, nullptr);
             if (res) {
                 REPARSE_DATA_BUFFER* rdb = (REPARSE_DATA_BUFFER*)buf.Data();
-                if (rdb->ReparseTag == IO_REPARSE_TAG_SYMLINK) {
-                    wchar16* str = (wchar16*)&rdb->SymbolicLinkReparseBuffer.PathBuffer[rdb->SymbolicLinkReparseBuffer.SubstituteNameOffset / sizeof(wchar16)];
-                    size_t len = rdb->SymbolicLinkReparseBuffer.SubstituteNameLength / sizeof(wchar16);
-                    return WideToUTF8(str, len);
-                } else if (rdb->ReparseTag == IO_REPARSE_TAG_MOUNT_POINT) {
-                    wchar16* str = (wchar16*)&rdb->MountPointReparseBuffer.PathBuffer[rdb->MountPointReparseBuffer.SubstituteNameOffset / sizeof(wchar16)];
-                    size_t len = rdb->MountPointReparseBuffer.SubstituteNameLength / sizeof(wchar16);
-                    return WideToUTF8(str, len);
-                }
-                //this reparse point is unsupported in arcadia
-                return TString();
+                return rdb;
             } else {
                 if (GetLastError() == ERROR_INSUFFICIENT_BUFFER) {
                     buf = TTempBuf(buf.Size() * 2);
                 } else {
-                    ythrow yexception() << "can't read link " << name;
+                    return nullptr;
                 }
             }
         }
+    }
+
+    TString WinReadLink(const TString& name) {
+        TFileHandle h = CreateFileWithUtf8Name(name, GENERIC_READ, FILE_SHARE_READ, OPEN_EXISTING,
+                                               FILE_FLAG_OPEN_REPARSE_POINT | FILE_FLAG_BACKUP_SEMANTICS, true);
+        TTempBuf buf;
+        REPARSE_DATA_BUFFER* rdb = ReadReparsePoint(h, buf);
+        if (rdb == nullptr) {
+            ythrow TIoSystemError() << "can't read reparse point " << name;
+        }
+        if (rdb->ReparseTag == IO_REPARSE_TAG_SYMLINK) {
+            wchar16* str = (wchar16*)&rdb->SymbolicLinkReparseBuffer.PathBuffer[rdb->SymbolicLinkReparseBuffer.SubstituteNameOffset / sizeof(wchar16)];
+            size_t len = rdb->SymbolicLinkReparseBuffer.SubstituteNameLength / sizeof(wchar16);
+            return WideToUTF8(str, len);
+        } else if (rdb->ReparseTag == IO_REPARSE_TAG_MOUNT_POINT) {
+            wchar16* str = (wchar16*)&rdb->MountPointReparseBuffer.PathBuffer[rdb->MountPointReparseBuffer.SubstituteNameOffset / sizeof(wchar16)];
+            size_t len = rdb->MountPointReparseBuffer.SubstituteNameLength / sizeof(wchar16);
+            return WideToUTF8(str, len);
+        }
+        //this reparse point is unsupported in arcadia
+        return TString();
+    }
+
+    ULONG WinReadReparseTag(HANDLE h) {
+        TTempBuf buf;
+        REPARSE_DATA_BUFFER* rdb = ReadReparsePoint(h, buf);
+        return rdb ? rdb->ReparseTag : 0;
     }
 
     // we can't use this function to get an analog of unix inode due to a lot of NTFS folders do not have this GUID
