f688298c4230ddef6e1232e30e2b8e28d6f6efa6
thegeorg <thegeorg@yandex-team.ru>
2023-01-16 06:29:02 +0300

    Remove abi namespace from type names
    ref:a07b636af2f712224331fcb60970bd4d7fa89da6

--- util/system/type_name.cpp
+++ util/system/type_name.cpp
@@ -8,6 +8,22 @@
 
 #include "type_name.h"
 
+namespace {
+
+#if defined(_LIBCPP_VERSION)
+    // libc++ is nested under std::__y1
+    constexpr std::string_view STD_ABI_PREFIX = "std::__y1::";
+#elif defined(_linux_)
+    // libstdc++ is nested under std::__cxx11
+    // FIXME: is there any way to test if we are building against libstdc++?
+    constexpr std::string_view STD_ABI_PREFIX = "std::__cxx11::";
+#else
+    // There is no need to cutoff ABI prefix on Windows
+#endif
+    constexpr std::string_view STD_PREFIX = "std::";
+
+} // anonymous namespace
+
 const char* NPrivate::TCppDemangler::Demangle(const char* name) {
 #ifndef __GNUC__
     return name;
@@ -28,9 +44,17 @@ TString CppDemangle(const TString& name) {
 }
 
 TString TypeName(const std::type_info& typeInfo) {
-    return CppDemangle(typeInfo.name()); // NOLINT(arcadia-typeid-name-restriction)
+    TString demangled = CppDemangle(typeInfo.name()); // NOLINT(arcadia-typeid-name-restriction)
+#if defined(_linux_) || defined(_darwin_)
+    SubstGlobal(demangled, STD_ABI_PREFIX, STD_PREFIX);
+#endif
+    return demangled;
 }
 
 TString TypeName(const std::type_index& typeIndex) {
-    return CppDemangle(typeIndex.name());
+    TString demangled = CppDemangle(typeIndex.name());
+#if defined(_linux_) || defined(_darwin_)
+    SubstGlobal(demangled, STD_ABI_PREFIX, STD_PREFIX);
+#endif
+    return demangled;
 }
