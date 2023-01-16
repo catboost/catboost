9a68139c62b7fbef8ddb3352159bda68473ea096
robot-contrib <robot-contrib@yandex-team.ru>
2023-01-16 11:06:38 +0300

    Update contrib/libs/re2 to 2021-11-01
    ref:dfdd5699c9ea49e3e028e66e37b3c38f4d98a5ea

--- contrib/libs/re2/re2/walker-inl.h
+++ contrib/libs/re2/re2/walker-inl.h
@@ -148,7 +148,8 @@ template<typename T> void Regexp::Walker<T>::Reset() {
   if (!stack_.empty()) {
     LOG(DFATAL) << "Stack not empty.";
     while (!stack_.empty()) {
-      delete[] stack_.top().child_args;
+      if (stack_.top().re->nsub_ > 1)
+        delete[] stack_.top().child_args;
       stack_.pop();
     }
   }
@@ -169,7 +170,7 @@ template<typename T> T Regexp::Walker<T>::WalkInternal(Regexp* re, T top_arg,
   for (;;) {
     T t;
     s = &stack_.top();
-    Regexp* re = s->re;
+    re = s->re;
     switch (s->n) {
       case -1: {
         if (--max_visits_ < 0) {
