a0ca943e3f33e8fd659249f50553c88eda10a60a
Andrey Khalyavin <halyavin@gmail.com>
2023-01-16 03:04:02 +0300

    Update libc++ to revision 81b6aa0e (30 Oct 2020).
    Notable changes:
    * switch-based std::variant implementation
    * don't truncated intermediate values to wchar_t which is 16 bit on Windows
    * remove dependency of random on iostream
    * clean up aligned/sized allocation and deallocation logic
    * make __clear_and_shrink() maintain string invariants
    * simplify macro definitions for the debug mode
    
    ref:50180cae0fb07a99af3d07e4b02b1796e1084280

--- contrib/libs/cxxsupp/libcxx/include/locale.h
+++ contrib/libs/cxxsupp/libcxx/include/locale.h
@@ -35,6 +35,10 @@ Functions:
 
 #include <__config>
 
+#if defined(_LIBCPP_HAS_NO_LOCALIZATION)
+#   error "Localization is not supported by this configuration of libc++"
+#endif
+
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #pragma GCC system_header
 #endif
