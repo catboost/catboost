420bcfe64788adf6218a6b3dea0bb70dc3731448
tobo <tobo@yandex-team.ru>
2023-01-16 19:05:18 +0300

    fix link
    Note: mandatory check (NEED_CHECK) was skipped
    ref:b2b5ce68fc11aedba0881831d883b865b9eb4874

--- util/system/env.cpp
+++ util/system/env.cpp
@@ -18,7 +18,7 @@
  *
  * Relevant links:
  *  - http://bugs.python.org/issue16633
- *  - https://rb.yandex-team.ru/arc/r/108892/
+ *  - https://a.yandex-team.ru/review/108892/details
  */
 
 TString GetEnv(const TString& key, const TString& def) {
