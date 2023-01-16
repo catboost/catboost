1222f1938f4a09dbc2f8103c305e2b00a374fb9b
snermolaev <snermolaev@yandex-team.ru>
2023-01-16 07:45:01 +0300

    always add .fbs.h when --add-flatbuf-result
    ref:7637f13eb939a999cc5b61648621e55702a40303

--- build/scripts/cpp_flatc_wrapper.py
+++ build/scripts/cpp_flatc_wrapper.py
@@ -5,9 +5,11 @@ import sys
 
 def main():
     cmd = sys.argv[1:]
+    h_file = None
     try:
         index = cmd.index('-o')
-        cmd[index+1] = os.path.dirname(cmd[index+1])
+        h_file = cmd[index+1]
+        cmd[index+1] = os.path.dirname(h_file)
     except (ValueError, IndexError):
         pass
     p = subprocess.Popen(cmd, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -17,7 +19,12 @@ def main():
             sys.stderr.write('stdout:\n{}\n'.format(out))
         if err:
             sys.stderr.write('stderr:\n{}\n'.format(err))
-    sys.exit(p.returncode)
+        sys.exit(p.returncode)
+    if h_file and h_file.endswith(('.fbs.h', '.fbs64.h')):
+        cpp_file = '{}.cpp'.format(h_file[:-2])
+        with open(cpp_file, 'w') as f:
+            f.write('#include "{}"\n'.format(os.path.basename(h_file)))
+    sys.exit(0)
 
 
 if __name__ == '__main__':
