9a7805b9b152ada75b87e4f50e050764df4efc16
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2023-01-16 08:35:41 +0300

    intermediate changes ref:9f6d50ed1e1b037b6b4091a7b80e1fe7cc478b90

--- contrib/python/filelock/py2/filelock/__init__.py
+++ contrib/python/filelock/py2/filelock/__init__.py
@@ -1,5 +1,9 @@
 """
 A platform independent file lock that supports the with-statement.
+
+.. autodata:: filelock.__version__
+   :no-value:
+
 """
 import sys
 import warnings
@@ -9,29 +13,33 @@ from ._error import Timeout
 from ._soft import SoftFileLock
 from ._unix import UnixFileLock, has_fcntl
 from ._windows import WindowsFileLock
-from .version import version as __version__
+from .version import version
 
-#: Alias for the lock, which should be used for the current platform. On Windows, this is an alias for
-# :class:`WindowsFileLock`, on Unix for :class:`UnixFileLock` and otherwise for :class:`SoftFileLock`.
-FileLock = None
+#: version of the project as a string
+__version__ = version
+
 
 if sys.platform == "win32":
-    FileLock = WindowsFileLock
+    _FileLock = WindowsFileLock
 elif has_fcntl:
-    FileLock = UnixFileLock
+    _FileLock = UnixFileLock
 else:
-    FileLock = SoftFileLock
-
+    _FileLock = SoftFileLock
     if warnings is not None:
         warnings.warn("only soft file lock is available")
 
+#: Alias for the lock, which should be used for the current platform. On Windows, this is an alias for
+# :class:`WindowsFileLock`, on Unix for :class:`UnixFileLock` and otherwise for :class:`SoftFileLock`.
+FileLock = _FileLock
+
+
 __all__ = [
-    "Timeout",
-    "BaseFileLock",
+    "__version__",
+    "FileLock",
+    "SoftFileLock",
     "WindowsFileLock",
     "UnixFileLock",
-    "SoftFileLock",
-    "FileLock",
+    "BaseFileLock",
+    "Timeout",
     "AcquireReturnProxy",
-    "__version__",
 ]
