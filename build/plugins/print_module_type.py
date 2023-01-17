0340455e30e1ffdc48e018fc324160d48bc647c0
spreis <spreis@yandex-team.ru>
2023-01-17 18:15:16 +0300

    Remove weird kiwi-specific macro
    ref:912558fc12bb7c291f0cb0e92e7787d6da931d1c

--- build/plugins/print_module_type.py
+++
@@ -1,5 +0,0 @@
-def onprint_module_type(unit, *args):
-    filepath = unit.get('KIWI_OUT_FILE')
-    if len(args) >= 2 and filepath is not None:
-        with open(filepath, "a") as file_handler:
-            print >>file_handler, "{0} {1} {2}".format(args[0], args[1], unit.path())
