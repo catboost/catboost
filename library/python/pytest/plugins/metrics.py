b7d2a02c5dc869a69a7f6b89982caa374f43fb81
qrort <qrort@yandex-team.ru>
2023-01-17 16:48:48 +0300

    KIKIMR-16114: yatest_common context file test
    ref:db9c47fe366b64fcafe29062f39ad357e0f1983c

---
+++ library/python/pytest/plugins/metrics.py
@@ -0,0 +1,10 @@
+class TestMetrics:
+    metrics = {}
+    def __getitem__(self, key):
+        return self.metrics.__getitem__(key)
+    def __setitem__(self, key, value):
+        return self.metrics.__setitem__(key, value)
+    def get(self, key):
+        return self.metrics.get(key)
+
+test_metrics = TestMetrics()
