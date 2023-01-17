a8c5c44c0f34af2c200c6b23c3f351d0b36309a2
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2023-01-17 09:59:40 +0300

    intermediate changes ref:58ec58fffb103e615bf84e4bd9a866f691cd5ab7

--- library/cpp/archive/models_archive_reader.h
+++ library/cpp/archive/models_archive_reader.h
@@ -2,6 +2,7 @@
 
 #include <util/generic/fwd.h>
 #include <util/generic/ptr.h>
+#include <util/generic/hash_set.h>
 
 class IInputStream;
 
@@ -17,4 +18,14 @@ public:
     virtual TBlob ObjectBlobByKey(const TStringBuf key) const = 0;
     virtual TBlob BlobByKey(const TStringBuf key) const = 0;
     virtual bool Compressed() const = 0;
+    virtual THashSet<TStringBuf> FilterByPrefix(TStringBuf prefix, TStringBuf suffix) const {
+        THashSet<TStringBuf> result;
+        for (size_t ind = 0; ind < Count(); ++ind) { 
+                TStringBuf path = KeyByIndex(ind);
+                if (path.StartsWith(prefix) && path.EndsWith(suffix)) {
+                    result.insert(path);
+                }
+        }
+        return result;
+    }
 };
