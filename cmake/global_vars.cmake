895c0fee063f6b7565706e208532e94949e3212a
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2023-01-17 10:48:42 +0300

    intermediate changes ref:96e07831ddb4b272bc0e5c3088344dabed1be25c

--- cmake/global_vars.cmake
+++ cmake/global_vars.cmake
@@ -29,3 +29,33 @@ if(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64" AND UNIX AND NOT APPLE AND NOT ANDR
   set(RAGEL_FLAGS -L -I ${CMAKE_SOURCE_DIR}/)
 endif()
 
+if(ANDROID AND CMAKE_ANDROID_ARCH STREQUAL "arm")
+  set(FAT_OBJECT_SUFFIX .a)
+  set(FAT_OBJECT_PREFIX lib)
+  set(FBS_CPP_FLAGS --no-warnings --cpp --keep-prefix --gen-mutable --schema -b --yandex-maps-iter --gen-object-api --filename-suffix .fbs)
+  set(RAGEL_FLAGS -L -I ${CMAKE_SOURCE_DIR}/)
+endif()
+
+if(ANDROID AND CMAKE_ANDROID_ARCH STREQUAL "arm64")
+  set(FAT_OBJECT_SUFFIX .a)
+  set(FAT_OBJECT_PREFIX lib)
+  set(FBS_CPP_FLAGS --no-warnings --cpp --keep-prefix --gen-mutable --schema -b --yandex-maps-iter --gen-object-api --filename-suffix .fbs)
+  set(RAGEL_FLAGS -L -I ${CMAKE_SOURCE_DIR}/)
+endif()
+
+if(ANDROID AND CMAKE_ANDROID_ARCH STREQUAL "x86")
+  set(FAT_OBJECT_SUFFIX .a)
+  set(FAT_OBJECT_PREFIX lib)
+  set(YASM_FLAGS -f elf32 -D UNIX -D _i686_ -D_YASM_ -g dwarf2)
+  set(FBS_CPP_FLAGS --no-warnings --cpp --keep-prefix --gen-mutable --schema -b --yandex-maps-iter --gen-object-api --filename-suffix .fbs)
+  set(RAGEL_FLAGS -L -I ${CMAKE_SOURCE_DIR}/)
+endif()
+
+if(ANDROID AND CMAKE_ANDROID_ARCH STREQUAL "x86_64")
+  set(FAT_OBJECT_SUFFIX .a)
+  set(FAT_OBJECT_PREFIX lib)
+  set(YASM_FLAGS -f elf64 -D UNIX -D _x86_64_ -D_YASM_ -g dwarf2)
+  set(FBS_CPP_FLAGS --no-warnings --cpp --keep-prefix --gen-mutable --schema -b --yandex-maps-iter --gen-object-api --filename-suffix .fbs)
+  set(RAGEL_FLAGS -L -I ${CMAKE_SOURCE_DIR}/)
+endif()
+
