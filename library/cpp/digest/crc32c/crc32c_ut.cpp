edf2f12abd19784209f90c5fbd858097c5c19de1
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2023-01-17 07:13:50 +0300

    intermediate changes ref:c3073221ca1e9401d9d76eebd38922bb982e0b6b

--- library/cpp/digest/crc32c/crc32c_ut.cpp
+++ library/cpp/digest/crc32c/crc32c_ut.cpp
@@ -20,4 +20,12 @@ Y_UNIT_TEST_SUITE(TestCrc32c) {
     Y_UNIT_TEST(TestExtend) {
         UNIT_ASSERT_VALUES_EQUAL(Crc32cExtend(1, "abc", 3), ui32(2466950601));
     }
+
+    Y_UNIT_TEST(TestCombine) {
+        ui32 aSum = Crc32c("abc", 3);
+        ui32 bSum = Crc32c("de", 2);
+        ui32 abSum = Crc32c("abcde", 5);
+
+        UNIT_ASSERT_VALUES_EQUAL(Crc32cCombine(aSum, bSum, 2), abSum);
+    }
 }
