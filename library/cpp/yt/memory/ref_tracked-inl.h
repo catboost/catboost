6b1ef3bb761589f0929b98b8d7d0b73a03191b3e
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2023-01-17 16:44:55 +0300

    intermediate changes ref:bc8259405482b4dd0b0da235f73b9692e878f05c

--- library/cpp/yt/memory/ref_tracked-inl.h
+++ library/cpp/yt/memory/ref_tracked-inl.h
@@ -18,13 +18,13 @@ template <class T>
 Y_FORCE_INLINE TRefCountedTypeCookie GetRefCountedTypeCookie()
 {
     static std::atomic<TRefCountedTypeCookie> cookie{NullRefCountedTypeCookie};
-    auto cookieValue = cookie.load(std::memory_order_relaxed);
+    auto cookieValue = cookie.load(std::memory_order::relaxed);
     if (Y_UNLIKELY(cookieValue == NullRefCountedTypeCookie)) {
         cookieValue = TRefCountedTrackerFacade::GetCookie(
             GetRefCountedTypeKey<T>(),
             sizeof(T),
             NYT::TSourceLocation());
-        cookie.store(cookieValue, std::memory_order_relaxed);
+        cookie.store(cookieValue, std::memory_order::relaxed);
     }
     return cookieValue;
 }
@@ -33,13 +33,13 @@ template <class T, class TTag, int Counter>
 Y_FORCE_INLINE TRefCountedTypeCookie GetRefCountedTypeCookieWithLocation(const TSourceLocation& location)
 {
     static std::atomic<TRefCountedTypeCookie> cookie{NullRefCountedTypeCookie};
-    auto cookieValue = cookie.load(std::memory_order_relaxed);
+    auto cookieValue = cookie.load(std::memory_order::relaxed);
     if (Y_UNLIKELY(cookieValue == NullRefCountedTypeCookie)) {
         cookieValue = TRefCountedTrackerFacade::GetCookie(
             GetRefCountedTypeKey<T>(),
             sizeof(T),
             location);
-        cookie.store(cookieValue, std::memory_order_relaxed);
+        cookie.store(cookieValue, std::memory_order::relaxed);
     }
     return cookieValue;
 }
