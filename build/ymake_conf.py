ca0e1f9ca311c790478a1e3e7bd1fbfc6fd130fe
thegeorg <thegeorg@yandex-team.ru>
2023-01-17 21:46:25 +0300

    Switch back to old good OS_SDK=ubuntu-14 by default
    ref:bc44e791b3e306c259965c434ccae2a1cddaeaec

--- build/ymake_conf.py
+++ build/ymake_conf.py
@@ -15,7 +15,6 @@ import re
 import subprocess
 import sys
 import tempfile
-import getpass
 
 import six
 
@@ -1076,14 +1075,6 @@ class GnuToolchainOptions(ToolchainOptions):
                 return 'ubuntu-14'
 
             # Default OS SDK for Linux builds
-            # Use fresh SDK for any execution except of automated run on Distbuild and Sandbox.
-            # This is temporary code for first stage of DEVTOOLS-9797
-            try:
-                user = getpass.getuser()
-                if not user.startswith('db-runner-') and user != 'sandbox':
-                    return 'ubuntu-18'
-            except:
-                pass
             return 'ubuntu-14'
 
 
