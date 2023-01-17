1295f9ba3695916e42638895727fedb1b2be98a0
v-homyakov <v-homyakov@yandex-team.ru>
2023-01-17 14:28:34 +0300

    FBP-191: devtools/frontend_build_platform/nots_plugins → devtools/frontend_build_platform/nots/toolchain_plugins
    ref:06e26da1e1cd02cea9d94e40f5bc1617f49d568a

--- build/plugins/lib/nots/typescript/ts_errors.py
+++ build/plugins/lib/nots/typescript/ts_errors.py
@@ -8,12 +8,3 @@ class TsValidationError(TsError):
         self.errors = errors
 
         super(TsValidationError, self).__init__("Invalid tsconfig {}:\n{}".format(path, "\n".join(errors)))
-
-
-class TsCompilationError(TsError):
-    def __init__(self, code, stdout, stderr):
-        self.code = code
-        self.stdout = stdout
-        self.stderr = stderr
-
-        super(TsCompilationError, self).__init__("tsc exited with code {}:\n{}\n{}".format(code, stdout, stderr))
