4fc837d7703c2e97480fc3758abcd20f7ab41c37
svidyuk <svidyuk@yandex-team.ru>
2023-01-17 06:43:00 +0300

    Configure time check for junit runners conflict
    DEVTOOLSSUPPORT-12150
    DEVTOOLSSUPPORT-7454
    
    ref:afb208fa3612997562e1430cd1b0288b5c5fb942

--- build/scripts/run_junit.py
+++ build/scripts/run_junit.py
@@ -6,11 +6,6 @@ import sys
 
 SHUTDOWN_SIGNAL = 'SIGUSR1'
 
-PROVIDES = {
-    "devtools/junit-runner/devtools-junit-runner.jar": "junit-runner",
-    "devtools/junit5-runner/devtools-junit5-runner.jar": "junit-runner",
-}
-
 
 class SignalInterruptionError(Exception):
     pass
@@ -20,60 +15,8 @@ def on_shutdown(s, f):
     raise SignalInterruptionError()
 
 
-def get_tracefile_path(args):
-    return args[args.index('--output') + 1]
-
-
-def dump_chunk_error(tracefile, name, imps):
-    with open(tracefile, 'a') as afile:
-        msg = {
-            "timestamp": time.time(),
-            "name": "chunk-event",
-            "value": {
-                "errors": [
-                    [
-                        "fail",
-                        "[[bad]]Test contains conflicting dependencies for [[imp]]{}[[bad]]: {}[[rst]]".format(
-                            name, ', '.join(imps)
-                        ),
-                    ],
-                ],
-            },
-        }
-        json.dump(msg, afile)
-        afile.write("\n")
-
-
-def verify_classpath(args):
-    cpfile = args[args.index('-classpath') + 1]
-    assert cpfile.startswith('@'), cpfile
-
-    cpfile = cpfile[1:]
-    assert os.path.exists(cpfile)
-
-    with open(cpfile) as afile:
-        data = afile.read().splitlines()
-
-    collisions = collections.defaultdict(set)
-    for cp in data:
-        if cp in PROVIDES:
-            collisions[PROVIDES[cp]].add(cp)
-
-    for name, imps in collisions.items():
-        if len(imps) > 1:
-            tracefile = get_tracefile_path(args)
-            dump_chunk_error(tracefile, name, imps)
-            return False
-    return True
-
-
 def main():
     args = sys.argv[1:]
-
-    # Emulates PROVIDES(X) for junit-runner and junit5-runner.
-    # For more info see DEVTOOLSSUPPORT-7454
-    if not verify_classpath(args):
-        return 1
 
     def execve():
         os.execve(args[0], args, os.environ)
