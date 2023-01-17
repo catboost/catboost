6bcb45d97c4208afe39d9f687d5dddf8c65d7f56
thegeorg <thegeorg@yandex-team.ru>
2023-01-17 08:28:21 +0300

    util: Drop NO_CITYHASH point of customisation
    ref:e93333748b1e3fc0a5b0b93fb0b3d4f9f329cb8f

--- util/generic/string_hash.h
+++ util/generic/string_hash.h
@@ -3,7 +3,7 @@
 #include <cstddef>
 
 // reduce code bloat and cycled includes, declare functions here
-#if defined(_64_) && !defined(NO_CITYHASH)
+#if defined(_64_)
 ui64 CityHash64(const char* buf, size_t len) noexcept;
 #else
 template <typename T>
@@ -13,7 +13,7 @@ T MurmurHash(const void* buf, size_t len) noexcept;
 namespace NHashPrivate {
     template <typename C>
     size_t ComputeStringHash(const C* ptr, size_t size) noexcept {
-#if defined(_64_) && !defined(NO_CITYHASH)
+#if defined(_64_)
         return CityHash64((const char*)ptr, size * sizeof(C));
 #else
         return MurmurHash<size_t>(ptr, size * sizeof(C));
