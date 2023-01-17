7afab1ea365c51adb9409a1dc5841f0df6837dae
thegeorg <thegeorg@yandex-team.ru>
2023-01-18 00:49:37 +0300

    Fix reimport for contrib/libs/lzma
    ref:73bc3fb0f99dff763dc03cde18925602579c2ef1

--- contrib/libs/lzma/common/config.h
+++ contrib/libs/lzma/common/config.h
@@ -1,21 +1,15 @@
 #pragma once
 
-#if defined(__APPLE__)
+#if defined(__ANDROID__) && defined(__arm__)
+#   include "config-android-arm.h"
+#elif defined(__ANDROID__) && defined(__aarch64__)
+#   include "config-android-arm64.h"
+#elif defined(__ANDROID__) && defined(__i686__)
+#   include "config-android-i686.h"
+#elif defined(__APPLE__)
 #   include "config-osx.h"
 #elif defined(_MSC_VER)
 #   include "config-win.h"
 #else
 #   include "config-linux.h"
-#endif
-
-#if defined(__ANDROID__) && defined(__arm__)
-#   include "config-android-arm.h"
-#endif
-
-#if defined(__ANDROID__) && defined(__aarch64__)
-#   include "config-android-arm64.h"
-#endif
-
-#if defined(__ANDROID__) && defined(__i686__)
-#   include "config-android-i686.h"
 #endif
