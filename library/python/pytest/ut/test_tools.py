cf2100d361805f9f8bfa211cf93fbadda96587ba
Aleksandr <ivansduck@gmail.com>
2023-01-18 02:43:50 +0300

    [library/python/pytest/ut/test_tools] Fixed style tests
    ref:e43407cbea4559e2de4755697a8aecd9c92cfa26

--- library/python/pytest/ut/test_tools.py
+++ library/python/pytest/ut/test_tools.py
@@ -9,28 +9,42 @@ def parameters(request):
     return request.param
 
 
-@pytest.mark.parametrize("node_id,expected_class_name,expected_test_name",
+@pytest.mark.parametrize(
+    "node_id,expected_class_name,expected_test_name",
     (
         ("package.test_script.py::test_name", "package.test_script.py", "test_name"),
         ("package.test_script.py", "package.test_script.py", "package.test_script.py"),
         ("package.test_script.py::class_name::test_name", "package.test_script.py::class_name", "test_name"),
-        ("package.test_script.py::class_name::subclass_name::test_name", "package.test_script.py::class_name", "test_name"),
-    )
+        (
+            "package.test_script.py::class_name::subclass_name::test_name",
+            "package.test_script.py::class_name",
+            "test_name",
+        ),
+    ),
 )
 def test_split_node_id_without_path(parameters, node_id, expected_class_name, expected_test_name):
     got = split_node_id(node_id + parameters)
     assert (expected_class_name, expected_test_name + parameters) == got
 
 
-@pytest.mark.parametrize("node_id,expected_class_name,expected_test_name",
+@pytest.mark.parametrize(
+    "node_id,expected_class_name,expected_test_name",
     (
         ("/arcadia/root/package/test_script.py", "package.test_script.py", "package.test_script.py"),
-        ("/arcadia/root/package/test_script.py::test_name","package.test_script.py", "test_name"),
-        ("/arcadia/root/package/test_script.py::class_name::test_name", "package.test_script.py::class_name", "test_name"),
-        ("/arcadia/root/package/test_script.py::class_name::subclass_name::test_name", "package.test_script.py::class_name", "test_name"),
+        ("/arcadia/root/package/test_script.py::test_name", "package.test_script.py", "test_name"),
+        (
+            "/arcadia/root/package/test_script.py::class_name::test_name",
+            "package.test_script.py::class_name",
+            "test_name",
+        ),
+        (
+            "/arcadia/root/package/test_script.py::class_name::subclass_name::test_name",
+            "package.test_script.py::class_name",
+            "test_name",
+        ),
         # If module is not found in sys.extra_modules use basename as a class name
         ("/arcadia/root/package/test_script2.py::test_name", "test_script2.py", "test_name"),
-    )
+    ),
 )
 def test_split_node_id_with_path(mocker, parameters, node_id, expected_class_name, expected_test_name):
     mocker.patch.object(sys, 'extra_modules', sys.extra_modules | {'__tests__.package.test_script'})
@@ -38,13 +52,14 @@ def test_split_node_id_with_path(mocker, parameters, node_id, expected_class_nam
     assert (expected_class_name, expected_test_name + parameters) == got
 
 
-@pytest.mark.parametrize("node_id,expected_class_name,expected_test_name",
+@pytest.mark.parametrize(
+    "node_id,expected_class_name,expected_test_name",
     (
         ("package.test_script.py::test_name", "package.test_script.py", "test_suffix"),
         ("package.test_script.py", "package.test_script.py", "test_suffix"),
         ("package.test_script.py::class_name::test_name", "package.test_script.py", "test_suffix"),
         ("package.test_script.py::class_name::subclass_name::test_name", "package.test_script.py", "test_suffix"),
-    )
+    ),
 )
 def test_split_node_id_with_test_suffix(mocker, parameters, node_id, expected_class_name, expected_test_name):
     got = split_node_id(node_id + parameters, "test_suffix")
