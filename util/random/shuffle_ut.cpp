3a2541514c4263d3bede0af0d4ca3c807db37039
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2023-01-17 08:04:27 +0300

    Pull request "ShufflePart function" by @igormarkov00 from https://github.com/catboost/catboost/pull/2087
    MERGED FROM https://github.com/catboost/catboost/pull/2087
    
    ref:ddacfb2ce4ebddc61722bf718edb28aee99e01df

--- util/random/shuffle_ut.cpp
+++ util/random/shuffle_ut.cpp
@@ -45,31 +45,70 @@ Y_UNIT_TEST_SUITE(TRandUtilsTest) {
         UNIT_ASSERT(s0 != s1); // if shuffle does work, chances it will fail are 1 to 64!.
     }
 
+    template <typename... A>
+    static void TestIterPartial(A&&... args) {
+        TString s0, s1;
+
+        auto f = [&](int shuffledSize) {
+            auto b = s1.begin();
+            auto e = s1.end();
+
+            PartialShuffle(b, e, shuffledSize, args...);
+        };
+
+        s1 = "";
+        f(0);
+
+        s1 = "01";
+        f(1);
+
+        s1 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.;?!";
+        s0 = s1.copy();
+        f(2);
+        size_t matchesCount = 0;
+        for (size_t i = 0; i < s0.size(); ++i) {
+            matchesCount += (s0[i] == s1[i]);
+        }
+
+        UNIT_ASSERT(matchesCount >= s1.size() - 4); // partial shuffle doesn't make non-necessary swaps.
+
+        s1 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.;?!";
+        s0 = s1.copy();
+        f(64);
+
+        UNIT_ASSERT(s0 != s1); // if partial shuffle does work, chances it will fail are 1 to 64!.
+    }
+
     Y_UNIT_TEST(TestShuffle) {
         TestRange();
+        TestIterPartial();
     }
 
     Y_UNIT_TEST(TestShuffleMersenne64) {
         TMersenne<ui64> prng(42);
 
         TestRange(prng);
+        TestIterPartial(prng);
     }
 
     Y_UNIT_TEST(TestShuffleMersenne32) {
         TMersenne<ui32> prng(24);
 
         TestIter(prng);
+        TestIterPartial(prng);
     }
 
     Y_UNIT_TEST(TestShuffleFast32) {
         TFastRng32 prng(24, 0);
 
         TestIter(prng);
+        TestIterPartial(prng);
     }
 
     Y_UNIT_TEST(TestShuffleFast64) {
         TFastRng64 prng(24, 0, 25, 1);
 
         TestIter(prng);
+        TestIterPartial(prng);
     }
 }
