fd41f2f867bd1129485b454af294e153c35834ab
thegeorg <thegeorg@yandex-team.ru>
2023-01-18 00:53:33 +0300

    Revert "Simplify openssl reimport routine a bit"
    This reverts commit 905b7005871eafe90a754a645585c0bdfed2e12d, reversing
    changes made to 16c1821065eaffe292749ecfac874ad0d4f17977.
    
    ref:6564c53773021d62d4860e14f581dba3a701fb4c

--- contrib/libs/openssl/apps/progs.h
+++ contrib/libs/openssl/apps/progs.h
@@ -1,7 +1,5 @@
-#pragma once
-
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && defined(_M_X64)
 #   include "progs-win.h"
 #else
-#   include "progs-linux.h"
+#   include "progs-linux_aarch64-linux-osx_arm64.h"
 #endif
