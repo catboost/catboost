fa5125d9fec39fa52a8808ccc645e6f87cb27541
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2023-01-17 16:05:06 +0300

    intermediate changes ref:baa696b0adc724cc53ea203cfbada82ff1f9639a

--- contrib/python/scipy/py3/scipy/linalg/_flinalgmodule.c
+++ contrib/python/scipy/py3/scipy/linalg/_flinalgmodule.c
@@ -1955,7 +1955,7 @@ PyMODINIT_FUNC init_flinalg(void) {
 #else
   m = _flinalg_module = Py_InitModule("_flinalg", f2py_module_methods);
 #endif
-  Py_TYPE(&PyFortran_Type) = &PyType_Type;
+  Py_SET_TYPE(&PyFortran_Type, &PyType_Type);
   import_array();
   if (PyErr_Occurred())
     {PyErr_SetString(PyExc_ImportError, "can't initialize module _flinalg (failed to import numpy)"); return RETVAL;}
