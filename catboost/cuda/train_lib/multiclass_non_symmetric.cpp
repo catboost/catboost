158ec34378411cc98230b98486c685c4a0713aad
espetrov <espetrov@yandex-team.com>
2023-02-08 16:33:02 +0300

    Enable RMSEWithUncertainty for Lossguide and Depthwise tree grow policy
    MLTOOLS-6100

--- catboost/cuda/train_lib/multiclass_non_symmetric.cpp
+++ catboost/cuda/train_lib/multiclass_non_symmetric.cpp
@@ -5,7 +5,10 @@ namespace NCatboostCuda {
     using TMultiClassTrainer = TGpuTrainer<TMultiClassificationTargets, TNonSymmetricTree>;
     TGpuTrainerFactory::TRegistrator<TMultiClassTrainer> MulticlassLossguideRegistrator(GetTrainerFactoryKey(ELossFunction::MultiClass, EGrowPolicy::Lossguide));
     TGpuTrainerFactory::TRegistrator<TMultiClassTrainer> MulticlassOneVsAllLossguideRegistrator(GetTrainerFactoryKey(ELossFunction::MultiClassOneVsAll, EGrowPolicy::Lossguide));
+    TGpuTrainerFactory::TRegistrator<TMultiClassTrainer> RMSEWithUncertaintyLossguideRegistrator(GetTrainerFactoryKey(ELossFunction::RMSEWithUncertainty, EGrowPolicy::Lossguide));
 
     TGpuTrainerFactory::TRegistrator<TMultiClassTrainer> MulticlassDepthwiseRegistrator(GetTrainerFactoryKey(ELossFunction::MultiClass, EGrowPolicy::Depthwise));
     TGpuTrainerFactory::TRegistrator<TMultiClassTrainer> MulticlassOneVsAllDepthwiseRegistrator(GetTrainerFactoryKey(ELossFunction::MultiClassOneVsAll, EGrowPolicy::Depthwise));
+    TGpuTrainerFactory::TRegistrator<TMultiClassTrainer> RMSEWithUncertaintyDepthwiseRegistrator(GetTrainerFactoryKey(ELossFunction::RMSEWithUncertainty, EGrowPolicy::Depthwise));
+
 }
