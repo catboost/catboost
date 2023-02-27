371a415c431de7ee0df100deb3e683a62c97e7ca
nik-bes <nik-bes@yandex-team.com>
2023-02-28 00:29:49 +0300

    pandas - fix gc track of timestamp

--- contrib/python/pandas/py3/pandas/_libs/tslibs/base.pyx
+++ contrib/python/pandas/py3/pandas/_libs/tslibs/base.pyx
@@ -7,6 +7,10 @@ This is analogous to core.dtypes.generic.
 
 from cpython.datetime cimport datetime
 
+cdef extern from "Python.h":
+    void PyObject_GC_UnTrack(object op)
+
 
 cdef class ABCTimestamp(datetime):
-    pass
+    def __dealloc__(ABCTimestamp self):
+        PyObject_GC_UnTrack(self)
