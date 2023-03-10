ca25f9554e98fa5022bcddc0f836edb47faa4afb
nik-bes <nik-bes@yandex-team.com>
2023-03-10 11:15:40 +0300

    protobuf - fix deprecated import
    https://setuptools.pypa.io/en/latest/references/keywords.html#keyword-namespace-packages

--- contrib/python/protobuf/py3/google/__init__.py
+++ contrib/python/protobuf/py3/google/__init__.py
@@ -1,4 +1,1 @@
-try:
-  __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-  __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
