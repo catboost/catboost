08665608b3f44163ab81cc4bb3ec257094214c46
akhropov <akhropov@yandex-team.com>
2023-03-25 01:03:24 +0300

    Support adding hnsw as submodule with --with-hnsw flag.

--- catboost/python-package/setup.py
+++ catboost/python-package/setup.py
@@ -1,6 +1,7 @@
 import distutils
 import logging
 import os
+import shutil
 import subprocess
 import sys
 from distutils.command.bdist import bdist as _bdist
@@ -13,6 +14,40 @@ from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 SETUP_DIR = os.path.abspath(os.path.dirname(__file__))
 PKG_INFO = 'PKG-INFO'
 EXT_SRC = 'catboost_so_src'
+
+
+def get_topsrc_dir():
+    if os.path.exists(os.path.join(SETUP_DIR, PKG_INFO)):
+        return os.path.join(SETUP_DIR, EXT_SRC)
+    else:
+        return os.path.abspath(os.path.join(SETUP_DIR, '..', '..'))
+
+def create_hnsw_submodule(verbose, dry_run):
+    logging.info('Creating hnsw submodule')
+    hnsw_submodule_dir = os.path.join(SETUP_DIR, 'catboost', 'hnsw')
+    if not os.path.exists(hnsw_submodule_dir):
+        hnsw_original_dir = os.path.join(get_topsrc_dir(), 'library', 'python', 'hnsw', 'hnsw')
+        if verbose:
+            logging.info(f'create symlink from {hnsw_original_dir} to {hnsw_submodule_dir}')
+        if not dry_run:
+            # there can be issues on Windows when creating symbolic and hard links
+            try:
+                os.symlink(hnsw_original_dir, hnsw_submodule_dir, target_is_directory=True)
+                return
+            except Exception as exception:
+                logging.error(f'Encountered an error ({str(exception)}) when creating symlink, try to create hardlink instead')
+            
+            if verbose:
+                logging.info(f'create hardlink from {hnsw_original_dir} to {hnsw_submodule_dir}')
+            try:
+                os.link(hnsw_original_dir, hnsw_submodule_dir)
+                return
+            except Exception as exception:
+                logging.error(f'Encountered an error ({str(exception)}) when creating hardlink, just copy instead')
+
+            if verbose:
+                logging.info(f'copy from {hnsw_original_dir} to {hnsw_submodule_dir}')
+            shutil.copytree(hnsw_original_dir, hnsw_submodule_dir, dirs_exist_ok=True)
 
 
 def get_all_cmake_lists(base_dir, add_android=True):
@@ -88,12 +123,14 @@ def python_version():
 class Helper(object):
     options = [
         ('with-cuda=', None, emph('Build with CUDA support (cuda-root|no)')),
+        ('with-hnsw', None, emph('Build with hnsw as catboost submodule')),
         ('parallel=', 'j', emph('Number of parallel build jobs')),
         ('prebuilt-extensions-build-root-dir=', None, emph('Use extensions from CatBoost project prebuilt with CMake')),
     ]
 
     def initialize_options(self):
         self.with_cuda = os.environ.get('CUDA_PATH') or os.environ.get('CUDA_ROOT') or None
+        self.with_hnsw = False
         self.parallel = None
         self.prebuilt_extensions_build_root_dir = None
 
@@ -107,6 +144,7 @@ class Helper(object):
         self.distribution.get_command_obj(subcommand).set_undefined_options(
             origin,
             ("with_cuda", "with_cuda"),
+            ("with_hnsw", "with_hnsw"),
             ("parallel", "parallel"),
             ('prebuilt_extensions_build_root_dir', 'prebuilt_extensions_build_root_dir')
         )
@@ -163,6 +201,13 @@ class bdist_wheel(_bdist_wheel):
         _bdist_wheel.run(self)
 
 
+class ExtensionWithSrcAndDstSubPath(Extension):
+    def __init__(self, name, cmake_build_sub_path, dst_sub_path):
+        super().__init__(name, sources=[])
+        self.cmake_build_sub_path = cmake_build_sub_path
+        self.dst_sub_path = dst_sub_path
+
+
 class build_ext(_build_ext):
 
     user_options = _build_ext.user_options + Helper.options
@@ -175,35 +220,45 @@ class build_ext(_build_ext):
         _build_ext.finalize_options(self)
         Helper.finalize_options(self)
 
-    def run(self):
-        if os.path.exists(os.path.join(SETUP_DIR, PKG_INFO)):
-            topsrc_dir = os.path.join(SETUP_DIR, EXT_SRC)
-        else:
-            topsrc_dir = os.path.join(SETUP_DIR, '..', '..')
-
-        catboost_ext = {
-            'linux': '_catboost.so',
-            'darwin': '_catboost.so',
-            'win32': '_catboost.pyd',
+    @staticmethod
+    def get_cmake_built_extension_filename(ext_name):
+        return {
+            'linux': f'lib{ext_name}.so',
+            'darwin': f'lib{ext_name}.dylib',
+            'win32': f'{ext_name}.dll',
         }[sys.platform]
 
+    @staticmethod
+    def get_extension_suffix():
+        return {
+            'linux': '.so',
+            'darwin': '.so',
+            'win32': '.pyd',
+        }[sys.platform]
+
+    def run(self):
         verbose = self.distribution.verbose
         dry_run = self.distribution.dry_run
 
-        put_dir = os.path.abspath(os.path.join(self.build_lib, 'catboost'))
+        for ext in self.extensions:
+            if not isinstance(ext, ExtensionWithSrcAndDstSubPath):
+                raise RuntimeError('Only ExtensionWithSrcAndDstSubPath extensions are supported')
 
-        distutils.dir_util.mkpath(put_dir, verbose=verbose, dry_run=dry_run)
+            put_dir = os.path.abspath(os.path.join(self.build_lib, ext.dst_sub_path))
+            distutils.dir_util.mkpath(put_dir, verbose=verbose, dry_run=dry_run)
 
         if self.prebuilt_extensions_build_root_dir is not None:
             build_dir = self.prebuilt_extensions_build_root_dir
         else:
             build_dir = os.path.abspath(self.build_temp)
-            self.build_with_cmake_and_ninja(topsrc_dir, build_dir, catboost_ext, verbose, dry_run)
+            self.build_with_cmake_and_ninja(get_topsrc_dir(), build_dir, verbose, dry_run)
 
-        self.copy_artifact_built_with_cmake(build_dir, put_dir, catboost_ext, verbose, dry_run)
+        self.copy_artifacts_built_with_cmake(build_dir, verbose, dry_run)
 
-    def build_with_cmake_and_ninja(self, topsrc_dir, build_dir, catboost_ext, verbose, dry_run):
-        logging.info('Buildling {} with cmake and ninja'.format(catboost_ext))
+    def build_with_cmake_and_ninja(self, topsrc_dir, build_dir, verbose, dry_run):
+        targets = [ext.name for ext in self.extensions]
+
+        logging.info(f'Buildling {",".join(targets)} with cmake and ninja')
 
         sys.path = [os.path.join(topsrc_dir, 'build')] + sys.path
         import build_native
@@ -216,7 +271,7 @@ class build_ext(_build_ext):
 
         build_native.build(
             build_root_dir=build_dir,
-            targets=['_catboost'],
+            targets=targets,
             verbose=verbose,
             dry_run=dry_run,
             have_cuda=bool(self.with_cuda),
@@ -224,18 +279,22 @@ class build_ext(_build_ext):
             cmake_extra_args=[f'-DPython3_ROOT_DIR={python3_root_dir}']
         )
 
-        logging.info('Successfully built {} {}'.format(catboost_ext, cuda_support_msg))
+        logging.info(f'Successfully built {",".join(targets)} {cuda_support_msg}')
 
-    def copy_artifact_built_with_cmake(self, build_dir, put_dir, catboost_ext, verbose, dry_run):
-        # TODO(akhropov): CMake produces wrong artifact names right now so we have to rename it
-        cmake_dll_artifact_name = {
-            'linux': 'lib_catboost.so',
-            'darwin': 'lib_catboost.dylib',
-            'win32': '_catboost.dll',
-        }[sys.platform]
-
-        dll = os.path.join(build_dir, 'catboost', 'python-package', 'catboost', cmake_dll_artifact_name)
-        distutils.file_util.copy_file(dll, os.path.join(put_dir, catboost_ext), verbose=verbose, dry_run=dry_run)
+    def copy_artifacts_built_with_cmake(self, build_dir, verbose, dry_run):
+        for ext in self.extensions:
+            # TODO(akhropov): CMake produces wrong artifact names right now so we have to rename it
+            dll = os.path.join(
+                build_dir,
+                ext.cmake_build_sub_path,
+                build_ext.get_cmake_built_extension_filename(ext.name)
+            )
+            distutils.file_util.copy_file(
+                dll,
+                os.path.join(self.build_lib, ext.dst_sub_path, ext.name + build_ext.get_extension_suffix()),
+                verbose=verbose,
+                dry_run=dry_run
+            )
 
 class sdist(_sdist):
 
@@ -251,6 +310,24 @@ class sdist(_sdist):
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)1.1s %(msg)s', level=logging.DEBUG, datefmt=emph('%X'))
+
+    extensions = [
+        ExtensionWithSrcAndDstSubPath(
+            '_catboost',
+            os.path.join('catboost', 'python-package', 'catboost'),
+            'catboost'
+        )
+    ]
+    if '--with-hnsw' in sys.argv:
+        create_hnsw_submodule('--verbose' in sys.argv, '--dry-run' in sys.argv)
+        extensions.append(
+            ExtensionWithSrcAndDstSubPath(
+                '_hnsw',
+                os.path.join('library', 'python', 'hnsw', 'hnsw'),
+                os.path.join('catboost', 'hnsw')
+            )
+        )
+
     setup(
         name=os.environ.get('CATBOOST_PACKAGE_NAME') or 'catboost',
         version=os.environ.get('CATBOOST_PACKAGE_VERSION') or guess_catboost_version(),
@@ -262,7 +339,7 @@ if __name__ == '__main__':
                 '.eslintrc',
             ],
         },
-        ext_modules=[Extension('_catboost', sources=[])],
+        ext_modules=extensions,
         cmdclass={
             'bdist_wheel': bdist_wheel,
             'bdist': bdist,
