52c5845e84447bf41fb2d5dd20a368a561e02365
robot-contrib <robot-contrib@yandex-team.com>
2023-04-08 09:19:31 +0300

    Update contrib/python/filelock/py3 to 3.10.4

--- contrib/python/filelock/py3/filelock/_unix.py
+++ contrib/python/filelock/py3/filelock/_unix.py
@@ -32,7 +32,7 @@ else:  # pragma: win32 no cover
 
         def _acquire(self) -> None:
             open_flags = os.O_RDWR | os.O_CREAT | os.O_TRUNC
-            fd = os.open(self._lock_file, open_flags)
+            fd = os.open(self._lock_file, open_flags, self._mode)
             try:
                 os.chmod(fd, self._mode)
             except PermissionError:
