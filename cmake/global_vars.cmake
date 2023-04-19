c26a2b9a796a5d2999b4ebb677a8240cd0750fe6
robot-ya-builder <robot-ya-builder@yandex-team.com>
2023-04-19 14:10:48 +0300

    External build system generator release 29
    Update tools: yexport

--- cmake/global_vars.cmake
+++ cmake/global_vars.cmake
@@ -19,7 +19,7 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_6
   set(RAGEL_FLAGS -L -I ${CMAKE_SOURCE_DIR}/)
 endif()
 
-if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64" AND NOT HAVE_CUDA)
   set(FBS_CPP_FLAGS --no-warnings --cpp --keep-prefix --gen-mutable --schema -b --yandex-maps-iter --gen-object-api --filename-suffix .fbs)
   set(RAGEL_FLAGS -L -I ${CMAKE_SOURCE_DIR}/)
 endif()
