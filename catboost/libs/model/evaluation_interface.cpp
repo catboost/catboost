62059e7af9ed2bab80066dee9aa4d040471dbb1c
akhropov <akhropov@yandex-team.com>
2023-04-24 20:08:43 +0300

    Add check that TEvaluationBackendFactory contains implementation for specified formulaEvaluatorType.

--- catboost/libs/model/evaluation_interface.cpp
+++ catboost/libs/model/evaluation_interface.cpp
@@ -1,8 +1,15 @@
 #include "evaluation_interface.h"
 
+#include <util/system/yassert.h>
 
 namespace NCB::NModelEvaluation {
     TModelEvaluatorPtr CreateEvaluator(EFormulaEvaluatorType formulaEvaluatorType, const TFullModel& model) {
-        return TEvaluationBackendFactory::Construct(formulaEvaluatorType, model);
+        CB_ENSURE(
+            TEvaluationBackendFactory::Has(formulaEvaluatorType),
+            "No implementation available for formulaEvaluatorType=\"" << formulaEvaluatorType << "\""
+        );
+        auto* evaluatorRawPtr = TEvaluationBackendFactory::Construct(formulaEvaluatorType, model);
+        Y_ASSERT(evaluatorRawPtr);
+        return TModelEvaluatorPtr(evaluatorRawPtr);
     }
 }
