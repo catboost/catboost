0b105ae87f39b871c7872922e9b92c3fedcc111e
philimonov <philimonov@yandex-team.com>
2023-07-13 19:34:28 +0300

    Опеспечить совместимость с GCC 12.3
    Команда робототехники Я.Маркета собирает свою кодовую базу с
    помощью GCC 10.3. Хотелось бы в ближайшем будущем обновиться
    до GCC 12.3. Часть кода собирается из директорий:
    * util
    * library
    * yandex_io
    
    Поправил ошибки лексического анализатора GCC 12.3 + добавил
    пропущенный стандартные include файлы

--- library/cpp/json/yson/json2yson.h
+++ library/cpp/json/yson/json2yson.h
@@ -73,7 +73,7 @@ namespace NJson2Yson {
     class TSkipAttributesProxy: public TBase {
     public:
         template <typename... TArgs>
-        TSkipAttributesProxy<TBase>(TArgs&&... args)
+        TSkipAttributesProxy(TArgs&&... args)
             : TBase(std::forward<TArgs>(args)...)
         {
         }
