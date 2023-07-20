d5433b82ae6c0baaaa14db0569e5326a7136afea
a-beliakov <a-beliakov@yandex-team.com>
2023-07-20 11:29:21 +0300

    Set idea.max.content.load.filesize globally

--- build/scripts/compile_java.py
+++ build/scripts/compile_java.py
@@ -78,7 +78,7 @@ def main():
             ts.write(' '.join(ktsrcs + srcs))
         kt_classes_dir = 'kt_cls'
         mkdir_p(kt_classes_dir)
-        sp.check_call([opts.java_bin, '-jar', opts.kotlin_compiler, '-classpath', classpath, '-d', kt_classes_dir] + ktc_opts + ['@' + temp_kt_sources_file])
+        sp.check_call([opts.java_bin, '-Didea.max.content.load.filesize=30720', '-jar', opts.kotlin_compiler, '-classpath', classpath, '-d', kt_classes_dir] + ktc_opts + ['@' + temp_kt_sources_file])
         classpath = os.pathsep.join([kt_classes_dir, classpath])
 
     if srcs:
