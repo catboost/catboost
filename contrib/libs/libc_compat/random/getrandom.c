737e37a54d14540ca67cb24af22b414a2e70a5b8
thegeorg <thegeorg@yandex-team.com>
2023-09-01 22:56:18 +0300

    Properly unpoison data generated by getrandom syscall

--- contrib/libs/libc_compat/random/getrandom.c
+++ contrib/libs/libc_compat/random/getrandom.c
@@ -1,7 +1,18 @@
 #include <sys/random.h>
 #include "syscall.h"
 
+#if defined(__has_feature)
+	#if __has_feature(memory_sanitizer)
+		#include <sanitizer/msan_interface.h>
+	#endif
+#endif
+
 ssize_t getrandom(void *buf, size_t buflen, unsigned flags)
 {
+#if defined(__has_feature)
+	#if __has_feature(memory_sanitizer)
+	__msan_unpoison(buf, buflen);
+	#endif
+#endif
 	return syscall(SYS_getrandom, buf, buflen, flags);
 }
