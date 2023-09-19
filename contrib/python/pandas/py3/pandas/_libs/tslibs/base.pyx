9ff288c848ef77e448238107a07e903f340e6c20
shadchin <shadchin@yandex-team.com>
2023-09-19 17:50:59 +0300

    Remove unnecessary patch from pandas

--- contrib/python/pandas/py3/pandas/_libs/tslibs/base.pyx
+++ contrib/python/pandas/py3/pandas/_libs/tslibs/base.pyx
@@ -7,9 +7,6 @@ This is analogous to core.dtypes.generic.
 
 from cpython.datetime cimport datetime
 
-cdef extern from "Python.h":
-    void PyObject_GC_UnTrack(object op)
 
 cdef class ABCTimestamp(datetime):
-    def __dealloc__(ABCTimestamp self):
-        PyObject_GC_UnTrack(self)
+    pass
