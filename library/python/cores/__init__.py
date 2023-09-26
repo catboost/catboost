be018a1c6642197b892e86d8d430e41fd76deee4
zhukoff-pavel <zhukoff-pavel@yandex-team.com>
2023-09-27 00:10:51 +0300

    fix run_ut to work with py3

--- library/python/cores/__init__.py
+++ library/python/cores/__init__.py
@@ -193,12 +193,12 @@ def resolve_addresses(addresses, symbolizer, binary):
         "--obj",
         binary,
     ]
-    proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,  **({'text': True} if six.PY3 else {}))
     out, err = proc.communicate(input="\n".join(addresses))
     if proc.returncode:
         raise Exception("Symbolizer failed with rc:{}\nstderr: {}".format(proc.returncode, err))
 
-    resolved = filter(None, out.split("\n\n"))
+    resolved = list(filter(None, out.split("\n\n")))
     if len(addresses) != len(resolved):
         raise Exception("llvm-symbolizer can not extract lines from addresses (count mismatch: {}-{})".format(len(addresses), len(resolved)))
 
