c7d67a1b7bf236397ed60cdd769b7a161af33ced
halyavin <halyavin@yandex-team.com>
2023-09-26 11:14:38 +0300

    Update libc++ to 9b03c08e (6 Mar 2022).
    Notable changes:
    * don't warn that coroutines are not supported when using experimental/coroutine
    * add _LIBCPP_HIDE_FROM_ABI to __quoted_proxy constructors
    * ADL-proof calls of __quoted
    * fix ctype facet `is` method for Windows
    * change return type of bit_width to int (LWG3656)
    * make private __wrap_iter constructors explicit
    * fix delayed initialization of `__fill_` in basic_ios
    * ranges::iter_move cleanup
    * ADL-proof __synth_three_way
    * reject random number generators with signed types
    * use C++ overloads from math.h on AIX
    * add explicit to some internal constructors
    * replace _LIBCPP_HAS_NO_STRONG_ENUMS with C++ version check
    * use `inline constexpr bool` instead of `constexpr bool` for helpers in ranges
    * reject uniform_int_distribution<bool>, uniform_int_distribution<char> and all user types
    * allow std::mergeable and std::sortable even with incomplete ranges
    * fix double close bug in std::filesystem::remove_all
    * remove recursion in basic_string::insert because it interferes with constexpr
    * fix error checking of wctob_l calls
    * pack _Flags class on AIX
    * fix integer type in __estimate_column_width exposed by AIX
    * qualify all std::move calls with std
    * make chrono includes granular
    * set std::numeric_limits::tinyness_before to true on ARM
    * add ranges::in_found_result and ranges::min_max_result

--- contrib/libs/cxxsupp/libcxx/include/__functional/ranges_operations.h
+++ contrib/libs/cxxsupp/libcxx/include/__functional/ranges_operations.h
@@ -20,7 +20,7 @@
 
 _LIBCPP_BEGIN_NAMESPACE_STD
 
-#if !defined(_LIBCPP_HAS_NO_CONCEPTS) && !defined(_LIBCPP_HAS_NO_INCOMPLETE_RANGES)
+#if !defined(_LIBCPP_HAS_NO_CONCEPTS)
 
 namespace ranges {
 
@@ -91,7 +91,8 @@ struct greater_equal {
 };
 
 } // namespace ranges
-#endif // !defined(_LIBCPP_HAS_NO_CONCEPTS) && !defined(_LIBCPP_HAS_NO_INCOMPLETE_RANGES)
+
+#endif // !defined(_LIBCPP_HAS_NO_CONCEPTS)
 
 _LIBCPP_END_NAMESPACE_STD
 
