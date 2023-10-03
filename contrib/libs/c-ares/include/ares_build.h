20da9b7659b234dd16e8b5bf158a1379f9d26322
robot-piglet <robot-piglet@yandex-team.com>
2023-10-03 15:08:15 +0300

    Intermediate changes

--- contrib/libs/c-ares/include/ares_build.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#if defined(_MSC_VER) && (defined(__x86_64__) || defined(_M_X64))
-#   include "ares_build-win-x86_64.h"
-#else
-#   include "ares_build-linux.h"
-#endif
--- contrib/libs/c-ares/src/lib/ares_config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#if defined(__ANDROID__)
-#   include "ares_config-android.h"
-#elif defined(__APPLE__)
-#   include "ares_config-osx.h"
-#else
-#   include "ares_config-linux.h"
-#endif
