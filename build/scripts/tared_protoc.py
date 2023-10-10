67996d02cc83ddd64148d298e9b72356a8214e0c
snermolaev <snermolaev@yandex-team.com>
2023-10-10 06:42:42 +0300

    binary stable preprocessed.tar.gz and .jsrc

--- build/scripts/tared_protoc.py
+++ build/scripts/tared_protoc.py
@@ -2,6 +2,7 @@ import os
 import optparse
 import tarfile
 import contextlib
+import stat
 import subprocess as sp
 
 
@@ -23,8 +24,21 @@ def main():
 
     sp.check_call(args)
 
-    with contextlib.closing(tarfile.open(opts.tar_output, 'w')) as tf:
-        tf.add(opts.protoc_out_dir, arcname='')
+    with tarfile.open(opts.tar_output, 'w', format=tarfile.USTAR_FORMAT) as tf:
+        for root, dirs, files in os.walk(opts.protoc_out_dir, topdown=True):
+            dirs.sort()
+            for name in sorted(files):
+                fname = os.path.join(root, name)
+                with open(fname, 'rb') as fin:
+                    tarinfo = tf.gettarinfo(fname, os.path.relpath(fname, opts.protoc_out_dir))
+                    tarinfo.mode = stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH if tarinfo.mode | stat.S_IXUSR else 0
+                    tarinfo.mode = tarinfo.mode | stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH
+                    tarinfo.mtime = 0
+                    tarinfo.uid = 0
+                    tarinfo.gid = 0
+                    tarinfo.uname = 'dummy'
+                    tarinfo.gname = 'dummy'
+                    tf.addfile(tarinfo, fin)
 
 
 if __name__ == '__main__':
