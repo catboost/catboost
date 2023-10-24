f2904afcd41c05a733877db189a01e40c58c5b64
akhropov <akhropov@yandex-team.com>
2023-10-25 00:00:34 +0300

    Adapt to RowEncoder API changes in Spark 3.5. Part 2.

--- catboost/spark/catboost4j-spark/core/src/spark_3.5/scala/ai/catboost/spark/impl/ExpressionEncoderSerializer.scala
+++ catboost/spark/catboost4j-spark/core/src/spark_3.5/scala/ai/catboost/spark/impl/ExpressionEncoderSerializer.scala
@@ -2,13 +2,13 @@ package ai.catboost.spark.impl
 
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.encoders.{ExpressionEncoder,RowEncoder}
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 import org.apache.spark.sql.types.StructType
 
 
 object ExpressionEncoderSerializer {
   def apply(schema: StructType) : ExpressionEncoderSerializer = {
-    new ExpressionEncoderSerializer(RowEncoder.encoderFor(schema).createSerializer())
+    new ExpressionEncoderSerializer(ExpressionEncoder(schema).createSerializer())
   }
 }
 
