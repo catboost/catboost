95e4e682e8a7d224b07bcb68a1dd93a714109d77
robot-piglet <robot-piglet@yandex-team.com>
2023-12-06 01:16:58 +0300

    Intermediate changes

--- contrib/libs/grpc/src/compiler/python_generator.cc
+++ contrib/libs/grpc/src/compiler/python_generator.cc
@@ -895,6 +895,7 @@ bool PythonGrpcGenerator::Generate(const FileDescriptor* file,
       file->name().find_last_of(".proto") == file->name().size() - 1) {
     TString base =
         file->name().substr(0, file->name().size() - proto_suffix_length);
+    std::replace(base.MutRef().begin(), base.MutRef().end(), '-', '_');
     pb2_file_name = base + "_pb2.py";
     pb2_grpc_file_name = base + "_pb2_grpc.py";
   } else {
