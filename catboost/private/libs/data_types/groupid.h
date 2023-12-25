065af7a87d12e31b74a19a0beb42ef1b641d8b5b
espetrov <espetrov@yandex-team.com>
2023-12-25 08:18:36 +0300

    Use group id's of type ui64 as is; make all mappers ordered; order by document_offset in reducers

--- catboost/private/libs/data_types/groupid.h
+++ catboost/private/libs/data_types/groupid.h
@@ -3,10 +3,15 @@
 #include <util/system/types.h>
 #include <util/generic/strbuf.h>
 #include <util/digest/city.h>
+#include <util/string/cast.h>
 
 using TGroupId = ui64;
 
 inline TGroupId CalcGroupIdFor(const TStringBuf& token) {
+    TGroupId groupId;
+    if (!token.empty() && token[0] != '0' && AllOf(token, ::isdigit) && TryFromString<ui64>(token, groupId)) {
+        return groupId;
+    }
     return CityHash64(token);
 }
 
