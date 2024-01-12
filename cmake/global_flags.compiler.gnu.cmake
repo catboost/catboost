f20be25ad008b1e65059876d0a278dfae4a6288c
akhropov <akhropov@yandex-team.com>
2024-01-12 12:52:39 +0300

    Compilation flags: Support clang-cl..

--- cmake/global_flags.compiler.gnu.cmake
+++ cmake/global_flags.compiler.gnu.cmake
@@ -48,37 +48,9 @@ if (CMAKE_SYSTEM_PROCESSOR MATCHES "^(arm.*|aarch64|ppc64le)")
   string(APPEND _GNU_COMMON_C_CXX_FLAGS " -fsigned-char")
 endif()
 
-if (CMAKE_SYSTEM_PROCESSOR MATCHES "^(i686|x86_64|AMD64)$")
-  if (CMAKE_SYSTEM_PROCESSOR STREQUAL "i686")
-    string(APPEND _GNU_COMMON_C_CXX_FLAGS " -m32")
-  elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "^(x86_64|AMD64)$")
-    string(APPEND _GNU_COMMON_C_CXX_FLAGS " -m64")
-  endif()
-  string(APPEND _GNU_COMMON_C_CXX_FLAGS "\
-    -msse2 \
-    -msse3 \
-    -mssse3 \
-  ")
+include(global_flags.compiler.gnu.march)
+string(APPEND _GNU_COMMON_C_CXX_FLAGS " ${_GNU_MARCH_C_CXX_FLAGS}")
 
-  if ((CMAKE_SYSTEM_PROCESSOR MATCHES "^(x86_64|AMD64)$") OR (NOT ANDROID))
-    string(APPEND _GNU_COMMON_C_CXX_FLAGS "\
-      -msse4.1 \
-      -msse4.2 \
-      -mpopcnt \
-    ")
-    if (NOT ANDROID)
-      # older clang versions did not support this feature on Android:
-      # https://reviews.llvm.org/rGc32d307a49f5255602e7543e64e6c38a7f536abc
-      string(APPEND _GNU_COMMON_C_CXX_FLAGS " -mcx16")
-    endif()
-  endif()
-
-  if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    string(APPEND _GNU_COMMON_C_CXX_FLAGS " -D_YNDX_LIBUNWIND_ENABLE_EXCEPTION_BACKTRACE")
-  endif()
-elseif (ANDROID AND (CMAKE_ANDROID_ARCH_ABI STREQUAL "armeabi-v7a"))
-  string(APPEND _GNU_COMMON_C_CXX_FLAGS " -mfloat-abi=softfp")
-endif()
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${_GNU_COMMON_C_CXX_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${_GNU_COMMON_C_CXX_FLAGS} \
