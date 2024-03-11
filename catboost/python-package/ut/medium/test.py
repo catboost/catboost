b552008125e8015a45aa016b102edccc90756a3c
Semyon Panenko <sopanenko@gmail.com>
2024-03-12 01:37:13 +0300

    Added unit test

--- catboost/python-package/ut/medium/test.py
+++ catboost/python-package/ut/medium/test.py
@@ -11130,3 +11130,66 @@ def test_carry_model():
     uplifted_model = uplift(model, {factor_id: (0, 1)})
     uplift_model_predict = uplifted_model.predict(pool_without_feature)
     assert np.max((uplift_pool_predict - uplift_model_predict) ** 2)**0.5 < 1e-8, 'Wrong uplift model predict'
+
+def test_custom_gpu_eval_metric():
+    
+    class LoglossMetric(object):
+        def get_final_error(self, error, weight):
+            return error / (weight + 1e-38)
+
+        def is_max_optimal(self):
+            return False
+
+        def evaluate(self, approxes, target, weight):
+            assert len(approxes) == 1
+            assert len(target) == len(approxes[0])
+
+            approx = approxes[0]
+
+            error_sum = 0.0
+            weight_sum = 0.0
+
+            for i in range(len(approx)):
+                e = np.exp(approx[i])
+                p = e / (1 + e)
+                w = 1.0 if weight is None else weight[i]
+                weight_sum += w
+                error_sum += -w * (target[i] * np.log(p) + (1 - target[i]) * np.log(1 - p))
+
+            return error_sum, weight_sum
+
+    train_pool = Pool(data=TRAIN_FILE, column_description=CD_FILE)
+    test_pool = Pool(data=TEST_FILE, column_description=CD_FILE)
+
+    model = CatBoostClassifier(
+        iterations=5,
+        learning_rate=0.03,
+        use_best_model=True,
+        loss_function="Logloss",
+        eval_metric=LoglossMetric(),
+        # Leaf estimation method and gradient iteration are set to match
+        # defaults for Logloss.
+        leaf_estimation_method="Newton",
+        leaf_estimation_iterations=1,
+        task_type='GPU',
+        devices='0',
+        metric_period=1
+    )
+
+    model.fit(train_pool, eval_set=test_pool)
+    pred1 = model.predict(test_pool, prediction_type='RawFormulaVal')
+
+    model2 = CatBoostClassifier(
+        iterations=5,
+        learning_rate=0.03,
+        use_best_model=True,
+        loss_function="Logloss",
+        task_type='GPU',
+        leaf_estimation_method="Newton",
+        leaf_estimation_iterations=1
+    )
+
+    model2.fit(train_pool, eval_set=test_pool)
+    pred2 = model2.predict(test_pool, prediction_type='RawFormulaVal')
+
+    assert np.all(pred1 == pred2)
