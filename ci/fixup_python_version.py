308f80246162e2e37ae774d5aa7f248b6a0478ee
akhropov <akhropov@yandex-team.com>
2024-03-27 08:50:32 +0300

    Follow python versions guidelines for development versions fce9119863178c93977640638f6596fa9ac6699f

--- ci/fixup_python_version.py
+++ ci/fixup_python_version.py
@@ -4,4 +4,4 @@ version = os.environ.get('CATBOOST_PACKAGE_VERSION')
 counter = os.environ.get('GO_PIPELINE_COUNTER', '0')
 if version:
     with open('catboost/python-package/catboost/version.py', 'w') as version_file:
-        version_file.write('VERSION = \'{}.{}\'\n'.format(version, counter))
+        version_file.write('VERSION = \'{}.dev{}\'\n'.format(version, counter))
