5f7a39f03c65cd7d93314a0722c2f272f3f1ab3b
robot-piglet <robot-piglet@yandex-team.com>
2024-04-16 21:36:37 +0300

    Intermediate changes

--- tools/enum_parser/parse_enum/ut/enums_with_header.h
+++ tools/enum_parser/parse_enum/ut/enums_with_header.h
@@ -6,3 +6,18 @@ enum EWithHeader {
     HThree,
 };
 
+
+constexpr unsigned EvalValue(unsigned r, unsigned d) {
+    while (r >= 50) {
+        r *= d;
+    }
+    return r;
+}
+
+//  enumeration with values that depend on the preprocessor, architecture and constexpr function evaluation
+enum class ENontrivialValues {
+    A = __LINE__,
+    B = EvalValue(1522858842, 13),
+    C,
+    D = sizeof(int*[A][C]),
+};
