7399a8811a4556dc38c55d7f86a584d84d56f44a
mikhnenko <mikhnenko@yandex-team.com>
2024-04-24 09:46:32 +0300

    Move disabling CUDA-specific defines to epiloge 49394d902af5e4388e6145ab9d0b8e0f8161fadc

--- contrib/libs/cxxsupp/libcxx/include/__config_epilogue.h
+++ contrib/libs/cxxsupp/libcxx/include/__config_epilogue.h
@@ -1,6 +1,15 @@
 #pragma once
 
 #if defined(__CUDACC__)
-	#undef _LIBCPP_USING_IF_EXISTS
-	#define _LIBCPP_USING_IF_EXISTS
+    #undef _LIBCPP_DECLSPEC_EMPTY_BASES
+    #define _LIBCPP_DECLSPEC_EMPTY_BASES
+
+    #undef _LIBCPP_NODEBUG
+    #define _LIBCPP_NODEBUG
+    
+    #undef _LIBCPP_PACKED
+    #define _LIBCPP_PACKED
+
+    #undef _LIBCPP_USING_IF_EXISTS
+    #define _LIBCPP_USING_IF_EXISTS
 #endif 
