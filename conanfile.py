f41e993e17faacacda9f10921545eda2f6de8d9b
dimdim11 <dimdim11@yandex-team.com>
2024-07-08 10:06:07 +0300

    Conan2 support by use_conan2 flag in yexport.toml
    Conan2 support by use_conan2 flag in yexport.toml
    53b2f6c57e617e4efacd04b2205da51579655ac8

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+from conan import ConanFile
+
+
+class App(ConanFile):
+
+    settings = "os", "compiler", "build_type", "arch"
+
+    options = {}
+
+    requires = "openssl/1.1.1t"
+
+    tool_requires = "ragel/6.10", "swig/4.0.2", "yasm/1.3.0"
+    generators = "cmake_find_package", "cmake_paths"
+
+    def imports(self):
+        self.copy(pattern="*swig*", src="bin", dst="./bin")
+        self.copy(pattern="*yasm*", src="bin", dst="./bin")
+        self.copy(pattern="ragel*", src="bin", dst="./bin")
+        self.copy(pattern="ytasm*", src="bin", dst="./bin")
+
+    def layout(self):
+        cmake_layout(self)
