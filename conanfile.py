3fdb0ac4b6f086fa3dbc13faacd282dc57b4b468
dimdim11 <dimdim11@yandex-team.com>
2024-08-13 18:39:13 +0300

    (2of3) Use requirements function in conanfile and support os depends conan attributes in new cmake generator
    Use requirements function in conanfile and support os depends conan attributes in new cmake generator
    2b00a93fb0df66b9bf77816d6447e19f7c7131e7

--- conanfile.py
+++ conanfile.py
@@ -7,11 +7,14 @@ class App(ConanFile):
 
     options = {}
 
-    requires = "openssl/1.1.1t"
-
-
     tool_requires = "ragel/6.10", "swig/4.0.2", "yasm/1.3.0"
 
+    def requirements(self):
+        requires = [ "openssl/1.1.1t" ]
+        for require in requires:
+            if ("linux-headers" in require) and (self.settings.os != "Linux"):
+                continue
+            self.requires(require)
 
     generators = "cmake_find_package", "cmake_paths"
 
