ab4f03fe6b78a69dfaf14e8e06ca38676a03bbac
akhropov <akhropov@yandex-team.com>
2024-09-03 11:46:11 +0300

    python package version consistency. d7792f742262b73299bbcb8c99e544044414ca4e

--- ci/fixup_python_version.py
+++ ci/fixup_python_version.py
@@ -1,7 +1,13 @@
 import os
 
 version = os.environ.get('CATBOOST_PACKAGE_VERSION')
-counter = os.environ.get('GO_PIPELINE_COUNTER', '0')
 if version:
+    github_short_sha = os.environ.get('GITHUB_SHORT_SHA')
+    if github_short_sha:
+        full_version = version + '.dev1+g' + github_short_sha
+    else:
+        counter = os.environ.get('GO_PIPELINE_COUNTER', '0')
+        full_version = version + '.dev' + counter
+
     with open('catboost/python-package/catboost/version.py', 'w') as version_file:
-        version_file.write('VERSION = \'{}.dev{}\'\n'.format(version, counter))
+        version_file.write('VERSION = \'{}\'\n'.format(full_version))
