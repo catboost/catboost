9027f445fea7cb19af936f5866c01990551c0e0f
somov <somov@yandex-team.com>
2024-09-03 09:06:30 +0300

    Do not raise additional exception when failed to decode protoc output ba7df0e549915def70c0b6d303aa625ddc636db9

--- build/scripts/cpp_proto_wrapper.py
+++ build/scripts/cpp_proto_wrapper.py
@@ -25,7 +25,7 @@ def main(namespace: argparse.Namespace) -> int:
         subprocess.check_output(namespace.subcommand, stdin=None, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         sys.stderr.write(
-            '{} returned non-zero exit code {}.\n{}\n'.format(' '.join(e.cmd), e.returncode, e.output.decode('utf-8'))
+            '{} returned non-zero exit code {}.\n{}\n'.format(' '.join(e.cmd), e.returncode, e.output.decode('utf-8', errors='ignore'))
         )
         return e.returncode
 
