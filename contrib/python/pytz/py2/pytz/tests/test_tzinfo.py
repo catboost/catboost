2a43851bff35e320c031f95630e01c433d09225e
robot-piglet <robot-piglet@yandex-team.com>
2024-10-03 13:08:05 +0300

    Intermediate changes commit_hash:8e579a20127858c0606c08f784792f66491a86ca

--- contrib/python/pytz/py2/pytz/tests/test_tzinfo.py
+++ contrib/python/pytz/py2/pytz/tests/test_tzinfo.py
@@ -27,8 +27,8 @@ from pytz.tzinfo import DstTzInfo, StaticTzInfo  # noqa
 
 # I test for expected version to ensure the correct version of pytz is
 # actually being tested.
-EXPECTED_VERSION = '2024.1'
-EXPECTED_OLSON_VERSION = '2024a'
+EXPECTED_VERSION = '2024.2'
+EXPECTED_OLSON_VERSION = '2024b'
 
 fmt = '%Y-%m-%d %H:%M:%S %Z%z'
 
