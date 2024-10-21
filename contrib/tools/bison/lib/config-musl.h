4566f5bd746d473856af0a2b949447d779cc0602
robot-piglet <robot-piglet@yandex-team.com>
2024-10-21 11:21:54 +0300

    Intermediate changes commit_hash:5f643124bd953be2643d2898e3dfaeffbc4ecff2

--- contrib/tools/bison/lib/config-musl.h
+++ contrib/tools/bison/lib/config-musl.h
@@ -1,3 +1,5 @@
 #define HAVE_SYS_TIMES_H 1
 #define HAVE_STRUCT_TMS 1
 #define HAVE_CLOCK_T 1
+
+#define _GL_ARG_NONNULL(...) 
---
+++ contrib/tools/m4/lib/config-musl.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define HAVE___FREADAHEAD 1
+
+#define regcomp rpl_regcomp
+#define regfree rpl_regfree
+
+#define _GL_ARG_NONNULL(...)
+#define _GL_ATTRIBUTE_FORMAT_PRINTF(...)
