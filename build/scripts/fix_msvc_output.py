4978c5453cc29ec3e3d93a86dcdad6346ffc24ad
robot-brewer <robot-brewer@yandex-team.com>
2024-10-22 08:39:26 +0300

    Release python3-frozen #8
    https://github.com/yandex/toolchain-registry/releases/tag/python3-frozen-v8
    commit_hash:db14a768da738da2ea13bd7cb7af738d83793500

--- build/scripts/fix_msvc_output.py
+++ build/scripts/fix_msvc_output.py
@@ -1,6 +1,9 @@
 import subprocess
-import sys
+import os, sys
 
+# Explicitly enable local imports
+# Don't forget to add imported scripts to inputs of the calling command!
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import process_command_files as pcf
 import process_whole_archive_option as pwa
 
--- build/scripts/fs_tools.py
+++ build/scripts/fs_tools.py
@@ -6,6 +6,9 @@ import sys
 import shutil
 import errno
 
+# Explicitly enable local imports
+# Don't forget to add imported scripts to inputs of the calling command!
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import process_command_files as pcf
 
 
