925c85c35f2d2e48b25b8205cddf8dfea116c0e8
robot-piglet <robot-piglet@yandex-team.com>
2024-10-24 14:47:13 +0300

    Intermediate changes commit_hash:0e2d0d9e51343b000ae66f3dd28626242d23053b

--- library/cpp/http/server/http_ex.cpp
+++ library/cpp/http/server/http_ex.cpp
@@ -87,7 +87,8 @@ bool THttpClientRequestExtension::ProcessHeaders(TBaseServerRequestData& rd, TBl
                     }
 
                     TBuffer buf(SafeIntegerCast<size_t>(contentLength));
-                    buf.Resize(Input().Load(buf.Data(), (size_t)contentLength));
+                    Input().LoadOrFail(buf.Data(), (size_t)contentLength);
+                    buf.Resize((size_t)contentLength);
                     postData = TBlob::FromBuffer(buf);
                 } else {
                     postData = TBlob::FromStream(Input());
