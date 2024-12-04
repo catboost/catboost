b6b87742d2b72d28b5ae123044cab76bda491a38
robot-piglet <robot-piglet@yandex-team.com>
2024-12-04 12:55:17 +0300

    Intermediate changes commit_hash:fa1d99de42ae8a539ffd72f2e4d71363ad33c3f9

---
+++ library/cpp/yt/memory/type_erasure.h
@@ -0,0 +1,418 @@
+#pragma once
+
+#include "type_erasure_detail.h"
+
+namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace NDetail {
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class T, class TCpo>
+struct TTypeErasableTraits;
+
+template <class T, auto Cpo, class TRet, bool NoExcept, class TCvThis, class... TArgs>
+struct TTypeErasableTraits<T, TOverloadedCpo<Cpo, TRet(TCvThis, TArgs...) noexcept(NoExcept)>>
+{
+    using TReplaced = TFromThis<T, TCvThis>;
+
+    static constexpr bool Value = NoExcept
+        ? requires (TReplaced t, TArgs... args) {
+            { Cpo(std::forward<TReplaced>(t), std::forward<TArgs>(args)...) } noexcept -> std::same_as<TRet>;
+        }
+        : requires (TReplaced t, TArgs... args) {
+            { Cpo(std::forward<TReplaced>(t), std::forward<TArgs>(args)...) } -> std::same_as<TRet>;
+        };
+};
+
+template <class T, class... TCpos>
+concept CAnyRefErasable = (TTypeErasableTraits<std::remove_cvref_t<T>, TCpos>::Value && ...);
+
+template <class T, bool EnableCopy, class... TCpos>
+concept CAnyObjectErasable =
+    CAnyRefErasable<T, TCpos...> &&
+    std::movable<std::remove_cvref_t<T>> &&
+    (!EnableCopy || std::copyable<std::remove_cvref_t<T>>);
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct TBadAnyCast
+    : public std::exception
+{ };
+
+////////////////////////////////////////////////////////////////////////////////
+
+// AnyRef.
+
+template <bool UseStaticVTable, class... TCpos>
+class TAnyRef
+    : private TAnyFragment<TAnyRef<UseStaticVTable, TCpos...>, TCpos>...
+{
+public:
+    using TStorage = TNonOwningStorage;
+
+    TAnyRef() = default;
+
+    template <class TConcrete>
+        requires
+            CAnyRefErasable<std::remove_cvref_t<TConcrete>, TCpos...> &&
+            (!std::same_as<std::remove_cvref_t<TConcrete>, TAnyRef>)
+    TAnyRef(TConcrete& concRef) noexcept
+    {
+        using TDecayed = std::remove_cvref_t<TConcrete>;
+
+        Storage_.Set(const_cast<void*>(reinterpret_cast<const void*>(std::addressof(concRef))));
+        Holder_ = THolder::template Create<TDecayed>();
+    }
+
+    // Copy/Move are trivial copy. Dtor is also trivial.
+
+    const auto& GetStorage() const &
+    {
+        return Storage_;
+    }
+
+    auto& GetStorage() &
+    {
+        return Storage_;
+    }
+
+    auto&& GetStorage() &&
+    {
+        return std::move(Storage_);
+    }
+
+    const auto& GetVTable() const
+    {
+        return Holder_.GetVTable();
+    }
+
+    template <class T>
+        requires CAnyRefErasable<T, TCpos...>
+    T& AnyCast() &
+    {
+        const auto& vtable = GetVTable();
+        if (!vtable.IsValid() || !vtable.template IsCurrentlyStored<T>()) {
+            throw TBadAnyCast{};
+        }
+
+        return Storage_.template As<T>();
+    }
+
+    template <class T>
+        requires CAnyRefErasable<T, TCpos...>
+    const T& AnyCast() const &
+    {
+        const auto& vtable = Holder_.GetVTable();
+        if (!vtable.IsValid() || !vtable.template IsCurrentlyStored<T>()) {
+            throw TBadAnyCast{};
+        }
+
+        return Storage_.template As<T>();
+    }
+
+private:
+    using TVTable = TVTable<TStorage, TCpos...>;
+    using THolder = TVTableHolder<TStorage, TVTable, UseStaticVTable>;
+
+    TStorage Storage_ = {};
+    THolder Holder_ = {};
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class... TCpos>
+struct TAnyRefPublicHelper
+{
+    using T = TAnyRef<(sizeof...(TCpos) >= 3), TCpos...>;
+};
+
+template <>
+struct TAnyRefPublicHelper<>
+{
+    using T = TAnyRef<false, TTagInvokeTag<NoopCpo>>;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class T>
+struct TIsAnyRef
+    : public std::false_type
+{ };
+
+template <bool UseStaticVTable, class... TCpos>
+struct TIsAnyRef<TAnyRef<UseStaticVTable, TCpos...>>
+    : public std::true_type
+{ };
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <bool EnableCopy, CStorage TStorage, class... TCpos>
+struct TRebindVTable
+{
+    using TVTable = TVTable<
+        TStorage,
+        TTagInvokeTag<Deleter>,
+        TTagInvokeTag<Mover<TStorage>>,
+        TTagInvokeTag<Copier<TStorage>>,
+        TCpos...>;
+};
+
+template <CStorage TStorage, class... TCpos>
+struct TRebindVTable<false, TStorage, TCpos...>
+{
+    using TVTable = TVTable<
+        TStorage,
+        TTagInvokeTag<Deleter>,
+        TTagInvokeTag<Mover<TStorage>>,
+        TCpos...>;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <bool UseStaticVTable, bool EnableCopy, size_t SmallObjectSize, size_t SmallObjectAlign, class... TCpos>
+class TAnyObject
+    : private TAnyFragment<
+        TAnyObject<
+            UseStaticVTable,
+            EnableCopy,
+            SmallObjectSize,
+            SmallObjectAlign,
+            TCpos...>,
+        TCpos>...
+{
+public:
+    using TStorage = TOwningStorage<SmallObjectSize, SmallObjectAlign>;
+
+    TAnyObject() = default;
+
+    template <class TConcrete>
+        requires
+            (!std::same_as<std::remove_cvref_t<TConcrete>, TAnyObject>) &&
+            std::constructible_from<std::remove_cvref_t<TConcrete>, TConcrete> &&
+            CAnyObjectErasable<std::remove_cvref_t<TConcrete>, EnableCopy, TCpos...> &&
+            (!TIsAnyRef<std::remove_cvref_t<TConcrete>>::value)
+    TAnyObject(TConcrete&& concrete)
+    {
+        Set<TConcrete>(std::forward<TConcrete>(concrete));
+    }
+
+    template <class TConcrete, class... TArgs>
+        requires
+            (!std::same_as<std::remove_cvref_t<TConcrete>, TAnyObject>) &&
+            std::constructible_from<std::remove_cvref_t<TConcrete>, TArgs...> &&
+            (!std::is_const_v<TConcrete>) &&
+            CAnyObjectErasable<std::remove_cvref_t<TConcrete>, EnableCopy, TCpos...> &&
+            (!TIsAnyRef<std::remove_cvref_t<TConcrete>>::value)
+    TAnyObject(std::in_place_type_t<TConcrete>, TArgs&&... args)
+    {
+        Set<TConcrete>(std::forward<TArgs>(args)...);
+    }
+
+    TAnyObject(TAnyObject&& other)
+        : Holder_(other.Holder_)
+    {
+        if (IsValid()) {
+            auto* mover = GetVTable().template GetFunctor<Mover<TStorage>>();
+            mover(std::move(other).GetStorage(), GetStorage());
+
+            other.Holder_.Reset();
+        }
+    }
+
+    TAnyObject& operator=(TAnyObject&& other)
+    {
+        if (this == &other) {
+            return *this;
+        }
+
+        Reset();
+
+        Holder_ = other.Holder_;
+
+        if (IsValid()) {
+            auto* mover = GetVTable().template GetFunctor<Mover<TStorage>>();
+            mover(std::move(other).GetStorage(), GetStorage());
+
+            other.Holder_.Reset();
+        }
+
+        return *this;
+    }
+
+    TAnyObject(const TAnyObject& other) requires EnableCopy
+        : Holder_(other.Holder_)
+    {
+        if (IsValid()) {
+            auto* copier = GetVTable().template GetFunctor<Copier<TStorage>>();
+            copier(other.GetStorage(), GetStorage());
+        }
+    }
+
+    TAnyObject& operator=(const TAnyObject& other) requires EnableCopy
+    {
+        if (this == &other) {
+            return *this;
+        }
+
+        Reset();
+
+        Holder_ = other.Holder_;
+
+        if (IsValid()) {
+            auto* copier = GetVTable().template GetFunctor<Copier<TStorage>>();
+            copier(other.GetStorage(), GetStorage());
+        }
+
+        return *this;
+    }
+
+    constexpr explicit operator bool() const
+    {
+        return IsValid();
+    }
+
+    ~TAnyObject()
+    {
+        Reset();
+    }
+
+    Y_FORCE_INLINE const auto& GetStorage() const &
+    {
+        return Storage_;
+    }
+
+    Y_FORCE_INLINE auto& GetStorage() &
+    {
+        return Storage_;
+    }
+
+    Y_FORCE_INLINE auto&& GetStorage() &&
+    {
+        return std::move(Storage_);
+    }
+
+    Y_FORCE_INLINE const auto& GetVTable() const
+    {
+        return Holder_.GetVTable();
+    }
+
+    template <class T>
+    Y_FORCE_INLINE T& AnyCast() &
+    {
+        using TDecayed = std::remove_cvref_t<T>;
+        using TWrapped = TOwningWrapper<TDecayed, TStorage, EnableCopy, TCpos...>;
+
+        const auto& vtable = GetVTable();
+        if (!vtable.IsValid() || !vtable.template IsCurrentlyStored<TWrapped>()) {
+            throw TBadAnyCast{};
+        }
+
+        return Storage_.template As<TWrapped>().Unwrap();
+    }
+
+    template <class T>
+    Y_FORCE_INLINE const T& AnyCast() const &
+    {
+        using TDecayed = std::remove_cvref_t<T>;
+        using TWrapped = TOwningWrapper<TDecayed, TStorage, EnableCopy, TCpos...>;
+
+        const auto& vtable = GetVTable();
+        if (!vtable.IsValid() || !vtable.template IsCurrentlyStored<TWrapped>()) {
+            throw TBadAnyCast{};
+        }
+
+        return Storage_.template As<TWrapped>().Unwrap();
+    }
+
+    Y_FORCE_INLINE bool IsValid() const
+    {
+        return Holder_.IsValid();
+    }
+
+private:
+    using TVTable = typename TRebindVTable<EnableCopy, TStorage, TCpos...>::TVTable;
+    using THolder = TVTableHolder<TStorage, TVTable, UseStaticVTable>;
+    using TTraits = std::allocator_traits<std::allocator<std::byte>>;
+
+    TStorage Storage_ = {};
+    THolder Holder_ = {};
+
+    static inline std::allocator<std::byte> Allocator = {};
+
+    Y_FORCE_INLINE void Reset() noexcept
+    {
+        const auto& vtable = Holder_.GetVTable();
+        if (Holder_.IsValid()) {
+            auto* deleter = vtable.template GetFunctor<Deleter>();
+            deleter(Storage_);
+
+            Holder_.Reset();
+        }
+    }
+
+    template <class TConcrete, class... TArgs>
+    Y_FORCE_INLINE void Set(TArgs&&... args)
+    {
+        using TDecayed = std::remove_cvref_t<TConcrete>;
+        using TWrapped = TOwningWrapper<TDecayed, TStorage, EnableCopy, TCpos...>;
+
+        Reset();
+
+        Holder_ = THolder::template Create<TWrapped>();
+
+        if constexpr (TStorage::template IsStatic<TWrapped>) {
+            Storage_.Set();
+        } else {
+            Storage_.Set(TTraits::allocate(Allocator, sizeof(TWrapped)));
+        }
+
+        TTraits::template construct<TWrapped>(
+            Allocator,
+            &Storage_.template As<TWrapped>(),
+            std::forward<TArgs>(args)...);
+    }
+};
+
+} // namespace NDetail
+
+////////////////////////////////////////////////////////////////////////////////
+
+// A simple placeholder for overloaded cpo usage.
+using NDetail::TErasedThis;
+
+////////////////////////////////////////////////////////////////////////////////
+
+// A wrapper class which carries all the necessary information for type erasure.
+template <auto Cpo, class TSignature>
+using TOverload = NDetail::TOverloadedCpo<Cpo, TSignature>;
+
+////////////////////////////////////////////////////////////////////////////////
+
+// A non-owning reference.
+template <class... TCpos>
+using TAnyRef = typename NDetail::TAnyRefPublicHelper<TCpos...>::T;
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class... TCpos>
+using TAnyObject = NDetail::TAnyObject<
+    (sizeof...(TCpos) >= 3),
+    /*EnableCopy*/ true,
+    /*SmallObjectSize*/ sizeof(void*) * 2,
+    /*SmallObjectAlign*/ alignof(void*) * 2,
+    TCpos...>;
+
+template <class... TCpos>
+using TAnyUnique = NDetail::TAnyObject<
+    (sizeof...(TCpos) >= 3),
+    /*EnableCopy*/ false,
+    /*SmallObjectSize*/ sizeof(void*) * 2,
+    /*SmallObjectAlign*/ alignof(void*) * 2,
+    TCpos...>;
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT
---
+++ library/cpp/yt/memory/unittests/type_erasure_ut.cpp
@@ -0,0 +1,333 @@
+#include <library/cpp/testing/gtest/gtest.h>
+
+#include <library/cpp/yt/memory/type_erasure.h>
+
+namespace NYT {
+namespace {
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct TTestCpo1
+    : public TTagInvokeCpoBase<TTestCpo1>
+{ };
+
+inline constexpr TTestCpo1 TestCpo = {};
+
+struct TCustomized
+{
+    int Value = 42;
+
+    friend int TagInvoke(TTagInvokeTag<TestCpo>, const TCustomized& this_)
+    {
+        return this_.Value + 1;
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+// 2 Cpos won't trigger static vtable
+static_assert(
+    sizeof(
+        TAnyRef<
+            TOverload<
+                TestCpo,
+                void(TErasedThis&)>,
+            TOverload<
+                TestCpo,
+                void(TErasedThis&&)>>)
+    == 8 + 2 * 8);
+
+// 3 Cpos trigger static vtable
+static_assert(
+    sizeof(
+        TAnyRef<
+            TOverload<
+                TestCpo,
+                void(TErasedThis&)>,
+            TOverload<
+                TestCpo,
+                void(TErasedThis&&)>,
+            TOverload<
+                TestCpo,
+                void(TErasedThis, int)>>)
+    == 8 + 8);
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST(TAnyRefTest, JustWorks)
+{
+    using TAnyRef = TAnyRef<TOverload<TestCpo, int(const TErasedThis&)>>;
+    TCustomized concrete{.Value = 42};
+
+    TAnyRef any{concrete};
+    EXPECT_EQ(TestCpo(any), 43);
+
+    auto copy = any;
+    EXPECT_EQ(TestCpo(copy), 43);
+
+    auto movedOut = std::move(any);
+    EXPECT_EQ(TestCpo(movedOut), 43);
+}
+
+TEST(TAnyRefTest, EmptyRef)
+{
+    TCustomized concrete{.Value = 11};
+
+    TAnyRef<> any{concrete};
+
+    static_assert(!std::invocable<TTagInvokeTag<TestCpo>, decltype(any)>);
+    const auto& conc = any.AnyCast<TCustomized>();
+    EXPECT_EQ(conc.Value, 11);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct TNoCopy
+{
+    TNoCopy() = default;
+
+    TNoCopy(const TNoCopy&) = delete;
+
+    TNoCopy(TNoCopy&&)
+    { }
+
+    TNoCopy& operator=(TNoCopy&&)
+    {
+        return *this;
+    }
+
+    int Val = 123;
+};
+
+static_assert(std::movable<TNoCopy>);
+
+struct TCustomized2
+{
+    int Value = 1;
+    static inline int DtorCount = 0;
+
+    TCustomized2() = default;
+
+    TCustomized2(const TCustomized2&)
+    { }
+
+    TCustomized2& operator=(const TCustomized2&)
+    {
+        return *this;
+    }
+
+    TCustomized2(TCustomized2&& other)
+        : Value(other.Value)
+    {
+        other.Value = -1;
+    }
+
+    TCustomized2& operator=(TCustomized2&& other)
+    {
+        if (this == &other) {
+            return *this;
+        }
+
+        Value = std::exchange(other.Value, -1);
+        return *this;
+    }
+
+    ~TCustomized2()
+    {
+        ++DtorCount;
+    }
+
+    friend const TNoCopy& TagInvoke(TTagInvokeTag<TestCpo>, TCustomized2&)
+    {
+        static TNoCopy noCp;
+        noCp.Val = 11;
+        return noCp;
+    }
+
+    friend int TagInvoke(TTagInvokeTag<TestCpo>, TCustomized2&& this_)
+    {
+        auto v = std::move(this_);
+
+        return 1212;
+    }
+
+    friend int TagInvoke(TTagInvokeTag<TestCpo>, TCustomized2&, int)
+    {
+        return 42;
+    }
+};
+
+static_assert(std::copyable<TCustomized2>);
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST(TAnyRefTest, CvRefCorrectness)
+{
+    using TAnyRef = TAnyRef<
+        TOverload<TestCpo, const TNoCopy&(TErasedThis&)>,
+        TOverload<TestCpo, int(TErasedThis&&)>>;
+
+    TCustomized2 cust = {};
+
+    cust.DtorCount = 0;
+    EXPECT_EQ(cust.Value, 1);
+
+    {
+        TAnyRef ref{cust};
+
+        EXPECT_EQ(TestCpo(ref).Val, 11);
+    }
+
+    EXPECT_EQ(cust.DtorCount, 0);
+    EXPECT_EQ(cust.Value, 1);
+
+    {
+        TAnyRef ref(cust);
+
+        EXPECT_EQ(TestCpo(std::move(ref)), 1212);
+    }
+
+    EXPECT_EQ(cust.DtorCount, 1);
+    EXPECT_EQ(cust.Value, -1);
+
+    cust.Value = 1;
+    TAnyRef any{cust};
+    {
+        TAnyRef copy{any};
+        TAnyRef movedOut{std::move(any)};
+        EXPECT_EQ(TestCpo(copy).Val, 11);
+        EXPECT_EQ(TestCpo(movedOut).Val, 11);
+    }
+
+    EXPECT_EQ(cust.DtorCount, 1);
+    EXPECT_EQ(cust.Value, 1);
+}
+
+TEST(TAnyRefTest, StaticVTableForAnyRef)
+{
+    using TAnyRef = TAnyRef<
+        TOverload<TestCpo, const TNoCopy&(TErasedThis&)>,
+        TOverload<TestCpo, int(TErasedThis&&)>,
+        TOverload<TestCpo, int(TErasedThis&, int)>
+    >;
+
+    TCustomized2 cst = {};
+    cst.Value = 1111;
+    cst.DtorCount = 0;
+
+    TAnyRef any{cst};
+    {
+        TAnyRef copy{any};
+        TAnyRef movedOut{std::move(any)};
+        EXPECT_EQ(TestCpo(copy).Val, 11);
+        EXPECT_EQ(TestCpo(movedOut).Val, 11);
+    }
+    EXPECT_EQ(cst.Value, 1111);
+    EXPECT_EQ(cst.DtorCount, 0);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST(TAnyObjectTest, JustWorks)
+{
+    TCustomized concrete{.Value = 42};
+
+    TAnyObject<TOverload<TestCpo, int(const TErasedThis&)>> any{concrete};
+    EXPECT_EQ(TestCpo(any), 43);
+
+    auto copy = any;
+    EXPECT_EQ(TestCpo(copy), 43);
+
+    auto movedOut = std::move(any);
+    EXPECT_EQ(TestCpo(movedOut), 43);
+}
+
+TEST(TAnyObjectTest, EmptyAny)
+{
+    TCustomized concrete{.Value = 11};
+
+    TAnyObject<> any{concrete};
+
+    static_assert(!std::invocable<TTagInvokeTag<TestCpo>, decltype(any)>);
+    const auto& conc = any.AnyCast<TCustomized>();
+    EXPECT_EQ(conc.Value, 11);
+}
+
+TEST(TAnyObjectTest, CvRefCorrectness)
+{
+    using TAnyObject = TAnyObject<
+        TOverload<TestCpo, const TNoCopy&(TErasedThis&)>,
+        TOverload<TestCpo, int(TErasedThis&&)>>;
+
+    TCustomized2 cust = {};
+
+    cust.DtorCount = 0;
+    EXPECT_EQ(cust.Value, 1);
+
+    {
+        TAnyObject any{cust};
+
+        EXPECT_EQ(TestCpo(any).Val, 11);
+    }
+
+    // Any object itself.
+    EXPECT_EQ(cust.DtorCount, 1);
+    EXPECT_EQ(cust.Value, 1);
+
+    {
+        TAnyObject any(cust);
+
+        EXPECT_EQ(TestCpo(std::move(any)), 1212);
+    }
+
+    // Second any object + moved out object.
+    EXPECT_EQ(cust.DtorCount, 3);
+    EXPECT_EQ(cust.Value, 1);
+
+    TAnyObject any{cust};
+    {
+        TAnyObject copy{any};
+        TAnyObject movedOut{std::move(any)};
+        EXPECT_EQ(TestCpo(copy).Val, 11);
+        EXPECT_EQ(TestCpo(movedOut).Val, 11);
+    }
+
+    EXPECT_EQ(cust.DtorCount, 5);
+    EXPECT_THROW(any.AnyCast<TCustomized2>(), NDetail::TBadAnyCast);
+}
+
+TEST(TAnyObjectTest, StaticVTableForAnyRef)
+{
+    using TAnyObject = TAnyObject<
+        TOverload<TestCpo, const TNoCopy&(TErasedThis&)>,
+        TOverload<TestCpo, int(TErasedThis&&)>,
+        TOverload<TestCpo, int(TErasedThis&, int)>
+    >;
+
+    TCustomized2 cst = {};
+    cst.Value = 1111;
+    cst.DtorCount = 0;
+
+    TAnyObject any{cst};
+    {
+        TAnyObject copy{any};
+        TAnyObject movedOut{std::move(any)};
+        EXPECT_EQ(TestCpo(copy).Val, 11);
+        EXPECT_EQ(TestCpo(movedOut).Val, 11);
+    }
+    EXPECT_EQ(cst.Value, 1111);
+    EXPECT_EQ(cst.DtorCount, 2);
+    EXPECT_FALSE(any.IsValid());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST(TAnyObjectTest, AnyMoveOnly)
+{
+    TAnyUnique<> any{std::in_place_type<TNoCopy>};
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace
+} // namespace NYT
