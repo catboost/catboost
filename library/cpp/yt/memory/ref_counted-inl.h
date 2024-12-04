838d8556232a07a64a65f380edb27c8b7b1bbace
lukyan <lukyan@yandex-team.com>
2024-12-04 13:14:05 +0300

    YT-23698: Check size of ref counted objects commit_hash:97159680fa733af4212c094e917bc78a47eb0c15

--- library/cpp/yt/memory/ref_counted-inl.h
+++ library/cpp/yt/memory/ref_counted-inl.h
@@ -149,6 +149,10 @@ struct TRefCountedTraits
 template <class T>
 struct TRefCountedTraits<T, true>
 {
+    static_assert(
+        sizeof(T) < (1ULL << PackedPtrTagBits),
+        "Ref counted object derived from TRefCountedBase exceedes max size");
+
     Y_FORCE_INLINE static const TRefCounter* GetRefCounter(const T* obj)
     {
         return obj;
