0f713e4f3972aa640f6bcdb0fab4257d46aaa1a6
akhropov <akhropov@yandex-team.com>
2024-12-07 00:22:10 +0300

    Use spaces for indentation as in all other files commit_hash:fdaeee67464f33671fe11ca16ec11bb55ac02593

--- catboost/node-package/bindings/catboost.d.ts
+++ catboost/node-package/bindings/catboost.d.ts
@@ -12,40 +12,40 @@ export type CatBoostEmbeddingFeatures = Array<Array<number[]>>;
 
 /** CatBoost model instance. */
 export class Model {
-	constructor(path?: string);
+    constructor(path?: string);
 
 	/** Load a model from the file. */
-	loadModel(path: string): void;
-	/** Set model prediction postprocessing type. Possible values are:
-	 * RawFormulaVal - raw sum of leaf values for each dimension,
+    loadModel(path: string): void;
+    /** Set model prediction postprocessing type. Possible value are:
+     * RawFormulaVal - raw sum of leaf values for each dimension,
      * Exponent - exp(sum(leaf values)),
-	 * RMSEWithUncertainty - pair (prediction, uncertainty),
-	 * Probability - (probablity for class_0, ..., probablity for class_i,...)
-	 * Class - id of a class with the maximum predicted probability
-	 * */
-	setPredictionType(predictionType: string): void;
-	/**
-	 * Calculate the prediction for multiple samples.
-	 * All defined feature arguments must have the same length.
-	 */
-	predict(floatFeatures: CatBoostFloatFeatures,
-		catFeatures?: CatBoostCategoryFeatures,
-		textFeatures?: CatBoostTextFeatures,
-		embeddingFeatures?: CatBoostEmbeddingFeatures): number[];
-	/** Enable evaluation on GPU device. */
-	enableGPUEvaluation(deviceId: number): void;
-	/** The number of numeric features. */
-	getFloatFeaturesCount(): number;
-	/** The number of categorical features. */
-	getCatFeaturesCount(): number;
-	/** The number of text features. */
-	getTextFeaturesCount(): number;
-	/** The number of embedding features. */
-	getEmbeddingFeaturesCount(): number;
-	/** The number of trees in the model. */
-	getTreeCount(): number;
-	/** The number of dimensions in the model. */
-	getDimensionsCount(): number;
-	/** The number of dimensions in the prediction. */
-	getPredictionDimensionsCount(): number;
+     * RMSEWithUncertainty - pair (prediction, uncertainty),
+     * Probability - (probablity for class_0, ..., probablity for class_i,...)
+     * Class - id of a class with the maximum predicted probability
+     * */
+    setPredictionType(predictionType: string): void;
+    /**
+     * Calculate the prediction for multiple samples.
+     * All defined feature arguments must have the same length.
+     */
+    predict(floatFeatures: CatBoostFloatFeatures,
+        catFeatures?: CatBoostCategoryFeatures,
+        textFeatures?: CatBoostTextFeatures,
+        embeddingFeatures?: CatBoostEmbeddingFeatures): number[];
+    /** Enable evaluation on GPU device. */
+    enableGPUEvaluation(deviceId: number): void;
+    /** The number of numeric features. */
+    getFloatFeaturesCount(): number;
+    /** The number of categorical features. */
+    getCatFeaturesCount(): number;
+    /** The number of text features. */
+    getTextFeaturesCount(): number;
+    /** The number of embedding features. */
+    getEmbeddingFeaturesCount(): number;
+    /** The number of trees in the model. */
+    getTreeCount(): number;
+    /** The number of dimensions in the model. */
+    getDimensionsCount(): number;
+    /** The number of dimensions in the prediction. */
+    getPredictionDimensionsCount(): number;
 }
