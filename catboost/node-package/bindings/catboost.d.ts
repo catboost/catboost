9404f034b61f65222ffc5cbdd442561af9dc50ee
akhropov <akhropov@yandex-team.com>
2024-12-07 05:14:34 +0300

    Add detailed information about interpretation of the 'predict' result values. Add 'MultiProbability' prediction type.. Fix #1960 commit_hash:70cca4bd8ba9ffb372c3ede7a9c810c544d6fa8c

--- catboost/node-package/bindings/catboost.d.ts
+++ catboost/node-package/bindings/catboost.d.ts
@@ -17,16 +17,40 @@ export class Model {
 	/** Load a model from the file. */
     loadModel(path: string): void;
     /** Set model prediction postprocessing type. Possible value are:
-     * RawFormulaVal - raw sum of leaf values for each dimension,
+     * RawFormulaVal - raw sum of leaf values for each dimension, this is the default
      * Exponent - exp(sum(leaf values)),
      * RMSEWithUncertainty - pair (prediction, uncertainty),
      * Probability - (probablity for class_0, ..., probablity for class_i,...)
-     * Class - id of a class with the maximum predicted probability
+     * MultiProbability - probability for each label (used for multilabel classification)
+     * Class - index of a class with the maximum predicted probability
      * */
     setPredictionType(predictionType: string): void;
     /**
      * Calculate the prediction for multiple samples.
      * All defined feature arguments must have the same length.
+     *
+     * The returned value contains [sampleCount x predictionDimensions] elements
+     *  (should be accessed using [sampleIndex * predictionDimensions + predictionDimensionIdx],
+     *   for simple cases when predictionDimensions = 1 it is just [sampleIndex])
+     *  and its interpretation depends on prediction type (can be set with 'setPredictionType'):
+     *  - RawFormulaVal (this is the default):
+     *    array of raw sum of leaf values for each dimension
+     *  - Exponent:
+     *    array of exp(sum(leaf values)) for each dimension
+     *  - RMSEWithUncertainty:
+     *    array of pairs (prediction, uncertainty)
+     *  - Probability:
+     *    - for binary classification models:
+     *      array of probabilities for positive class (calculated as sigmoid(rawFormulaVal))
+     *    - for multiclassification models:
+     *      array of array of probabilities for each class (calculated as softmax(rawFormulaVal))
+     *  - MultiProbability:
+     *    array of probabilities for each label (calculated as sigmoid(rawFormulaVal))
+     *      (used for multilabel classification)
+     *  - Class:
+     *    array of predicted class indices.
+     *
+     * predictionDimensions can be obtained using 'getPredictionDimensionsCount' method.
      */
     predict(floatFeatures: CatBoostFloatFeatures,
         catFeatures?: CatBoostCategoryFeatures,
