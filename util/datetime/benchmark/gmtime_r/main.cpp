695b736afd903c067fd0fafd44c5c80bdb040a02
tobo <tobo@yandex-team.com>
2025-02-07 15:32:15 +0300

    add gmtime_r benchmark for reference commit_hash:9b7a9d3b03b72af3900448dedeb78dff5e16af88

--- util/datetime/benchmark/gmtime_r/main.cpp
+++ util/datetime/benchmark/gmtime_r/main.cpp
@@ -34,11 +34,30 @@ void BM_GmTimeRRandom(benchmark::State& state, TDuration window) {
     }
 }
 
+void BM_gmtime_r_Random(benchmark::State& state, TDuration window) {
+    time_t now = TInstant::Now().TimeT();
+    struct tm buf {};
+
+    TFastRng<ui32> rng(2);
+    const size_t range = window.Seconds();
+    for (auto _ : state) {
+        size_t offset = rng.GenRand() % range;
+        time_t v = now - offset;
+        Y_DO_NOT_OPTIMIZE_AWAY(gmtime_r(&v, &buf));
+    }
+}
+
 BENCHMARK(BM_GmTimeR);
 BENCHMARK(BM_gmtime_r);
 BENCHMARK_CAPTURE(BM_GmTimeRRandom, last_hour, TDuration::Hours(1));
+BENCHMARK_CAPTURE(BM_gmtime_r_Random, last_hour, TDuration::Hours(1));
 BENCHMARK_CAPTURE(BM_GmTimeRRandom, last_day, TDuration::Days(1));
-BENCHMARK_CAPTURE(BM_GmTimeRRandom, last_mount, TDuration::Days(31));
+BENCHMARK_CAPTURE(BM_gmtime_r_Random, last_day, TDuration::Days(1));
+BENCHMARK_CAPTURE(BM_GmTimeRRandom, last_month, TDuration::Days(31));
+BENCHMARK_CAPTURE(BM_gmtime_r_Random, last_month, TDuration::Days(31));
 BENCHMARK_CAPTURE(BM_GmTimeRRandom, last_year, TDuration::Days(365));
+BENCHMARK_CAPTURE(BM_gmtime_r_Random, last_year, TDuration::Days(365));
 BENCHMARK_CAPTURE(BM_GmTimeRRandom, last_decade, TDuration::Days(3653));
+BENCHMARK_CAPTURE(BM_gmtime_r_Random, last_decade, TDuration::Days(3653));
 BENCHMARK_CAPTURE(BM_GmTimeRRandom, last_half_centry, TDuration::Days(18262));
+BENCHMARK_CAPTURE(BM_gmtime_r_Random, last_half_century, TDuration::Days(18262));
