bdf1cc762a5b90debc3a0d76f743eee2e1670b0f
robot-contrib <robot-contrib@yandex-team.com>
2025-02-12 14:51:12 +0300

    Update contrib/python/fonttools to 4.55.7 commit_hash:bf5a157fb86cafa598ed84f020c655698cdb57c8

--- contrib/python/fonttools/fontTools/__init__.py
+++ contrib/python/fonttools/fontTools/__init__.py
@@ -3,6 +3,6 @@ from fontTools.misc.loggingTools import configLogger
 
 log = logging.getLogger(__name__)
 
-version = __version__ = "4.55.6"
+version = __version__ = "4.55.7"
 
 __all__ = ["version", "log", "configLogger"]
