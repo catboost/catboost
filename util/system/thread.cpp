1f08032d7f210a61ba50816612a3d18e9adff7af
vchigrin <vchigrin@yandex-team.com>
2025-03-01 21:28:55 +0300

    feat envoy: Fix linking errors in util when building for WASM platform.
    Fix linking errors due to missed _Unwind_Backtrace symbol.
    
    Fix linking error due to missed CurrentThreadId implementation.
    commit_hash:ba09595a0e5feaefe5ad506c0005815f4c11a153

--- util/system/thread.cpp
+++ util/system/thread.cpp
@@ -535,11 +535,11 @@ TCurrentThreadLimits::TCurrentThreadLimits() noexcept
     : StackBegin(nullptr)
     , StackLength(0)
 {
-#if defined(_linux_) || defined(_cygwin_) || defined(_freebsd_)
+#if defined(_linux_) || defined(_cygwin_) || defined(_freebsd_) || defined(__EMSCRIPTEN__)
     pthread_attr_t attr;
     pthread_attr_init(&attr);
 
-    #if defined(_linux_) || defined(_cygwin_)
+    #if defined(_linux_) || defined(_cygwin_) || defined(__EMSCRIPTEN__)
     Y_ABORT_UNLESS(pthread_getattr_np(pthread_self(), &attr) == 0, "pthread_getattr failed");
     #else
     Y_ABORT_UNLESS(pthread_attr_get_np(pthread_self(), &attr) == 0, "pthread_attr_get_np failed");
