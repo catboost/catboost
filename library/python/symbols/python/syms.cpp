a50b5e2aa7fe8fb55e0a705908310ee9940094e2
thegeorg <thegeorg@yandex-team.com>
2025-03-11 20:02:44 +0300

    Merge win_unicode_console symbols into library/python/symbols/python
    This continues the work started in rXXXXXX.
    commit_hash:8dedee6f36d2b5708f97d6b97bc8bc80ab55a10b

--- library/python/symbols/python/syms.cpp
+++ library/python/symbols/python/syms.cpp
@@ -1,20 +1,4 @@
-#define SYM(SYM_NAME) extern "C" void SYM_NAME();
-SYM(PyBuffer_Release)
-SYM(PyCapsule_GetContext)
-SYM(PyCapsule_GetDestructor)
-SYM(PyCapsule_GetName)
-SYM(PyCapsule_GetPointer)
-SYM(PyCapsule_IsValid)
-SYM(PyCapsule_New)
-SYM(PyCapsule_SetContext)
-SYM(PyCapsule_SetDestructor)
-SYM(PyCapsule_SetName)
-SYM(PyCapsule_SetPointer)
-SYM(PyCell_New)
-SYM(PyObject_GetBuffer)
-SYM(Py_DecRef)
-SYM(Py_IncRef)
-#undef SYM
+#include <Python.h>
 
 #include <library/python/symbols/registry/syms.h>
 
@@ -31,7 +15,14 @@ SYM(PyCapsule_SetDestructor)
 SYM(PyCapsule_SetName)
 SYM(PyCapsule_SetPointer)
 SYM(PyCell_New)
+SYM(PyMem_Malloc)
 SYM(PyObject_GetBuffer)
+SYM(PyOS_Readline)
 SYM(Py_DecRef)
 SYM(Py_IncRef)
+
+#if PY_VERSION_HEX < 0x3000000
+SYM(PyFile_SetEncoding)
+SYM(PyFile_AsFile)
+#endif
 END_SYMS()
