c0d7b5bcfaca6b6e36561160633e582d9beabc9e
nadya02 <nadya02@yandex-team.com>
2025-05-16 18:45:35 +0300

    YT-24913: Split memory tracker commit_hash:af573daeadda0ee67ff6c0a595b6dc3ad6314c51

--- library/cpp/yt/memory/public.h
+++ library/cpp/yt/memory/public.h
@@ -12,8 +12,7 @@ constexpr size_t CacheLineSize = 64;
 class TChunkedMemoryPool;
 
 DECLARE_REFCOUNTED_STRUCT(IMemoryChunkProvider)
-DECLARE_REFCOUNTED_STRUCT(IMemoryUsageTracker)
-DECLARE_REFCOUNTED_STRUCT(IReservingMemoryUsageTracker)
+DECLARE_REFCOUNTED_STRUCT(ISimpleMemoryUsageTracker)
 DECLARE_REFCOUNTED_STRUCT(TSharedRangeHolder)
 
 using TMemoryTag = ui32;
