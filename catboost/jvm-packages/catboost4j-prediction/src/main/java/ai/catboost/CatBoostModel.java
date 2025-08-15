a3d941856f60e012c0250363f8d7a780990fe411
Saskov Lev Kirillovich <lsaskov@ozon.ru>
2025-08-15 10:26:41 +0300

    Java binding for predict transposed

--- catboost/jvm-packages/catboost4j-prediction/src/main/java/ai/catboost/CatBoostModel.java
+++ catboost/jvm-packages/catboost4j-prediction/src/main/java/ai/catboost/CatBoostModel.java
@@ -678,6 +678,25 @@ public class CatBoostModel implements AutoCloseable {
     }
 
     /**
+     * Apply model to a batch of objects, but features of objects used in column format.
+     * @param transposedFeatures numeric features in column format (row of matrix is one feature for all objects).
+     * @return                   Model predictions.
+     * @throws CatBoostError     In case of error within native library.
+     */
+    @NotNull
+    public CatBoostPredictions predictTransposed(final float[][] transposedFeatures) throws CatBoostError {
+        int resultSize = transposedFeatures[0].length;
+        final CatBoostPredictions prediction = new CatBoostPredictions(resultSize, getPredictionDimension());
+        implLibrary.catBoostModelPredictTransposed(
+            handle,
+            transposedFeatures,
+            prediction.getRawData()
+        );
+
+        return prediction;
+    }
+
+    /**
      * Same as {@link #predict(float[][], String[][], String[][], float[][][], CatBoostPredictions)}, but returns prediction instead of taking
      * it as the last parameter.
      *
