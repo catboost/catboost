f47cac055fd2c20e15d2280284b3fdc445349274
namorniradnug <namorniradnug@yandex-team.com>
2025-08-20 15:59:22 +0300

    Don't use deprecated literal operator
    Newer clang versions produce the `deprecated-literal-operator` warning.
    See https://reviews.llvm.org/D152632 and https://wg21.link/CWG2521.
    commit_hash:52ebdc26dfe511681a1d73f24a8ba96592bb1b47

--- util/generic/size_literals.h
+++ util/generic/size_literals.h
@@ -6,31 +6,31 @@
 
 // Unsigned literals
 
-constexpr ui64 operator"" _B(unsigned long long value) noexcept {
+constexpr ui64 operator""_B(unsigned long long value) noexcept {
     return value;
 }
 
-constexpr ui64 operator"" _KB(unsigned long long value) noexcept {
+constexpr ui64 operator""_KB(unsigned long long value) noexcept {
     return value * 1024;
 }
 
-constexpr ui64 operator"" _MB(unsigned long long value) noexcept {
+constexpr ui64 operator""_MB(unsigned long long value) noexcept {
     return value * 1024_KB;
 }
 
-constexpr ui64 operator"" _GB(unsigned long long value) noexcept {
+constexpr ui64 operator""_GB(unsigned long long value) noexcept {
     return value * 1024_MB;
 }
 
-constexpr ui64 operator"" _TB(unsigned long long value) noexcept {
+constexpr ui64 operator""_TB(unsigned long long value) noexcept {
     return value * 1024_GB;
 }
 
-constexpr ui64 operator"" _PB(unsigned long long value) noexcept {
+constexpr ui64 operator""_PB(unsigned long long value) noexcept {
     return value * 1024_TB;
 }
 
-constexpr ui64 operator"" _EB(unsigned long long value) noexcept {
+constexpr ui64 operator""_EB(unsigned long long value) noexcept {
     return value * 1024_PB;
 }
 
@@ -44,26 +44,26 @@ namespace NPrivate {
     }
 } // namespace NPrivate
 
-constexpr i64 operator"" _KBs(const unsigned long long value) noexcept {
+constexpr i64 operator""_KBs(const unsigned long long value) noexcept {
     return ::NPrivate::SignedCast(value * 1024);
 }
 
-constexpr i64 operator"" _MBs(unsigned long long value) noexcept {
+constexpr i64 operator""_MBs(unsigned long long value) noexcept {
     return ::NPrivate::SignedCast(value * 1024_KB);
 }
 
-constexpr i64 operator"" _GBs(unsigned long long value) noexcept {
+constexpr i64 operator""_GBs(unsigned long long value) noexcept {
     return ::NPrivate::SignedCast(value * 1024_MB);
 }
 
-constexpr i64 operator"" _TBs(unsigned long long value) noexcept {
+constexpr i64 operator""_TBs(unsigned long long value) noexcept {
     return ::NPrivate::SignedCast(value * 1024_GB);
 }
 
-constexpr i64 operator"" _PBs(unsigned long long value) noexcept {
+constexpr i64 operator""_PBs(unsigned long long value) noexcept {
     return ::NPrivate::SignedCast(value * 1024_TB);
 }
 
-constexpr i64 operator"" _EBs(unsigned long long value) noexcept {
+constexpr i64 operator""_EBs(unsigned long long value) noexcept {
     return ::NPrivate::SignedCast(value * 1024_PB);
 }
