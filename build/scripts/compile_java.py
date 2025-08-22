be266be5dcd177c045a5593146fdf93b869e3b39
snermolaev <snermolaev@yandex-team.com>
2025-08-22 09:18:41 +0300

    fix linter reports for build/scripts/compile_java.py commit_hash:b8e331f74518106d63b5be296355b75242b93bfc

--- build/scripts/compile_java.py
+++ build/scripts/compile_java.py
@@ -11,8 +11,8 @@ import sys
 # Explicitly enable local imports
 # Don't forget to add imported scripts to inputs of the calling command!
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-import process_command_files as pcf
-import java_command_file as jcf
+import process_command_files as pcf  # noqa: E402
+import java_command_file as jcf  # noqa: E402
 
 
 def parse_args(args):
