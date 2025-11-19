9621027e76d4cebbe7d1a0cb925676ae37dd0ade
pnv1 <pnv1@yandex-team.com>
2025-11-19 20:35:36 +0300

    Add no sse_4_1+ support to utf8_range library
    Targets that are built with -DUSE\_SSE4=no flag were broken in <https://nda.ya.ru/t/-HYmZ41z7NF6to>
    This patch fixes it
    commit_hash:da349c1267cc666537799b871ba56d2002380aeb

--- contrib/restricted/google/utf8_range/utf8_range.h
+++ contrib/restricted/google/utf8_range/utf8_range.h
@@ -15,6 +15,15 @@ int utf8_range_IsValid(const char* data, size_t len);
 // structurally valid UTF-8.
 size_t utf8_range_ValidPrefix(const char* data, size_t len);
 
+// Legacy API for backward compatibility
+// Only needed for platforms without SSE4.1 (where range2-sse.c is not compiled)
+#ifndef __SSE4_1__
+int utf8_naive(const unsigned char* data, int len);
+static inline int utf8_range2(const unsigned char* data, int len) {
+  return utf8_naive(data, len);
+}
+#endif
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif
