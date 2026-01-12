b7f6f7fefa4ebd08557491626a3fe7810620ac61
swarmer <swarmer@yandex-team.com>
2026-01-12 20:00:49 +0300

    implement Y_NO_UNIQUE_ADDRESS using msvc::no_unique_address on windows
    MSVC and clang-cl implements the C++20 `[[no_unique_address]]` feature via a specific, non-standard attribute: `[[msvc::no_unique_address]]`.
    commit_hash:975835261fcf805bfe65b51fb43852f71ff67edd

--- util/system/compiler.h
+++ util/system/compiler.h
@@ -795,6 +795,8 @@ Y_FORCE_INLINE void DoNotOptimizeAway(const T&) = delete;
  */
 #if Y_HAS_CPP_ATTRIBUTE(no_unique_address)
     #define Y_NO_UNIQUE_ADDRESS [[no_unique_address]]
+#elif Y_HAS_CPP_ATTRIBUTE(msvc::no_unique_address)
+    #define Y_NO_UNIQUE_ADDRESS [[msvc::no_unique_address]]
 #else
     #define Y_NO_UNIQUE_ADDRESS
 #endif
