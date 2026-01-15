85f307c4b8134025ad83895964c27061488614e8
mikhnenko <mikhnenko@yandex-team.com>
2026-01-15 20:25:32 +0300

    Fix building for CUDA 12.2 commit_hash:49b9a261c679963a41ba5c901fc47321fc6cb4d7

--- contrib/libs/cxxsupp/libcxx/include/__type_traits/datasizeof.h
+++ contrib/libs/cxxsupp/libcxx/include/__type_traits/datasizeof.h
@@ -33,7 +33,10 @@ inline const size_t __datasizeof_v = __datasizeof(_Tp);
 #else
 template <class _Tp>
 struct _FirstPaddingByte {
-  _LIBCPP_NO_UNIQUE_ADDRESS _Tp __v_;
+#if !defined(__CUDACC__) || __CUDACC_VER_MAJOR__ != 12 || __CUDACC_VER_MINOR__ != 2
+  _LIBCPP_NO_UNIQUE_ADDRESS
+#endif
+  _Tp __v_;
   char __first_padding_byte_;
 };
 
