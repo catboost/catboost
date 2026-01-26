29f3e2a9946f178fc870d881fa0c86b0b3ec74ca
akhropov <akhropov@yandex-team.com>
2026-01-26 23:39:49 +0300

    Fix for open source pytest: Remove broken and unused conftest.py. commit_hash:4edeeac20955a63c124671e40fb621bf84441778

--- catboost/pytest/large/conftest.py
+++
@@ -1,5 +0,0 @@
-try:
-    from catboost_pytest_lib import compressed_data  # noqa
-except ImportError:
-    from lib import compressed_data  # noqa
-    pytest_plugins = ["lib.common.pytest_plugin"]
