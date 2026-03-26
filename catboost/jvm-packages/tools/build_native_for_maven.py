29600d4a17f530a50e5755568d2f249025606720
akhropov <akhropov@yandex-team.com>
2026-03-26 23:15:30 +0300

    Add missing type annotations commit_hash:5d9ca18f680aeb7a1c83cc87709fc665d7aba01d

--- catboost/jvm-packages/tools/build_native_for_maven.py
+++ catboost/jvm-packages/tools/build_native_for_maven.py
@@ -25,18 +25,18 @@ import sys
 import zipfile
 
 
-def _get_platform():
+def _get_platform() -> str:
     if sys.platform.startswith('linux'):
         return 'linux'
     return sys.platform
 
-def _get_arch():
+def _get_arch() -> str:
     machine = platform.machine()
     if machine.lower() == 'amd64':
         return 'x86_64'
     return machine
 
-def _re_match_line_in_file(file_name: str, regexp: str):
+def _re_match_line_in_file(file_name: str, regexp: str) -> bool:
     matcher = re.compile(regexp)
     with open(file_name) as f:
         for line in f.readlines():
@@ -44,7 +44,7 @@ def _re_match_line_in_file(file_name: str, regexp: str):
                 return True
     return False
 
-def _get_arcadia_root():
+def _get_arcadia_root() -> str:
     arcadia_root = None
     path = os.path.dirname(os.path.abspath(sys.argv[0]))
     while True:
@@ -67,22 +67,22 @@ def _get_arcadia_root():
     return arcadia_root
 
 
-def _get_ya_path():
+def _get_ya_path() -> str:
     ya_path = os.path.join(_get_arcadia_root(), 'ya')
     assert os.path.isfile(ya_path), 'no `ya` in arcadia root'
     assert os.access(ya_path, os.X_OK), '`ya` must be executable'
     return ya_path
 
 
-def _get_package_resources_dir(base_dir):
+def _get_package_resources_dir(base_dir: str) -> str:
     return os.path.join(base_dir, 'src', 'main', 'resources')
 
 
-def _get_native_lib_dir(root_dir, package_arcadia_path):
+def _get_native_lib_dir(root_dir: str, package_arcadia_path: str) -> str:
     return os.path.join(root_dir, package_arcadia_path, 'src', 'native_impl')
 
 
-def _build_native_platform_to_jvm_platform(build_native_platform):
+def _build_native_platform_to_jvm_platform(build_native_platform: str) -> str:
     target_system, target_arch = build_native_platform.split('-')
     if target_system == 'windows':
         target_system = 'win32'
@@ -91,7 +91,7 @@ def _build_native_platform_to_jvm_platform(build_native_platform):
     return f'{target_system}-{target_arch}'
 
 
-def _get_platform_resources_dir(parsed_args):
+def _get_platform_resources_dir(parsed_args) -> str:
     if parsed_args.macos_universal_binaries:
         return 'darwin-universal2'
     elif parsed_args.target_platform is not None:
@@ -99,7 +99,7 @@ def _get_platform_resources_dir(parsed_args):
     else:
         return ''.join((_get_platform(), '-', _get_arch()))
 
-def _is_dll_java(project_path):
+def _is_dll_java(project_path: str) -> bool:
     return _re_match_line_in_file(os.path.join(project_path, 'ya.make'), '\W*DLL_JAVA\(.*\)$')
 
 
@@ -109,7 +109,7 @@ def _extract_classes_from_jar(jar_file, dst_dir):
             if member_name.endswith('.class'):
                 zf.extract(member_name, dst_dir)
 
-def _get_ya_make_platform_from_build_native_platform(target_platform):
+def _get_ya_make_platform_from_build_native_platform(target_platform: str) -> str:
     platform, arch = target_platform.lower().split('-')
     if arch == 'amd64':
         arch = 'x86_64'
@@ -119,7 +119,7 @@ def _get_ya_make_platform_from_build_native_platform(target_platform):
     return 'default-' + platform + '-' + arch
 
 
-def build_shared_lib_with_ya(parsed_args, top_src_root_dir, package_src_sub_path, extra_ya_make_args):
+def build_shared_lib_with_ya(parsed_args, top_src_root_dir: str, package_src_sub_path: str, extra_ya_make_args):
     print('building dynamic library with `ya`', file=sys.stderr)
     sys.stderr.flush()
 
@@ -179,7 +179,7 @@ def build_shared_lib_with_ya(parsed_args, top_src_root_dir, package_src_sub_path
             stdout=sys.stdout,
             stderr=sys.stderr)
 
-def postprocess_after_ya(native_lib_build_dir, lib_name, resources_dir, base_dir):
+def postprocess_after_ya(native_lib_build_dir: str, lib_name: str, resources_dir, base_dir: str):
     jar_name = lib_name + '.jar'
     jar_src_path = os.path.join(native_lib_build_dir, jar_name)
 
@@ -203,7 +203,7 @@ def postprocess_after_ya(native_lib_build_dir, lib_name, resources_dir, base_dir
 
         shutil.copy(os.path.join(native_lib_build_dir, lib_name + '-sources.jar'), target_dir)
 
-def postprocess_after_cmake(parsed_args, top_src_root_dir, resources_dir):
+def postprocess_after_cmake(parsed_args, top_src_root_dir: str, resources_dir: str):
     package_src_sub_path = os.path.relpath(parsed_args.base_dir, top_src_root_dir)
     if parsed_args.macos_universal_binaries:
         # build_native.build does not produce anything except final binaries in standard native_lib_build_dir place
