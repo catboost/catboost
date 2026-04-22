1074e8d31ad193c5fc61aca53013a384236c66b5
babenko <babenko@yandex-team.com>
2026-04-22 09:41:38 +0300

    YT-18571: Add format string checks to TStringBuilderBase::AppendFormat; also fix some releaved issues commit_hash:dd15e713a6a83c8a14f1df2f011fa06f189f4a00

--- library/cpp/yt/string/format-inl.h
+++ library/cpp/yt/string/format-inl.h
@@ -941,7 +941,7 @@ void FormatValue(
 {
     std::apply(
         [&] <class... TInnerArgs> (TInnerArgs&&... args) {
-            builder->AppendFormat(value.Format_, std::forward<TInnerArgs>(args)...);
+            builder->AppendFormat(TRuntimeFormat{value.Format_}, std::forward<TInnerArgs>(args)...);
         },
         value.Args_);
 }
