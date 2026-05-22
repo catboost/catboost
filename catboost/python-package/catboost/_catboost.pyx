48cd5736ea719f9616c6aed30003bfe6be15db4b
Pavel Ilin <ipaul64321@gmail.com>
2026-05-22 13:45:25 -0500

    added support of memoryview for blob arg of load_model

--- catboost/python-package/catboost/_catboost.pyx
+++ catboost/python-package/catboost/_catboost.pyx
@@ -5830,8 +5830,10 @@ cdef class _CatBoost:
         return py_serialized_model_str
 
     cpdef _deserialize_model(self, serialized_model_str):
+        cdef const unsigned char[::1] buf
+        buf = serialized_model_str
         self.model_blob = serialized_model_str
-        cdef TFullModel tmp_model = ReadZeroCopyModel(<char*>serialized_model_str, len(serialized_model_str))
+        cdef TFullModel tmp_model = ReadZeroCopyModel(<char*>&buf[0], len(buf))
         self.__model.Swap(tmp_model)
         self.__metrics_history = GetTrainingMetrics(self.__model[0])
 
