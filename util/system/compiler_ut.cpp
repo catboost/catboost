fc2fb482cc95db3d430c02fd3c2186037bc9ea77
vadim-xd <vadim-xd@yandex-team.com>
2026-05-25 20:01:13 +0300

    Add Y_INITIALIZED (silencer for bugprone-use-after-move) commit_hash:e683c92bdbc059b1f08f9a6239e9348849914e84

--- util/system/compiler_ut.cpp
+++ util/system/compiler_ut.cpp
@@ -2,6 +2,8 @@
 
 #include <library/cpp/testing/unittest/registar.h>
 
+#include <string>
+
 Y_UNIT_TEST_SUITE(TCompilerTest) {
     Y_UNIT_TEST(TestPragmaNoWshadow) {
         Y_PRAGMA_DIAGNOSTIC_PUSH
@@ -68,5 +70,14 @@ Y_UNIT_TEST_SUITE(TCompilerTest) {
         Bar();
 
         Y_PRAGMA_DIAGNOSTIC_POP
+    }
+
+    Y_UNIT_TEST(TestYInitialized) {
+        std::string s;
+        if (false) {
+            auto _ = std::move(s);
+        }
+        Y_INITIALIZED(s);   // not moved
+        Y_UNUSED(s.size()); // no bugprone-use-after-move warning
     }
 } // Y_UNIT_TEST_SUITE(TCompilerTest)
