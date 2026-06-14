628a81ce0226287fba72b41bfce2e13408ad1d24
thegeorg <thegeorg@yandex-team.com>
2026-06-14 18:27:23 +0300

    Import contrib/libs/jemalloc against modern nixpkgs commit_hash:91b685c6f9fc9c6009e262412a0dbcfbdd262e6b

--- contrib/libs/jemalloc/include/jemalloc/internal/jemalloc_internal_defs-linux.h
+++ contrib/libs/jemalloc/include/jemalloc/internal/jemalloc_internal_defs-linux.h
@@ -370,10 +370,10 @@
 #define LG_SIZEOF_INTMAX_T 3
 
 /* glibc malloc hooks (__malloc_hook, __realloc_hook, __free_hook). */
-#define JEMALLOC_GLIBC_MALLOC_HOOK 
+/* #undef JEMALLOC_GLIBC_MALLOC_HOOK */
 
 /* glibc memalign hook. */
-#define JEMALLOC_GLIBC_MEMALIGN_HOOK 
+/* #undef JEMALLOC_GLIBC_MEMALIGN_HOOK */
 
 /* pthread support */
 #define JEMALLOC_HAVE_PTHREAD 
