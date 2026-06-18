d4fec8778baf1a508ad3108fc0b3fd16585e9283
robot-piglet <robot-piglet@yandex-team.com>
2026-06-18 12:24:42 +0300

    Intermediate changes commit_hash:c405f13d6cfb6fcfa6d65079e669b6ca4ca1d21b

--- library/cpp/yt/system/benchmarks/cpu_id.cpp
+++ library/cpp/yt/system/benchmarks/cpu_id.cpp
@@ -4,26 +4,8 @@
 
 #include <sched.h>
 
-#include <util/system/types.h>
-
 namespace NYT {
 namespace {
-
-////////////////////////////////////////////////////////////////////////////////
-
-// The mechanism GetCurrentCpuId replaces in the per-CPU profiling sensors:
-// NProfiling::TTscp::Get() obtains the processor id via a serializing rdtscp.
-// Replicated here (instead of depending on yt/yt/core) to compare the bare cost.
-Y_FORCE_INLINE int GetCpuIdViaRdtscp()
-{
-#if defined(__x86_64__)
-    ui64 rax, rcx, rdx;
-    asm volatile ("rdtscp\n" : "=a" (rax), "=c" (rcx), "=d" (rdx) : : );
-    return static_cast<int>(rcx) & 63;
-#else
-    return sched_getcpu();
-#endif
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -35,15 +17,6 @@ void BM_GetCurrentCpuId(benchmark::State& state)
     }
 }
 BENCHMARK(BM_GetCurrentCpuId);
-
-// rdtscp, as in TTscp::Get().
-void BM_Rdtscp(benchmark::State& state)
-{
-    for (auto _ : state) {
-        benchmark::DoNotOptimize(GetCpuIdViaRdtscp());
-    }
-}
-BENCHMARK(BM_Rdtscp);
 
 // The fallback path: sched_getcpu() (vDSO getcpu).
 void BM_SchedGetCpu(benchmark::State& state)
