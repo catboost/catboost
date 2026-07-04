10b939d9dbab2fb9e9ad242c9de1143df310380d
akhropov <akhropov@yandex-team.com>
2026-07-05 00:44:27 +0300

    Fix style commit_hash:bda1de72d8951d875c11e8d3be02942a50bb59a4

--- library/cpp/hnsw/index_builder/mobius_transform.h
+++ library/cpp/hnsw/index_builder/mobius_transform.h
@@ -24,19 +24,18 @@ template <class TDenseVectorLikeStorage,
         class TVectorComponent = typename std::remove_const<typename std::remove_pointer<typename TDenseVectorLikeStorage::TItem>::type>::type,
         class TOutVectorComponent = typename std::conditional<std::is_same<TVectorComponent, double>::value, double, float>::type>
     TDenseVectorStorage<TOutVectorComponent> TransformMobius(const TDenseVectorLikeStorage& itemStorage) {
-        if(itemStorage.GetNumItems() == 0)
+        if (itemStorage.GetNumItems() == 0) {
             return TDenseVectorStorage<TOutVectorComponent>(TBlob(), itemStorage.GetDimension());
+        }
         TBuffer buffer;
         buffer.Resize(itemStorage.GetNumItems() * itemStorage.GetDimension() * sizeof(TOutVectorComponent));
         TOutVectorComponent* bufferPtr = reinterpret_cast<TOutVectorComponent*>(buffer.data());
-        for(size_t i = 0; i < itemStorage.GetNumItems(); i++)
-        {
+        for (size_t i = 0; i < itemStorage.GetNumItems(); i++) {
             auto currentVector = itemStorage.GetItem(i);
             TOutVectorComponent vectorLengthSqr = DotProduct(currentVector, currentVector, itemStorage.GetDimension());
             Y_ABORT_UNLESS(vectorLengthSqr > 0, "All vectors should have positive length");
-            for(size_t j = 0; j < itemStorage.GetDimension(); j++)
-            {
-                *bufferPtr = static_cast<TOutVectorComponent>(currentVector[j])/vectorLengthSqr;
+            for (size_t j = 0; j < itemStorage.GetDimension(); j++) {
+                *bufferPtr = static_cast<TOutVectorComponent>(currentVector[j]) / vectorLengthSqr;
                 bufferPtr++;
             }
         }
