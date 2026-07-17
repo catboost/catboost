7ecbba6deb923775ceb25eb22dd9c632acfad506
andreyshspb <andreyshspb@yandex-team.com>
2026-07-17 11:28:34 +0300

    Add Emirati Arabic to cpp/langs
    Add Emirati Arabic to cpp/langs
    commit_hash:d5525e91a6bde4b123eca81915b55d4e3002430e

--- library/cpp/langs/langs.cpp
+++ library/cpp/langs/langs.cpp
@@ -185,6 +185,7 @@ namespace {
         {LANG_KRL, SCRIPT_LATIN, "Karelian Proper", "krl", "krl", nullptr},
         {LANG_OLO, SCRIPT_LATIN, "Livvi-Karelian", "olo", "olo", nullptr},
         {LANG_JDT, SCRIPT_CYRILLIC, "Judeo-Tat", "jdt", "jdt", nullptr},
+        {LANG_ARE, SCRIPT_ARABIC, "Emirati Arabic", "are", "ae", "ar-AE"},
     };
 
     static_assert(static_cast<size_t>(LANG_MAX) == Y_ARRAY_SIZE(LanguageNameAndEnum), "Size doesn't match");
