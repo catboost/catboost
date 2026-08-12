ae107dbd5238d6b58a045156cb2b40589aaf027b
Aryan Trehan <tre_aryan@outlook.com>
2026-08-12 16:06:27 +0530

    valid/invalid tests configured

--- catboost/libs/model/ut/cpp_model_export_ut.cpp
+++ catboost/libs/model/ut/cpp_model_export_ut.cpp
@@ -1,13 +1,15 @@
 #include <catboost/libs/model/ut/lib/model_test_helpers.h>
 #include <catboost/libs/model/model_export/model_exporter.h>
-#include <library/cpp/testing/unittest/registar.h>
+
 #include <library/cpp/json/json_value.h>
 #include <library/cpp/json/json_writer.h>
+#include <library/cpp/testing/unittest/registar.h>
 
 #include <util/stream/file.h>
 
 using namespace std;
 using namespace NCB;
+using namespace NFs;
 
 namespace {
     TString BuildUserParams(const TString& Namespace) {
@@ -22,27 +24,19 @@ namespace {
 }
 
 Y_UNIT_TEST_SUITE(TCppModelExportNamespace) {
-
-    Y_UNIT_TEST(TestNoNamespace) {
-        TFullModel model = TrainFloatCatboostModel();
-        ExportModel(model, "model_no_ns.cpp", EModelType::Cpp);
-
-        TString code = ReadFile("model_no_ns.cpp");
-        UNIT_ASSERT(!code.Contains("namespace"));
-    }
-
-    Y_UNIT_TEST(TestValidNamespace) {
-        TFullModel model = TrainFloatCatboostModel();
-        ExportModel(model, "model_valid_ns.cpp", EModelType::Cpp, BuildUserParams("MyNamespace"));
-
-        TString code = ReadFile("model_valid_ns.cpp");
-        UNIT_ASSERT(code.Contains("namespace MyNamespace {"));
-    }
-
     Y_UNIT_TEST(TestInvalidNamespace) {
         TFullModel model = TrainFloatCatboostModel();
         UNIT_ASSERT_EXCEPTION(
+            ExportModel(model, "model_invalid_ns.cpp", EModelType::Cpp, BuildUserParams("")),
+            yexception);
+        UNIT_ASSERT_EXCEPTION(
             ExportModel(model, "model_invalid_ns.cpp", EModelType::Cpp, BuildUserParams("123abc")),
             yexception);
+        UNIT_ASSERT_EXCEPTION(
+            ExportModel(model, "model_invalid_ns.cpp", EModelType::Cpp, BuildUserParams("abc$")),
+            yexception);    
+        UNIT_ASSERT_EXCEPTION(
+            ExportModel(model, "model_invalid_ns.cpp", EModelType::Cpp, BuildUserParams("const")),
+            yexception);    
     }
 }
