6206ceb1bdf5c45db01ae84ce800b2026150af86
vadim-xd <vadim-xd@yandex-team.com>
2026-09-08 01:14:25 +0300

    Fix ::DoSwap for final classes, use fallback if Swap/swap is not invocable commit_hash:498c81752115ca9a97cd2f927b85b38ffb9c981e

--- util/generic/utility_ut.cpp
+++ util/generic/utility_ut.cpp
@@ -15,13 +15,29 @@ public:
 
     inline void Swap(TTest& t) {
         DoSwap(Val, t.Val);
+        SwapCalled = true;
     }
 
     int Val;
+    bool SwapCalled = false;
 
 private:
     TTest(const TTest&);
     TTest& operator=(const TTest&);
+};
+
+struct TFinal final {
+    int Val;
+};
+
+class TPrivateSwap {
+public:
+    int Val;
+
+private:
+    inline void Swap(TPrivateSwap&) {
+        Y_ABORT("Should not be called");
+    }
 };
 
 struct TUnorderedTag {
@@ -58,6 +74,27 @@ Y_UNIT_TEST_SUITE(TUtilityTest) {
     Y_UNIT_TEST(TestSwapClass) {
         TTest i(0);
         TTest j(1);
+
+        DoSwap(i, j);
+
+        UNIT_ASSERT_EQUAL(i.Val, 1);
+        UNIT_ASSERT_EQUAL(j.Val, 0);
+        UNIT_ASSERT(i.SwapCalled ^ j.SwapCalled);  // exactly one Swap() call
+    }
+
+    Y_UNIT_TEST(TestSwapPrivateMethod) {
+        TPrivateSwap i(0);
+        TPrivateSwap j(1);
+
+        DoSwap(i, j);
+
+        UNIT_ASSERT_EQUAL(i.Val, 1);
+        UNIT_ASSERT_EQUAL(j.Val, 0);
+    }
+
+    Y_UNIT_TEST(TestSwapFinalClass) {
+        TFinal i(0);
+        TFinal j(1);
 
         DoSwap(i, j);
 
