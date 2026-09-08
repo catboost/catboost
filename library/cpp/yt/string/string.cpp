b53d12eab64ecd69c1b95459f2ba654861afd636
osidorkin <osidorkin@yandex-team.com>
2026-09-08 10:42:46 +0300

    YT-27951: Use TStringBuf, fix comment style commit_hash:816a1c86a06d0ee3f5e8411f440698ed327866f5

--- library/cpp/yt/string/string.cpp
+++ library/cpp/yt/string/string.cpp
@@ -382,12 +382,12 @@ std::string TruncateString(std::string string, int lengthLimit, TStringBuf trunc
     return string;
 }
 
-TTruncatedStringView::TTruncatedStringView(const std::string& value, int limit)
+TTruncatedStringView::TTruncatedStringView(TStringBuf value Y_LIFETIME_BOUND, int limit)
     : Value_(value)
     , Limit_(std::max(limit, 0))
 { }
 
-void TTruncatedStringView::WriteToBuilder(TStringBuilderBase* builder, TStringBuf /* spec */) const
+void TTruncatedStringView::WriteToBuilder(TStringBuilderBase* builder, TStringBuf /*spec*/) const
 {
     i64 valueSize = std::ssize(Value_);
     i64 maxSize = Limit_ + std::ssize(DefaultTruncatedMessage);
